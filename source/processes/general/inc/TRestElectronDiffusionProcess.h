27b14773d6a82eb1edc4f615cb32efe61dd4690d
Javier Galan <j.a.galan.81@gmail.com>
2015-10-27 13:13:19 +0100

    Added a variable for spatial resolution of electrons

--- source/processes/general/inc/TRestElectronDiffusionProcess.h
+++ source/processes/general/inc/TRestElectronDiffusionProcess.h
@@ -31,6 +31,7 @@ class TRestElectronDiffusionProcess:public TRestEventProcess {
         Double_t fMinPosition;
 
         Double_t fElectricField;
+        Double_t fResolution;
 
         void InitFromConfigFile();
 
