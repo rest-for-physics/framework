0e7e953855789aa20a853396e620c72d27753e7f
Javier Galan <j.a.galan.81@gmail.com>
2015-10-27 13:19:02 +0100

    Reindent-ed file
    Former-commit-id: ab6b1fe28f9ca2df28cf82e55a3e483a76095387

--- source/events/geant4/inc/TRestHits.h
+++ source/events/geant4/inc/TRestHits.h
@@ -16,43 +16,44 @@ using namespace std;
 //! It saves a 3-coordinate position and an energy for each punctual deposition.
 class TRestHits : public TObject
 {
-	public:
-	Int_t     fNHits;	///< Number of punctual energy depositions, it is the lenght for all the array
-	Double_t  fTotEnergy;	///< Event total energy
+    public:
+        Int_t     fNHits;	///< Number of punctual energy depositions, it is the lenght for all the array
+        Double_t  fTotEnergy;	///< Event total energy
 
-	vector <Int_t>   fX;		// [fNHits] Position on X axis for each punctual deposition (units microms)
-	vector <Int_t>   fY;		// [fNHits] Position on Y axis for each punctual deposition (units microms)
-	vector <Int_t>   fZ;		// [fNHits] Position on Z axis for each punctual deposition (units microms)
-	vector <Int_t>   fEnergy;	// [fNHits] Energy deposited at each 3-coordinate position (units eV)
+        vector <Int_t>   fX;		// [fNHits] Position on X axis for each punctual deposition (units microms)
+        vector <Int_t>   fY;		// [fNHits] Position on Y axis for each punctual deposition (units microms)
+        vector <Int_t>   fZ;		// [fNHits] Position on Z axis for each punctual deposition (units microms)
+        vector <Int_t>   fEnergy;	// [fNHits] Energy deposited at each 3-coordinate position (units eV)
 
-	//! Changes the orgin of the Cartesian coordinate system
-	void ChangeOrigin(double origx, double origy, double origz);
+        //! Changes the orgin of the Cartesian coordinate system
+        void ChangeOrigin(double origx, double origy, double origz);
 
-    void AddHit( Double_t x, Double_t y, Double_t z, Double_t en );
-    void AddHit( TVector3 pos, Double_t en );
-    void RemoveHits( );
+        void AddHit( Double_t x, Double_t y, Double_t z, Double_t en );
+        void AddHit( TVector3 pos, Double_t en );
+        void RemoveHits( );
 
-    Int_t GetNumberOfHits( ) { return fNHits; }
+        Int_t GetNumberOfHits( ) { return fNHits; }
 
-     Double_t GetX( int n ) { return ( (Double_t) fX[n])/1000.; } // return value in mm
-     Double_t GetY( int n ) { return ( (Double_t) fY[n])/1000.; } // return value in mm
-     Double_t GetZ( int n ) { return ( (Double_t) fZ[n])/1000.; } // return value in mm
-     Double_t GetEnergy( int n ) { return ( (Double_t) fEnergy[n])/1000.; } //return value in keV
+        Double_t GetX( int n ) { return ( (Double_t) fX[n])/1000.; } // return value in mm
+        Double_t GetY( int n ) { return ( (Double_t) fY[n])/1000.; } // return value in mm
+        Double_t GetZ( int n ) { return ( (Double_t) fZ[n])/1000.; } // return value in mm
+        Double_t GetEnergy( int n ) { return ( (Double_t) fEnergy[n])/1000.; } //return value in keV
+
+        Double_t GetDistance2( int n, int m );
+
+        TVector3 Get( int n ) {
+            return TVector3 ( ( (Double_t) fX[n])/1000., ((Double_t) fY[n])/1000., ((Double_t) fZ[n])/1000. ) ; 
+        }
 
 
-     TVector3 Get( int n ) {
-        return TVector3 ( ( (Double_t) fX[n])/1000., ((Double_t) fY[n])/1000., ((Double_t) fZ[n])/1000. ) ; 
-      }
+        Double_t GetTotalDepositedEnergy() { return fTotEnergy; }
 
 
-	Double_t GetTotalDepositedEnergy() { return fTotEnergy; }
-	
+        //Construtor
+        TRestHits();
+        //Destructor
+        ~TRestHits();
 
-	//Construtor
-	TRestHits();
-	//Destructor
-	~TRestHits();
-
-	ClassDef(TRestHits, 1);
+        ClassDef(TRestHits, 1);
 };
 #endif
