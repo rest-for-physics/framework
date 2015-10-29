a0dc5d44b1c5e0cc010a8c61c0c7b99e0d073c66
juanan <juanan@sultan.unizar.es>
2015-10-29 18:13:01 +0100

    A function DrawEvent have been added to TRestSignalEvent, it returns a TPad with the pulsed. Adding getters and setters, to the classes.

--- source/events/general/src/TRestSignalEvent.cxx
+++ source/events/general/src/TRestSignalEvent.cxx
@@ -17,6 +17,8 @@
 
 
 #include "TRestSignalEvent.h"
+#include "TGraph.h"
+#include "TH1.h"
 
 ClassImp(TRestSignalEvent)
 //______________________________________________________________________________
@@ -36,12 +38,61 @@ void TRestSignalEvent::Initialize()
 {
     TRestEvent::Initialize();
     fEventClassName = "TRestSignalEvent";
-
+    fSignal.clear();
 }
 
 void TRestSignalEvent::PrintEvent()
 {
-    TRestEvent::PrintEvent();
+    //TRestEvent::PrintEvent();
 
 
 }
+
+//Draw current event in a Tpad
+TPad *TRestSignalEvent::DrawEvent(){
+
+int nSignals = this->GetNumberOfSignals();
+
+TGraph *gr = new TGraph[nSignals];
+
+TVector2 *v;
+
+int c;
+
+double maxX=-1E10,minX=1E10,maxY=-1E10,minY=1E10;
+
+//cout<<"N Signals "<<nSignals<<endl;
+
+	for(int i=0;i<nSignals;i++){
+	c=0;
+	//cout<<"Signal "<<i<<" points"<<fSignal[i].GetNumberOfPoints()<<endl;
+		for(int j=0;j<fSignal[i].GetNumberOfPoints();j++){
+		v=fSignal[i].GetPoint(j);
+		gr[i].SetPoint(c,v->X(),v->Y());
+		if(v->X()>maxX)maxX=v->X();
+		if(v->X()<minX)minX=v->X();
+		if(v->Y()>maxY)maxY=v->Y();
+		if(v->Y()<minY)minY=v->Y();
+		
+		//cout<<v->X()<<"  "<<v->Y()<<endl;
+		c++;
+		
+		}
+	
+	}
+
+TPad *pad = new TPad(this->GetClassName().Data()," ",0,0,1,1);
+pad->Draw();
+pad->cd();
+pad->DrawFrame(minX,minY,maxX,maxY);
+for(int i=0;i<nSignals;i++){
+pad->cd();
+gr[i].Draw("LP");
+}
+
+return pad;
+
+}
+
+
+
