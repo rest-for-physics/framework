5a961bd70317dd46ca93b65dd77c977eadfc244b
Javier Galan <j.a.galan.81@gmail.com>
2015-11-02 20:15:36 +0100

    Added Getters/Setters/Print/Sort/Integral methods to Signal event classes

--- source/events/general/src/TRestSignalEvent.cxx
+++ source/events/general/src/TRestSignalEvent.cxx
@@ -12,24 +12,24 @@
 ///             sept 2015:   First concept
 ///                 Created as part of the conceptualization of existing REST 
 ///                 software.
-///                 JuanAn Garcia
+///                 JuanAn Garcia/Javier Galan
 ///_______________________________________________________________________________
 
 
 #include "TRestSignalEvent.h"
 
 ClassImp(TRestSignalEvent)
-//______________________________________________________________________________
-    TRestSignalEvent::TRestSignalEvent()
+    //______________________________________________________________________________
+TRestSignalEvent::TRestSignalEvent()
 {
-   // TRestSignalEvent default constructor
-   Initialize();
+    // TRestSignalEvent default constructor
+    Initialize();
 }
 
 //______________________________________________________________________________
 TRestSignalEvent::~TRestSignalEvent()
 {
-   // TRestSignalEvent destructor
+    // TRestSignalEvent destructor
 }
 
 void TRestSignalEvent::Initialize()
@@ -39,9 +39,59 @@ void TRestSignalEvent::Initialize()
 
 }
 
+void TRestSignalEvent::AddSignal(TRestSignal s) 
+{ 
+    if( signalIDExists( s.GetSignalID() ) )
+    {
+        cout << "Warning. Signal ID : " << s.GetSignalID() << " already exists. Signal will not be added to signal event" << endl;
+        return;
+    }
+
+    fSignal.push_back(s);
+}
+
+Int_t TRestSignalEvent::GetSignalIndex( Int_t signalID ) 
+{ 
+    for( int i = 0; i < GetNumberOfSignals(); i++ ) 
+        if (fSignal[i].GetSignalID() == signalID ) 
+            return i; 
+    return -1; 
+} 
+
+Double_t TRestSignalEvent::GetIntegral( ) 
+{
+    Double_t sum = 0;
+    for( int i = 0; i < GetNumberOfSignals(); i++ )
+    {
+        sum += fSignal[i].GetIntegral();
+    }
+    return sum;
+}
+
+void TRestSignalEvent::AddChargeToSignal( Int_t sgnlID, Double_t tm, Double_t chrg )
+{
+    Int_t sgnlIndex = GetSignalIndex( sgnlID );
+    if( sgnlIndex == -1 ) { cout << "Warning. Signal ID does not exist. I cannot add charge to signal" << endl; return; }
+
+    fSignal[sgnlIndex].AddDeposit( tm, chrg );
+}
+
 void TRestSignalEvent::PrintEvent()
 {
     TRestEvent::PrintEvent();
+
+    for( int i = 0; i < GetNumberOfSignals(); i++ )
+    {
+        cout << "================================================" << endl;
+        cout << "Signal ID : " << fSignal[i].GetSignalID() << endl;
+        cout << "Integral : " << fSignal[i].GetIntegral() << endl;
+        cout << "------------------------------------------------" << endl;
+        fSignal[i].Print();
+        cout << "================================================" << endl;
+
+
+    }
+
 
 
 }
