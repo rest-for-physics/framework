12af0225d40e810293103f1d1b293b0595f442b3
Javier Gracia <jgraciag@unizar.es>
2016-03-07 18:49:38 +0100

    A script to generate the gaussian convolution process to a TRestSignalEvent is implemented
    Former-commit-id: fa3c3c5f9798956ce0755446760fa12c9956a743

---
+++ scripts/restsim/RESTSIM_GaussianConvolution.C
@@ -0,0 +1,17 @@
+
+Int_t RESTSIM_GaussianConvolution( TString fName, Int_t firstEvent = 0, Int_t numberOfEventsToProcess = 0, char *cfgFilename = "myConfig.rml" )
+{
+    TRestRun *run = new TRestRun( cfgFilename );
+
+    run->OpenInputFile( fName );
+
+    run->PrintInfo();
+    
+    TRestSignalGaussianConvolutionProcess *eGaussConvProcess = new TRestSignalGaussianConvolutionProcess( );
+    
+    run->AddProcess( eGaussConvProcess, cfgFilename );
+
+    run->ProcessEvents( firstEvent, numberOfEventsToProcess );
+
+    delete run;
+}
