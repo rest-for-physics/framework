7ff843f50f53fced8a1bf3c3456ee778fe89126e
Javier Galan <j.a.galan.81@gmail.com>
2016-03-18 08:31:58 +0100

    pandaXIII examples. Modified default maxStepSize and experiment name.
    Former-commit-id: cf0ba163b2668f62a9e11a28016d36dba4d37e05

--- config/pandaXIII/generateExamples.sh
+++ config/pandaXIII/generateExamples.sh
@@ -24,186 +24,179 @@ GEO='REST_GEOMETRY_TYPE=traditional;REST_GEOMETRY_SETUP=pandaXIII_Setup_WT12m.gd
 GEO_SWT='REST_GEOMETRY_TYPE=traditional;REST_GEOMETRY_SETUP=pandaXIII_Setup_WT6m.gdml'
 
  ######################       GAS        ###########################
-PARAMS='REST_EMIN=0;REST_EMAX=10;REST_MAXSTEPSIZE=100'
-NEVENTS=10000
+PARAMS='REST_EMIN=0;REST_EMAX=10;REST_MAXSTEPSIZE=10'
 
 mkdir gas -p
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml gas/Xe137.rml \
- "[REST_VOLUME=gas;$Xe137;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=gas;$Xe137;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml gas/Co60.rml \
- "[REST_VOLUME=gas;$Co60;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=gas;$Co60;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml gas/Bi214.rml \
- "[REST_VOLUME=gas;$Bi214;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=gas;$Bi214;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/decay0FromVolume.rml gas/Xe136bb0n.rml \
- "[REST_VOLUME=gas;$XeNLDBD;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=gas;$XeNLDBD;$GEO;$PARAMS]"
  
  ./../replaceRMLVars.py templates/decay0FromVolume.rml gas/Xe136_0nubb_00.rml \
- "[REST_VOLUME=gas;REST_GENERATOR=Xe136_0nubb_00;REST_GENERATOR_FILE=0nubb_data_00_o.evt;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=gas;REST_GENERATOR=Xe136_0nubb_00;REST_GENERATOR_FILE=0nubb_data_00_o.evt;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/decay0FromVolume.rml gas/Xe136bb2n.rml \
- "[REST_VOLUME=gas;$Xe2NDBD;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=gas;$Xe2NDBD;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/decay0FromVolume.rml gas/Xe136_2nubb_00.rml \
- "[REST_VOLUME=gas;REST_GENERATOR=Xe136_2nubb_00;REST_GENERATOR_FILE=2nubb_data_00_o.evt;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=gas;REST_GENERATOR=Xe136_2nubb_00;REST_GENERATOR_FILE=2nubb_data_00_o.evt;$GEO;$PARAMS]"
 
 
  ######################       VESSEL        ###########################
 
-PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=100'
-NEVENTS=100000
+PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=10'
 
 mkdir vessel -p
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml vessel/Co60.rml \
- "[REST_VOLUME=vessel;$Co60;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=vessel;$Co60;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml vessel/Th232.rml \
- "[REST_VOLUME=vessel;$Th232;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=vessel;$Th232;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml vessel/Ra228.rml \
- "[REST_VOLUME=vessel;$Ra228;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=vessel;$Ra228;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml vessel/U238.rml \
- "[REST_VOLUME=vessel;$U238;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=vessel;$U238;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml vessel/Th234.rml \
- "[REST_VOLUME=vessel;$Th234;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=vessel;$Th234;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml vessel/Bi214.rml \
- "[REST_VOLUME=vessel;$Bi214;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=vessel;$Bi214;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml vessel/Tl208.rml \
- "[REST_VOLUME=vessel;$Tl208;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=vessel;$Tl208;$GEO;$PARAMS]"
 
 
 
  ######################       WATER TANK        ###########################
 
-PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=100'
-NEVENTS=100000
+PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=10'
 
 mkdir waterTank -p
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml waterTank/Co60.rml \
- "[REST_VOLUME=waterTank;$Co60;$GEO_SWT;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=waterTank;$Co60;$GEO_SWT;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml waterTank/Th232.rml \
- "[REST_VOLUME=waterTank;$Th232;$GEO_SWT;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=waterTank;$Th232;$GEO_SWT;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml waterTank/Ra228.rml \
- "[REST_VOLUME=waterTank;$Ra228;$GEO_SWT;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=waterTank;$Ra228;$GEO_SWT;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml waterTank/U238.rml \
- "[REST_VOLUME=waterTank;$U238;$GEO_SWT;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=waterTank;$U238;$GEO_SWT;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml waterTank/Th234.rml \
- "[REST_VOLUME=waterTank;$Th234;$GEO_SWT;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=waterTank;$Th234;$GEO_SWT;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml waterTank/Bi214.rml \
- "[REST_VOLUME=waterTank;$Bi214;$GEO_SWT;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=waterTank;$Bi214;$GEO_SWT;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml waterTank/Tl208.rml \
- "[REST_VOLUME=waterTank;$Tl208;$GEO_SWT;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=waterTank;$Tl208;$GEO_SWT;$PARAMS]"
 
 
 
  ######################       MICROMEGAS        ###########################
 
-PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=100'
-NEVENTS=100000
+PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=10'
 
 VOLUME='micromegas1'
 
 mkdir $VOLUME -p
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Co60.rml \
- "[REST_VOLUME=$VOLUME;$Co60;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Co60;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Th232.rml \
- "[REST_VOLUME=$VOLUME;$Th232;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Th232;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Ra228.rml \
- "[REST_VOLUME=$VOLUME;$Ra228;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Ra228;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/U238.rml \
- "[REST_VOLUME=$VOLUME;$U238;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$U238;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Th234.rml \
- "[REST_VOLUME=$VOLUME;$Th234;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Th234;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Bi214.rml \
- "[REST_VOLUME=$VOLUME;$Bi214;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Bi214;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Tl208.rml \
- "[REST_VOLUME=$VOLUME;$Tl208;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Tl208;$GEO;$PARAMS]"
 
 
  ######################       RINGS        ###########################
 
-PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=100'
-NEVENTS=100000
+PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=10'
 
 VOLUME='rings'
 
 mkdir $VOLUME -p
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Co60.rml \
- "[REST_VOLUME=$VOLUME;$Co60;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Co60;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Th232.rml \
- "[REST_VOLUME=$VOLUME;$Th232;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Th232;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Ra228.rml \
- "[REST_VOLUME=$VOLUME;$Ra228;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Ra228;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/U238.rml \
- "[REST_VOLUME=$VOLUME;$U238;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$U238;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Th234.rml \
- "[REST_VOLUME=$VOLUME;$Th234;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Th234;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Bi214.rml \
- "[REST_VOLUME=$VOLUME;$Bi214;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Bi214;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Tl208.rml \
- "[REST_VOLUME=$VOLUME;$Tl208;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Tl208;$GEO;$PARAMS]"
 
  ######################       SUPPORTS        ###########################
 
-PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=100'
-NEVENTS=100000
+PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=10'
 
 VOLUME='supports'
 
 mkdir $VOLUME -p
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Co60.rml \
- "[REST_VOLUME=$VOLUME;$Co60;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Co60;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Th232.rml \
- "[REST_VOLUME=$VOLUME;$Th232;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Th232;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Ra228.rml \
- "[REST_VOLUME=$VOLUME;$Ra228;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Ra228;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/U238.rml \
- "[REST_VOLUME=$VOLUME;$U238;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$U238;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Th234.rml \
- "[REST_VOLUME=$VOLUME;$Th234;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Th234;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Bi214.rml \
- "[REST_VOLUME=$VOLUME;$Bi214;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Bi214;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVolume.rml $VOLUME/Tl208.rml \
- "[REST_VOLUME=$VOLUME;$Tl208;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[REST_VOLUME=$VOLUME;$Tl208;$GEO;$PARAMS]"
 
 
  ######################       ELECTRONICS        ###########################
 
-PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=100'
-NEVENTS=100000
+PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=10'
 
 VOLUME='electronics'
 
@@ -212,25 +205,25 @@ mkdir $VOLUME -p
 POSITION='REST_X=0;REST_Y=0;REST_Z=-1150'
 
  ./../replaceRMLVars.py templates/isotopeFromPoint.rml $VOLUME/Co60.rml \
- "[$POSITION;$Co60;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$POSITION;$Co60;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromPoint.rml $VOLUME/Th232.rml \
- "[$POSITION;$Th232;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$POSITION;$Th232;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromPoint.rml $VOLUME/Ra228.rml \
- "[$POSITION;$Ra228;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$POSITION;$Ra228;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromPoint.rml $VOLUME/U238.rml \
- "[$POSITION;$U238;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$POSITION;$U238;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromPoint.rml $VOLUME/Th234.rml \
- "[$POSITION;$Th234;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$POSITION;$Th234;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromPoint.rml $VOLUME/Bi214.rml \
- "[$POSITION;$Bi214;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$POSITION;$Bi214;$GEO;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromPoint.rml $VOLUME/Tl208.rml \
- "[$POSITION;$Tl208;$GEO;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$POSITION;$Tl208;$GEO;$PARAMS]"
 
 
  ######################       RESISTIVE FIELD CAGE      ###########################
@@ -238,31 +231,29 @@ POSITION='REST_X=0;REST_Y=0;REST_Z=-1150'
 GEO='REST_GEOMETRY_TYPE=resistive;REST_GEOMETRY_SETUP=pandaXIII_Setup.gdml'
 VCYL='REST_LENGTH=2;REST_RADIUS=730'
 
-NEVENTS=100000
-PARAMS='REST_EMIN=0;REST_EMAX=10;REST_MAXSTEPSIZE=100'
+PARAMS='REST_EMIN=0;REST_EMAX=10;REST_MAXSTEPSIZE=10'
 
 VOLUME='resistive'
 
 mkdir $VOLUME -p
 
  ./../replaceRMLVars.py templates/isotopeFromVCylinder.rml $VOLUME/Bi214.rml \
- "[$Bi214;$GEO;$VCYL;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$Bi214;$GEO;$VCYL;$PARAMS]"
 
  ./../replaceRMLVars.py templates/isotopeFromVCylinder.rml $VOLUME/Tl208.rml \
- "[$Tl208;$GEO;$VCYL;$PARAMS;REST_NEVENTS=$NEVENTS]"
+ "[$Tl208;$GEO;$VCYL;$PARAMS]"
 
 
  ######################       EXTERNAL GAMMA      ###########################
 
 GEO='REST_GEOMETRY_TYPE=traditional;REST_GEOMETRY_SETUP=pandaXIII_Setup_WT12m.gdml'
-PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=100'
-NEVENTS=100000
+PARAMS='REST_EMIN=0;REST_EMAX=50;REST_MAXSTEPSIZE=10'
 
 mkdir -p external
 
  ../replaceRMLVars.py templates/biasedIsotopeFromVirtualBox.rml external/gammaFromU238AfterBiasing.rml \
- "[REST_ISOTOPE=U238;$PARAMS;REST_NEVENTS=$NEVENTS;REST_BOXSIZE=3000]" 
+ "[REST_ISOTOPE=U238;$PARAMS;REST_BOXSIZE=3000]" 
 
  ../replaceRMLVars.py templates/biasedIsotopeFromVirtualBox.rml external/gammaFromTh232AfterBiasing.rml \
- "[REST_ISOTOPE=Th232;$PARAMS;REST_NEVENTS=$NEVENTS;REST_BOXSIZE=3000]" 
+ "[REST_ISOTOPE=Th232;$PARAMS;REST_BOXSIZE=3000]" 
 
