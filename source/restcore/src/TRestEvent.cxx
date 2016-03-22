09877e7126db23a708b10bad0fef3bfab8eacb65
Javier Galan <j.a.galan.81@gmail.com>
2016-03-22 10:12:00 +0100

    TRestEvent. Major modificacion to include a subEventId and subEventTag inside any event.
    Former-commit-id: 3f25a9cacd0ec65a380933ddd20aa4dc9856a4e7

--- source/restcore/src/TRestEvent.cxx
+++ source/restcore/src/TRestEvent.cxx
@@ -20,28 +20,52 @@
 using namespace std;
 
 ClassImp(TRestEvent)
-//______________________________________________________________________________
-    TRestEvent::TRestEvent()
+    //______________________________________________________________________________
+TRestEvent::TRestEvent()
 {
 
-   // TRestEvent default constructor
+    // TRestEvent default constructor
 }
 
 //______________________________________________________________________________
 TRestEvent::~TRestEvent()
 {
-   // TRestEvent destructor
+    // TRestEvent destructor
 }
 
 void TRestEvent::Initialize()
 {
- fEventID = 0; fEventTime=0; fEventClassName="TRestEvent"; 
+    fEventID = 0; 
+    fEventTime = 0; 
+    fSubEventID = 0;
+    fSubEventTag = "";
+    fOk = true;
+}
+
+void TRestEvent::SetEventTime( Double_t time )
+{ 
+    Int_t sec = (Int_t)time;
+    Int_t nsec = (Int_t) ((time-sec)*1E9);
+
+    fEventTime.SetSec(sec);
+    fEventTime.SetNanoSec(nsec);
+}
+
+void TRestEvent::SetEventTime( Double_t seconds, Double_t nanoseconds )
+{
+    fEventTime.SetSec( seconds );
+    fEventTime.SetNanoSec( nanoseconds );
 }
 
 void TRestEvent::PrintEvent()
 {
     cout << "*******************************************************" << endl;
-    cout << " EVENT : " << GetEventID() << " TIME : " << GetEventTime() << endl;
+    cout << " EVENT ID : " << GetEventID() << " TIME : " << GetEventTime() << endl;
+    cout << " SUB-EVENT ID : " << GetSubEventID();
+    if ( fSubEventTag != "" ) cout << "   SUB-EVENT TAG : \"" << fSubEventTag << "\""; 
+    cout << endl;
+    if( fOk ) cout << " Status : OK" << endl;
+    else cout << " Status : NOT OK" << endl;
     cout << "*******************************************************" << endl;
 
 }
