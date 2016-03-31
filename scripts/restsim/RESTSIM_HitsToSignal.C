5668e78f70e78102cc4efa8d4b3a135e3501423c
Javier Galan <j.a.galan.81@gmail.com>
2016-03-31 22:05:21 +0200

    Revised scripts to use the new default config template.
    Former-commit-id: 202908f5d7f8193dd89d4e319f77de491c54ded5

--- scripts/restsim/RESTSIM_HitsToSignal.C
+++ scripts/restsim/RESTSIM_HitsToSignal.C
@@ -1,5 +1,5 @@
 
-Int_t RESTSIM_HitsToSignal( TString fName, Int_t firstEvent = 0, Int_t numberOfEventsToProcess = 0, char *cfgFilename = "myConfig.rml" )
+Int_t RESTSIM_HitsToSignal( TString fName, Int_t firstEvent = 0, Int_t numberOfEventsToProcess = 0, char *cfgFilename = "template/config.rml" )
 {
 
     TRestRun *run = new TRestRun( cfgFilename );
