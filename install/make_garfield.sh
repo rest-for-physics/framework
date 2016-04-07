b587858b578f0ea08563a09ce5951179bd9e29d5
Hao Liang <lihawl@126.com>
2016-04-07 21:13:32 +0800

    tiny change in make_garfield
    Former-commit-id: 192e7fb78d1590256611d7247ce42d74407f8c61

--- install/make_garfield.sh
+++ install/make_garfield.sh
@@ -5,7 +5,9 @@ set -e
 source REST_v2/install/REST_ENV.sh
 mkdir external/garfield -p
 cd external/garfield
+    echo checkout the code...
     svn co http://svn.cern.ch/guest/garfield/trunk $GARFIELD_HOME
+    echo building...
     mkdir build -p
     cd build
         cmake ..
