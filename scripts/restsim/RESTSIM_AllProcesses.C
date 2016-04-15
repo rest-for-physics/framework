04d67369fab6ae5ffd3aadf0760b7c894741ef05
Javier Galan <j.a.galan.81@gmail.com>
2016-04-15 20:41:14 +0200

    RESTSIM_AllProcesses.C commented.
    Former-commit-id: 22ccd6afe22ae1a8807aedac3816cc19315b9f20

--- scripts/restsim/RESTSIM_AllProcesses.C
+++ scripts/restsim/RESTSIM_AllProcesses.C
@@ -32,15 +32,44 @@ Int_t RESTSIM_AllProcesses( TString fName, Int_t firstEvent = 0, Int_t numberOfE
 
     TRestTrackPathMinimizationProcess *trackPathMinimizationProcess = new TRestTrackPathMinimizationProcess( );
 
-    // There is an issue when trying to use the viewers it seems related to Geant4 analysis process
-    // you need to comment the g4AnalysisProcess to skip this problem
+    // TRestG4Event
+    // ============
+    // This process fills the analysis tree taking values from a TRestG4Event
     run->AddProcess( g4AnalysisProcess, cfgFilename );
+
+    // This process produces the electron generation and diffusion of the hits inside a TRestG4Event
+    // It uses the readout to define the drift volume and the gas properties.
+    // TRestG4Event will be transformed into a TRestHitsEvent
     run->AddProcess( eDiffProcess, cfgFilename );
+
+    // TRestHitsEvent
+    // ============
+    // This process will take the hits in a TRestHitsEvent.
+    // It will translate them to signals following the description of the readout provided, and the properties of the gas.
+    // TRestHitsEvent will be transformed into a TRestSignalEvent
     run->AddProcess( hToSignalProcess, cfgFilename );
+
+    // TRestSignalEvent
+    // ================
+    // This process extracts parameter from a TRestSignalEvent and will fill the analysis tree.
     run->AddProcess( signalAnalysisProcess, cfgFilename );
+
+    // This process will do the hit reconstruction, recovering the hits from signal daq channels.
+    // It will use the readout and the gas properties
     run->AddProcess( signalToHitsProcess, cfgFilename );
+
+    // This process will do clusterization of neightbouring hits grouping them into tracks
     run->AddProcess( hitsToTrackProcess, cfgFilename );
+
+    // TRestTrackEvent
+    // ================
+    // This process will do hit reduction, it will merge the closest hits up to a defined distance,
+    // or until the number of nodes below a given value.
     run->AddProcess( trackReductionProcess, cfgFilename );
+
+    // This process will find the minimum path between the hits of each track. 
+    // This process does a very basic blob finding (for testing pourposes), this needs to be debugged and optimized.
+    // The blobs are used to define the fixed extremes in the path minimization.
     run->AddProcess( trackPathMinimizationProcess, cfgFilename );
 
     run->ProcessEvents( firstEvent, numberOfEventsToProcess );
