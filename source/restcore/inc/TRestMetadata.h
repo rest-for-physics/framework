8b81bfd6d231a1c820db4eaf066fc5434c72984a
Javier Galan <j.a.galan.81@gmail.com>
2016-04-28 15:59:25 +0200

    TRestMetadata. Added method to allow XML-style comments. Added capability to extract a section with a specific name using LoadSectionMetadata. Other members addded to determine if is a root file. GetChar() method added.
    Former-commit-id: b046fd486c65da2f621095dc5050e3d3c1fe5371

--- source/restcore/inc/TRestMetadata.h
+++ source/restcore/inc/TRestMetadata.h
@@ -84,8 +84,9 @@ class TRestMetadata:public TNamed {
 
         virtual void Initialize() = 0;// { cout << __PRETTY_FUNCTION__ << endl; };
 
-        Int_t LoadSectionMetadata( std::string section, std::string cfgFileName );
+        Int_t LoadSectionMetadata( std::string section, std::string cfgFileName, string name="" );
 
+        Int_t LoadConfigFromFile( string cfgFileName, std::string name );
         Int_t LoadConfigFromFile( std::string cfgFileName );
 
     private:
@@ -114,6 +115,7 @@ class TRestMetadata:public TNamed {
         std::string ReplaceMathematicalExpressions( std::string buffer );
         std::string ReplaceEnvironmentalVariables( const std::string buffer );
         std::string ExtractLoopStructure( std::string in, size_t pos );
+        std::string RemoveComments( string in );
 
         std::string GetSectionByNameFromFile( std::string nref, std::string fref );
         Int_t FindSection( std::string buffer, size_t startPos = 0 );
@@ -152,7 +154,9 @@ class TRestMetadata:public TNamed {
         static std::string RemoveWhiteSpaces( std::string in );
         static std::string Replace( std::string in, std::string thisString, std::string byThisString, size_t fromPosition );
         static Int_t Count( std::string s, std::string sbstring);
-        static bool fileExists(const std::string& filename);
+        static bool fileExists( const std::string& filename );
+        static bool isRootFile( const std::string& filename ); 
+        static void GetChar(){ cout << "Press a KEY to continue ..." << endl; getchar(); }
 
 
         //////////////////////////////////////////////////
