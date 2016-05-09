a750201b1e1b4fb640a600028c34a7d5c519ad94
Javier Galan <j.a.galan.81@gmail.com>
2016-05-09 09:25:50 +0200

    config/pandaXIII added supporters to cleanExamples.sh
    Former-commit-id: fa49ea50982452fa5ecf7b7e68e6c074c9cfb5cc

--- config/pandaXIII/cleanExamples.sh
+++ config/pandaXIII/cleanExamples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a dirs=( "micromegas1" "vessel" "electronics" "supports" "rings" "gas" "resistive" "external" "waterTank" "external" )
+declare -a dirs=( "micromegas1" "vessel" "electronics" "supporters" "rings" "gas" "resistive" "external" "waterTank" "external" )
 
 for dir in "${dirs[@]}"
 do
