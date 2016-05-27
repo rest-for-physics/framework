1edbc21c950388050b941a493ee76c2367ab83fa
Javier Galan <j.a.galan.81@gmail.com>
2016-05-27 15:16:14 +0200

    Added new signal process TRestSignalShaping process. Added new AGET response.
    Former-commit-id: a9adb263de960f5d4acf324c74e1fb43cf0848a5

---
+++ source/processes/signal/inc/TRestSignalShapingProcess.h
@@ -0,0 +1,67 @@
+///______________________________________________________________________________
+///______________________________________________________________________________
+///______________________________________________________________________________
+///             
+///
+///             RESTSoft : Software for Rare Event Searches with TPCs
+///
+///             TRestSignalShapingProcess.h
+///
+///_______________________________________________________________________________
+
+
+#ifndef RestCore_TRestSignalShapingProcess
+#define RestCore_TRestSignalShapingProcess
+
+#include <TRestReadout.h>
+#include <TRestSignalEvent.h>
+
+#include "TRestEventProcess.h"
+
+class TRestSignalShapingProcess:public TRestEventProcess {
+
+    private:
+        TRestSignalEvent *fInputSignalEvent;
+        TRestSignalEvent *fOutputSignalEvent;
+        TRestSignal *responseSignal;
+
+        void InitFromConfigFile();
+
+        void Initialize();
+
+        void LoadDefaultConfig();
+
+    protected:
+        //add here the members of your event process
+        TString fResponseFilename;
+
+    public:
+        void InitProcess();
+        void BeginOfEventProcess(); 
+        TRestEvent *ProcessEvent( TRestEvent *eventInput );
+        void EndOfEventProcess(); 
+        void EndProcess();
+
+        void LoadConfig( std::string cfgFilename );
+
+        void PrintMetadata() 
+        { 
+            BeginPrintProcess();
+
+            EndPrintProcess();
+        }
+
+        TRestMetadata *GetProcessMetadata( ) { return NULL; }
+
+        TString GetProcessName() { return (TString) "signalShaping"; }
+
+        //Constructor
+        TRestSignalShapingProcess();
+        TRestSignalShapingProcess( char *cfgFileName );
+        //Destructor
+        ~TRestSignalShapingProcess();
+
+        ClassDef(TRestSignalShapingProcess, 1);      // Template for a REST "event process" class inherited from TRestEventProcess
+};
+#endif
+
