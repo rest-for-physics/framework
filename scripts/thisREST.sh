dfe210f36d57e36eaa398464bb39669ef9f873dc
Catofes <i@catofes.com>
2016-07-14 13:45:28 +0800

    Update CMakeLits.
    Use building garfield by default. But you can set BUILD_GARFIELD FALSE
    to use external garfield.
    
    fix bug in thisREST.sh
    
    
    Former-commit-id: 205ac9825cafd6f1697a8c07eb4dffd2c4cff944

--- scripts/thisREST.sh
+++ scripts/thisREST.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 REST_DIR=$(cd "$(dirname "$0")"; pwd)
+if [ ! -d $REST_DIR/lib/libRestCore.so ]; then
+    REST_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+fi
 
+if [ ! -d $REST_DIR/lib/libRestCore.so ]; then
+    echo "Can not find REST path."
+    exit 1
+fi
 export PATH=$PATH:$REST_DIR/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$REST_DIR/lib
 export LIBRARY_PATH=$LIBRARY_PATH:$REST_DIR/lib
