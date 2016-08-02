953f9eb9269b8aad8d3901cf7431b921726d9d19
Javier Galan <j.a.galan.81@gmail.com>
2016-08-02 11:16:55 +0200

    Added REST_PATH and restRoot definitions.
    Former-commit-id: 637da8b7a43ee0e14faf8e8caca7e4fefa21edd7

--- scripts/thisREST.sh
+++ scripts/thisREST.sh
@@ -4,12 +4,17 @@ REST_DIR=$(cd "$(dirname "$0")"; pwd)
 if [ ! -s $REST_DIR/lib/libRestCore.so ]; then
     REST_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 fi
-echo $REST_DIR
+
 if [ ! -s "$REST_DIR/lib/libRestCore.so" ]; then
     echo "Can not find REST path."
 	exit 1
 fi
-echo "REST path set."
+
+echo "REST path set to $REST_DIR"
+
+alias restRoot="root -l $REST_DIR/LoadRESTScripts.C"
+
+export REST_PATH=$REST_DIR
 export PATH=$PATH:$REST_DIR/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$REST_DIR/lib
 export LIBRARY_PATH=$LIBRARY_PATH:$REST_DIR/lib
