ad40f62d0a4d3c3601464f06ed2d7bf9b5f2e6dc
Javier Galan <j.a.galan.81@gmail.com>
2016-08-08 12:07:40 +0200

    CMake minor modifications.
    Former-commit-id: ce5dc1de86cc60bf73300bf571b91e9699dc794d

--- scripts/thisREST.sh
+++ scripts/thisREST.sh
@@ -10,6 +10,13 @@ if [ ! -s "$REST_DIR/lib/libRestCore.so" ]; then
 	exit 1
 fi
 
+alias restRoot="root -l $REST_DIR/LoadRESTScripts.C"
+
+export REST_PATH=$REST_DIR
+export PATH=$PATH:$REST_DIR/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$REST_DIR/lib
+export LIBRARY_PATH=$LIBRARY_PATH:$REST_DIR/lib
+
 echo "                                                    "
 echo "  **************************************************"
 echo "              W E L C O M E   to  R E S T           "
@@ -22,12 +29,15 @@ echo "                                                    "
 echo "        Use restRoot command to launch ROOT with    "
 echo "        REST libraries and scripts loaded           "
 echo "                                                    "
+echo "     REST Support mail-list : rest-support@cern.ch  "
+echo "     REST Development mail-list : rest-dev@cern.ch  "
+echo "                                                    "
+echo "         You can subscribe at egroups.cern.ch       "
+echo "                                                    "
+echo "         If you have no CERN account you can        "
+echo "         get an account linked to your usual        "
+echo "             mail address at this site:             "
+echo "                                                    "
+echo "      https://account.cern.ch/account/Externals/    "
 echo "  **************************************************"
 echo "                                                    "
-
-alias restRoot="root -l $REST_DIR/LoadRESTScripts.C"
-
-export REST_PATH=$REST_DIR
-export PATH=$PATH:$REST_DIR/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$REST_DIR/lib
-export LIBRARY_PATH=$LIBRARY_PATH:$REST_DIR/lib
