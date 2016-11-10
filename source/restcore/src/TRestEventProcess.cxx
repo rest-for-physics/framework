37567c74a25524d988706e830e3b9509c22fed40
Igor Irastorza <igor.irastorza@cern.ch>
2016-11-11 00:08:38 +0100

    TRestEventProcess: document class for doxygen
    Conflicts:
    	source/restcore/inc/TRestEventProcess.h
    
    Former-commit-id: 88fe103169f79a9fc3807daad405b5e94ee130bb

--- source/restcore/src/TRestEventProcess.cxx
+++ source/restcore/src/TRestEventProcess.cxx
@@ -1,47 +1,81 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///             
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
+
+
+//////////////////////////////////////////////////////////////////////////
 ///
-///             RESTSoft : Software for Rare Event Searches with TPCs
+/// RESTsoft - Software for Rare Event Searches with TPCs
 ///
-///             TRestEventProcess.cxx
+/// \class      TRestEventProcess
+/// A base class for any REST event process
 ///
-///             Base class from which to inherit all REST process classes inherit
-///             A process class in REST deals with one input, ouput (or both) 
-///             single REST events. RestMetadata controls the access to config 
-///             files.
+/// One of the core classes of REST. Absract class
+/// from which all REST "event process classes" must derive.
+/// An event process in REST is an atomic operation on one single
+/// (input) event, potentially producing a different (output) event.
+/// Which type of (input/output) events will depent on the type of
+/// process. Processes can be tailored via metadata classes.
+/// All this needs to be defined in the inherited class.
+/// TRestEventProcess provides the basic structure (virtual functions)
 ///
-///             jun 2014:   First concept
-///                 Created as part of the conceptualization of existing REST 
-///                 software.
-///                 Igor G. Irastorza
-///_______________________________________________________________________________
+///
+/// History of developments:
+///
+/// 2014-june: First concept. As part of conceptualization of previous REST
+///       code (REST v2) 
+///       Igor G. Irastorza
+///
+//////////////////////////////////////////////////////////////////////////
 
 
 #include "TRestEventProcess.h"
 using namespace std;
 
 ClassImp(TRestEventProcess)
-//______________________________________________________________________________
-    TRestEventProcess::TRestEventProcess()
+
+//////////////////////////////////////////////////////////////////////////
+/// TRestEventProcess default constructor
+
+TRestEventProcess::TRestEventProcess()
 {
-   // TRestEventProcess default constructor
 }
 
+//////////////////////////////////////////////////////////////////////////
+/// TRestEventProcess destructor
 
-//______________________________________________________________________________
 TRestEventProcess::~TRestEventProcess()
 {
-   // TRestEventProcess destructor
 }
+
+//////////////////////////////////////////////////////////////////////////
+/// Returns pointer to gas (TRestGas) metadata
+///
+/// TODO : For the moment this function will return the first occurence of TRestGas.
+/// What happens if there are several? And if I want to use one gas from the config file? 
+/// We need to introduce an option somewhere.
+/// For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
 
 TRestMetadata *TRestEventProcess::GetGasMetadata( )
 {
-    // TODO : For the moment this function will return the first occurence of TRestGas.
-    // What happens if there are several? And if I want to use one gas from the config file? 
-    // We need to introduce an option somewhere.
-    // For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
 
     for( size_t i = 0; i < fRunMetadata.size(); i++ )
         if ( fRunMetadata[i]->ClassName() == (TString) "TRestGas" )
@@ -50,13 +84,16 @@ TRestMetadata *TRestEventProcess::GetGasMetadata( )
     return NULL;
 }
 
+//////////////////////////////////////////////////////////////////////////
+/// Returns pointer to readout metadata
+///
+/// TODO : For the moment this function will return the first occurence of TRestReadout.
+/// What happens if there are several? And if I want to use one gas from the config file? 
+/// We need to introduce an option somewhere.
+/// For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
+
 TRestMetadata *TRestEventProcess::GetReadoutMetadata( )
 {
-    // TODO : For the moment this function will return the first occurence of TRestReadout.
-    // What happens if there are several? And if I want to use one gas from the config file? 
-    // We need to introduce an option somewhere.
-    // For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
-
     for( size_t i = 0; i < fRunMetadata.size(); i++ )
         if ( fRunMetadata[i]->ClassName() == (TString) "TRestReadout" )
             return fRunMetadata[i];
@@ -64,13 +101,16 @@ TRestMetadata *TRestEventProcess::GetReadoutMetadata( )
     return NULL;
 }
 
+//////////////////////////////////////////////////////////////////////////
+/// Returns pointer to G4 metadata
+///
+/// TODO : For the moment this function will return the first occurence of TRestG4Metadata.
+/// What happens if there are several? And if I want to use one gas from the config file? 
+/// We need to introduce an option somewhere.
+/// For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
+
 TRestMetadata *TRestEventProcess::GetGeant4Metadata( )
 {
-    // TODO : For the moment this function will return the first occurence of TRestReadout.
-    // What happens if there are several? And if I want to use one gas from the config file? 
-    // We need to introduce an option somewhere.
-    // For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
-
     for( size_t i = 0; i < fRunMetadata.size(); i++ )
         if ( fRunMetadata[i]->ClassName() == (TString) "TRestG4Metadata" )
             return fRunMetadata[i];
@@ -78,19 +118,29 @@ TRestMetadata *TRestEventProcess::GetGeant4Metadata( )
     return NULL;
 }
 
+//////////////////////////////////////////////////////////////////////////
+/// Returns pointer to G4 metadata
+///
+/// TODO : For the moment this function will return the first occurence of TRestDetectorSetup.
+/// What happens if there are several? And if I want to use one gas from the config file? 
+/// We need to introduce an option somewhere.
+/// For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
+
 TRestMetadata *TRestEventProcess::GetDetectorSetup( )
 {
-    // TODO : For the moment this function will return the first occurence of TRestReadout.
-    // What happens if there are several? And if I want to use one gas from the config file? 
-    // We need to introduce an option somewhere.
-    // For the moment I know there will be an existing gas file since the hits come from electronDiffusion.
-
     for( size_t i = 0; i < fRunMetadata.size(); i++ )
         if ( fRunMetadata[i]->ClassName() == (TString) "TRestDetectorSetup" )
             return fRunMetadata[i];
 
     return NULL;
 }
+
+//////////////////////////////////////////////////////////////////////////
+/// Retrieve parameter with name parName from metadata className
+///
+/// \param className string with name of metadata class to access
+/// \param parName  string with name of parameter to retrieve
+///
 
 Double_t TRestEventProcess::GetDoubleParameterFromClass( TString className, TString parName )
 {
@@ -104,6 +154,13 @@ Double_t TRestEventProcess::GetDoubleParameterFromClass( TString className, TStr
 
     return PARAMETER_NOT_FOUND_DBL;
 }
+
+//////////////////////////////////////////////////////////////////////////
+/// Retrieve parameter with name parName from metadata className
+///
+/// \param className string with name of metadata class to access
+/// \param parName  string with name of parameter to retrieve
+///
 
 Double_t TRestEventProcess::GetDoubleParameterFromClassWithUnits( TString className, TString parName )
 {
@@ -141,6 +198,11 @@ void TRestEventProcess::EndProcess()
    cout << GetName() << ": Process ending..." << endl;
 }
 */
+
+//////////////////////////////////////////////////////////////////////////
+/// Show default console output at starting of process
+///
+
 void TRestEventProcess::BeginPrintProcess()
 {
 	cout << endl;
@@ -166,6 +228,10 @@ void TRestEventProcess::BeginPrintProcess()
 		cout << endl;
 	}
 }
+
+//////////////////////////////////////////////////////////////////////////
+/// Show default console output at finish of process
+///
 
 void TRestEventProcess::EndPrintProcess()
 {
