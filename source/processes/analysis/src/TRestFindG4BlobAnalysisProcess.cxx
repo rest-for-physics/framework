c622f007e4039fe9f3afca1ca9e069f717d05508
Javier Galan <j.a.galan.81@gmail.com>
2016-11-17 11:39:19 +0100

    TRestFindG4BlobAnalysisProcess. Added blob energy analysis observables.
    Former-commit-id: ea58a378356ed80615191d1f2a375bc1d969b836

--- source/processes/analysis/src/TRestFindG4BlobAnalysisProcess.cxx
+++ source/processes/analysis/src/TRestFindG4BlobAnalysisProcess.cxx
@@ -64,7 +64,28 @@ void TRestFindG4BlobAnalysisProcess::LoadConfig( std::string cfgFilename, std::s
 //______________________________________________________________________________
 void TRestFindG4BlobAnalysisProcess::InitProcess()
 {
-    TRestEventProcess::ReadObservables();
+    std::vector <string> fObservables;
+    fObservables = TRestEventProcess::ReadObservables();
+
+    for( unsigned int i = 0; i < fObservables.size(); i++ )
+    {
+        if( fObservables[i].find( "Q1_R") != string::npos )
+            fQ1_Observables.push_back( fObservables[i] );
+        if( fObservables[i].find( "Q2_R") != string::npos )
+            fQ2_Observables.push_back( fObservables[i] );
+    }
+
+    for( unsigned int i = 0; i < fQ1_Observables.size(); i++ )
+    {
+        Double_t r1 = atof ( fQ1_Observables[i].substr( 4, fQ1_Observables[i].length() - 4 ).c_str() );
+        fQ1_Radius.push_back( r1 );
+    }
+
+    for( unsigned int i = 0; i < fQ2_Observables.size(); i++ )
+    {
+        Double_t r2 = atof ( fQ2_Observables[i].substr( 4, fQ2_Observables[i].length() - 4 ).c_str() );
+        fQ2_Radius.push_back( r2 );
+    }
 
     fG4Metadata = (TRestG4Metadata *) GetGeant4Metadata( );
 }
@@ -111,36 +132,63 @@ TRestEvent* TRestFindG4BlobAnalysisProcess::ProcessEvent( TRestEvent *evInput )
                 xBlob1 = track->GetHits()->GetX( nHits-1); 
                 yBlob1 = track->GetHits()->GetY( nHits-1); 
                 zBlob1 = track->GetHits()->GetZ( nHits-1); 
-
-                obsName = this->GetName() + (TString) ".x1";
-                fAnalysisTree->SetObservableValue( obsName, xBlob1 );
-
-                obsName = this->GetName() + (TString) ".y1";
-                fAnalysisTree->SetObservableValue( obsName, yBlob1 );
-
-                obsName = this->GetName() + (TString) ".z1";
-                fAnalysisTree->SetObservableValue( obsName, zBlob1 );
             }
             else
             {
                 xBlob2 = track->GetHits()->GetX( nHits-1); 
                 yBlob2 = track->GetHits()->GetY( nHits-1); 
                 zBlob2 = track->GetHits()->GetZ( nHits-1); 
-                
-                obsName = this->GetName() + (TString) ".x2";
-                fAnalysisTree->SetObservableValue( obsName, xBlob2 );
-
-                obsName = this->GetName() + (TString) ".y2";
-                fAnalysisTree->SetObservableValue( obsName, yBlob2 );
-
-                obsName = this->GetName() + (TString) ".z2";
-                fAnalysisTree->SetObservableValue( obsName, zBlob2 );
             }
 
 
             nBlobs++;
         }
     }
+    
+    // The blob with z-coordinate closer to z=0 is stored in x1,y1,z1 
+    Double_t x1,y1,z1;
+    Double_t x2,y2,z2;
+
+    if( fabs( zBlob1 ) < fabs( zBlob2 ) )
+    {
+        x1 = xBlob1;
+        y1 = yBlob1;
+        z1 = zBlob1;
+
+        x2 = xBlob2;
+        y2 = yBlob2;
+        z2 = zBlob2;
+    }
+    else
+    {
+        x1 = xBlob2;
+        y1 = yBlob2;
+        z1 = zBlob2;
+
+        x2 = xBlob1;
+        y2 = yBlob1;
+        z2 = zBlob1;
+
+    }
+
+
+    obsName = this->GetName() + (TString) ".x1";
+    fAnalysisTree->SetObservableValue( obsName, x1 );
+
+    obsName = this->GetName() + (TString) ".y1";
+    fAnalysisTree->SetObservableValue( obsName, y1 );
+
+    obsName = this->GetName() + (TString) ".z1";
+    fAnalysisTree->SetObservableValue( obsName, z1 );
+    
+    obsName = this->GetName() + (TString) ".x2";
+    fAnalysisTree->SetObservableValue( obsName, x2 );
+
+    obsName = this->GetName() + (TString) ".y2";
+    fAnalysisTree->SetObservableValue( obsName, y2 );
+
+    obsName = this->GetName() + (TString) ".z2";
+    fAnalysisTree->SetObservableValue( obsName, z2 );
 
     Double_t deltaX = xBlob1 - xBlob2;
     Double_t deltaY = yBlob1 - yBlob2;
@@ -151,6 +199,25 @@ TRestEvent* TRestFindG4BlobAnalysisProcess::ProcessEvent( TRestEvent *evInput )
 
     obsName = this->GetName() + (TString) ".distance";
     fAnalysisTree->SetObservableValue( obsName, blobDistance );
+
+    /// We get the energy of the blobs
+    TRestHits hits = fG4Event->GetHits();
+    
+    for( unsigned int n = 0; n < fQ1_Observables.size(); n++ )
+    {
+        Double_t q = hits.GetEnergyInSphere( x1, y1, z1, fQ1_Radius[n] );
+
+        obsName = this->GetName() + (TString) "." + (TString) fQ1_Observables[n];
+        fAnalysisTree->SetObservableValue( obsName, q );
+    }
+
+    for( unsigned int n = 0; n < fQ2_Observables.size(); n++ )
+    {
+        Double_t q = hits.GetEnergyInSphere( x2, y2, z2, fQ2_Radius[n] );
+
+        obsName = this->GetName() + (TString) "." + (TString) fQ2_Observables[n];
+        fAnalysisTree->SetObservableValue( obsName, q );
+    }
 
     return fG4Event;
 }
