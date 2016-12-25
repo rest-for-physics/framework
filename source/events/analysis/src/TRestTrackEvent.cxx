7ce39c2e6553a7f86424bd8e95e04231817ccd24
Javier Galan <j.a.galan.81@gmail.com>
2016-12-25 23:08:22 +0100

    TRestTrackEvent. Added options to DrawEvent (WIP)
    Former-commit-id: 20ca7f6ff87a5d84ba64ad556431947581fc446d

--- source/events/analysis/src/TRestTrackEvent.cxx
+++ source/events/analysis/src/TRestTrackEvent.cxx
@@ -19,6 +19,8 @@
 
 
 #include "TRestTrackEvent.h"
+#include "TRestTools.h"
+
 using namespace std;
 
 ClassImp(TRestTrackEvent)
@@ -236,8 +238,25 @@ void TRestTrackEvent::PrintEvent( Bool_t fullInfo )
 }
 
 //Draw current event in a Tpad
-TPad *TRestTrackEvent::DrawEvent()
+TPad *TRestTrackEvent::DrawEvent( TString option )
 {
+    Bool_t drawXZ = false;
+    Bool_t drawYZ = false;
+    Bool_t drawXY = false;
+    Bool_t drawXYZ = false;
+    Bool_t drawLines = false;
+
+    vector <TString> optList = REST_Tools::GetOptions( option );
+
+    for( unsigned int n = 0; n < optList.size(); n++ )
+    {
+        if( optList[n] == "XZ" ) drawXZ = true;
+        if( optList[n] == "YZ" ) drawYZ = true;
+        if( optList[n] == "XY" ) drawXY = true;
+        if( optList[n] == "XYZ" ) drawXYZ = true;
+        if( optList[n] == "L" || optList[n] == "lines"  ) drawLines = true;
+    }
+
     if( fXYHit != NULL ) { delete[] fXYHit; fXYHit=NULL;}
     if( fXZHit != NULL ) { delete[] fXZHit; fXZHit=NULL;}
     if( fYZHit != NULL ) { delete[] fYZHit; fYZHit=NULL;}
@@ -247,12 +266,6 @@ TPad *TRestTrackEvent::DrawEvent()
     if( fPad != NULL ) { delete fPad; fPad=NULL;}
 
     int nTracks = this->GetNumberOfTracks();
-
-    /*
-    PrintOnlyTracks();
-    PrintEvent();
-    getchar();
-    */
 
     if( nTracks == 0 )
     {
