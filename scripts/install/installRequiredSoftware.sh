a7b48e69dfa9eb017c288fe9d8892039408f4f59
Javier Galan <j.a.galan.81@gmail.com>
2017-01-07 11:33:44 +0100

    installRequiredSoftware.sh updated.
    Former-commit-id: 79544d54ef3fda6408a01af561543ed5ec0071b7

---
+++ scripts/install/installRequiredSoftware.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo apt-get -y install vim zip unzip
+
+sudo apt-get -y install cmake g++ gfortran
+sudo apt-get -y install libgl1-mesa-dev libglu1-mesa-dev
+
+# version control system
+sudo apt-get -y install subversion git
+
+# we install our version of root
+#sudo apt-get install root-system
+
+# garfield
+
+# latex
+sudo apt-get -y install texlive-latex-base
+
+# root
+sudo apt-get -y install libxpm-dev libxft-dev 
+
+# geant4
+sudo apt-get -y install expat libexpat1-dev zlib1g-dev libxerces-c3.1 libxerces-c-dev libicu-dev qt4-dev-tools qt4-doc qt4-qtconfig
+
+
+
