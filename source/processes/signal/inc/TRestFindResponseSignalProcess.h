ac5a8c4bc4a1627b6e3b9a34a4dbae76e57756a2
Javier Galan <j.a.galan.81@gmail.com>
2017-03-04 09:38:37 +0800

    Added TRestFindResponseSignalProcess added to get a file with response files from data.
    Former-commit-id: 7dd7c797fdec316851dcca671757e1de7990fcb5

---
+++ source/processes/signal/inc/TRestFindResponseSignalProcess.h
@@ -0,0 +1,64 @@
+///______________________________________________________________________________
+///______________________________________________________________________________
+///______________________________________________________________________________
+///             
+///
+///             RESTSoft : Software for Rare Event Searches with TPCs
+///
+///             TRestFindResponseSignalProcess.h
+///
+///_______________________________________________________________________________
+
+
+#ifndef RestCore_TRestFindResponseSignalProcess
+#define RestCore_TRestFindResponseSignalProcess
+
+#include <TRestRawSignalEvent.h>
+
+#include "TRestEventProcess.h"
+
+class TRestFindResponseSignalProcess:public TRestEventProcess {
+
+    private:
+        TRestRawSignalEvent *fInputSignalEvent;
+        TRestRawSignalEvent *fOutputSignalEvent;
+
+        void InitFromConfigFile();
+
+        void Initialize();
+
+        void LoadDefaultConfig();
+
+    protected:
+        //add here the members of your event process
+
+    public:
+        void InitProcess();
+        void BeginOfEventProcess(); 
+        TRestEvent *ProcessEvent( TRestEvent *eventInput );
+        void EndOfEventProcess(); 
+        void EndProcess();
+
+        void LoadConfig( std::string cfgFilename, string name = "" );
+
+        void PrintMetadata() 
+        { 
+            BeginPrintProcess();
+
+            EndPrintProcess();
+        }
+
+        TRestMetadata *GetProcessMetadata( ) { return NULL; }
+
+        TString GetProcessName() { return (TString) "findResponseSignal"; }
+
+        //Constructor
+        TRestFindResponseSignalProcess();
+        TRestFindResponseSignalProcess( char *cfgFileName );
+        //Destructor
+        ~TRestFindResponseSignalProcess();
+
+        ClassDef(TRestFindResponseSignalProcess, 1);      // Template for a REST "event process" class inherited from TRestEventProcess
+};
+#endif
+
