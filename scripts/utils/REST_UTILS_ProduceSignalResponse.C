957bdb1233d47514efa7b253decbb2068b222a41
Javier Galan <j.a.galan.81@gmail.com>
2017-03-04 09:36:35 +0800

    REST_UTILS_ProduceSignalResponse modified to get as input a TRestRawSignalEvent.
    Former-commit-id: c51016a2e816bbcc5fc57eebeefde6cf8c1f22c0

--- scripts/utils/REST_UTILS_ProduceSignalResponse.C
+++ scripts/utils/REST_UTILS_ProduceSignalResponse.C
@@ -1,69 +1,57 @@
+Int_t minTime = 156;
+Int_t maxTime = 159;
 
 Int_t REST_UTILS_ProduceSignalResponse( TString inputFileName, TString outputFileName, Int_t nPoints = 512, Double_t threshold = 1 )
 {
     TRestRun *run = new TRestRun();
     run->OpenInputFile( inputFileName );
 
-    TRestSignalEvent *evt = new TRestSignalEvent();
+    TRestRawSignalEvent *evt = new TRestRawSignalEvent();
     run->PrintAllMetadata();
     run->SetInputEvent( evt );
 
-    TRestSignal *averageSignal = new TRestSignal();
-    averageSignal->Initialize();
-    for( int i = 0; i < nPoints; i++ )
-        averageSignal->NewPoint( i, 0 );
+    Double_t dataArray[512];
+    for( int n = 0; n < 512; n++ )
+		dataArray[n] = 0;
 
-    Int_t n = 0;
+    Int_t N = 0;
     for( int i = 0; i < run->GetEntries(); i++ )
     {
         run->GetEntry(i);
         if( evt->GetNumberOfSignals() == 1 )
         {
-            TRestSignal *s = evt->GetSignal(0);
-            if ( s->GetMaxPeakValue() > 2500 )
+            TRestRawSignal *s = evt->GetSignal(0);
+            Int_t maxPeakBin = s->GetMaxPeakBin();
+	    if( maxPeakBin > minTime && maxPeakBin < maxTime )
             {
-                averageSignal->SignalAddition( s );
-                n++;
+		if( s->GetNumberOfPoints() != 512 )
+		{
+			cout << "Warning. Number of points is not 512" << endl;
+			continue;
+		}
+
+		for( int n = 0; n < s->GetNumberOfPoints(); n++ )
+		{
+			dataArray[n] += (Double_t) s->GetData(n);
+		}
+                N++;
             }
-
-        }
-    }
-    Double_t factor = 1. / ((double) n);
-    averageSignal->MultiplySignalBy( factor );
-
-    Double_t baseline = averageSignal->SubstractBaseline( 20, 100 );
-
-    Double_t peakTime = averageSignal->GetMaxPeakTime();
-    cout << "Peak time : " << peakTime << endl;
-
-    Int_t continueLoop = 1;
-    Int_t startTime = 0;
-    for( int i = peakTime; i > 0 && continueLoop; i-- )
-    {
-        if( averageSignal->GetData( i ) <= threshold )
-        {
-            startTime = i;
-            continueLoop = 0;
         }
     }
 
-    continueLoop = 1;
-    Int_t endTime = 0;
-    for( int i = peakTime; i < nPoints && continueLoop; i++ )
-    {
-        if( averageSignal->GetData( i ) <= threshold )
-        {
-            endTime = i;
-            continueLoop = 0;
-        }
-    }
+    for( int n = 0; n < 512; n++ )
+	cout << "n : " << n << " Data : " << dataArray[n]/N << endl;
 
     TRestSignal *responseSignal = new TRestSignal();
     responseSignal->Initialize();
-    for( int i = startTime; i <= endTime; i++ )
-        responseSignal->NewPoint( i, averageSignal->GetData(i) );
+
+    dataArray[0] = dataArray[1];
+
+    for( int i = 0; i < 512; i++ )
+        responseSignal->NewPoint( (Float_t) i, (Float_t) (dataArray[i]/N) );
 
     Double_t integral = responseSignal->GetIntegral();
+
     responseSignal->MultiplySignalBy( 1./integral );
 
     cout << "Normalized integral : " << responseSignal->GetIntegral() << endl;
@@ -71,10 +59,10 @@ Int_t REST_UTILS_ProduceSignalResponse( TString inputFileName, TString outputFil
     TFile *f = new TFile( outputFileName, "RECREATE" );
     f->cd();
     responseSignal->Write("signal Response");
-    responseSignal->GetGraph()->Draw();
+ //   responseSignal->GetGraph()->Draw();
 
     f->Close();
 
     cout << "Number of entries : " << run->GetEntries() << endl;
-    cout << "Number of good signals : " << n << endl;
+    cout << "Number of good signals : " << N << endl;
 }
