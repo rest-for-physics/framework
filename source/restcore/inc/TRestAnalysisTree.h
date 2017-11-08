d966b7478b5d178b20d32622cc6726c49cc63f65
nkx Nicholas <bur_ning@outlook.com>
2017-11-08 23:15:22 +0800

    Changed TRestAnalysisTree
    Former-commit-id: 7be58459f978c8a26eded30a89c4abdb9cf7c186

--- source/restcore/inc/TRestAnalysisTree.h
+++ source/restcore/inc/TRestAnalysisTree.h
@@ -22,101 +22,143 @@
 #include "TRestEvent.h"
 
 #include "TTree.h"
+class TRestBranch :public TBranch {
+public:
+	void SetBranchAddressQuick(char* addr) {
+		fAddress = addr;
+	}
+};
 
-class TRestAnalysisTree:public TTree {
+class TRestAnalysisTree :public TTree {
 
-    private:
-
-        void Initialize();
+private:
 
 #ifndef __CINT__
-        Bool_t fConnected;
-        Bool_t fBranchesCreated;
+	Bool_t fConnected;//!
+	Bool_t fBranchesCreated;//!
 
-        Int_t fEventID;
-        Int_t fSubEventID;
-        Double_t fTimeStamp;
-        TString *fSubEventTag;
-        Int_t fRunOrigin;
-        Int_t fSubRunOrigin;
+	Int_t fEventID;//!
+	Int_t fSubEventID;//!
+	Double_t fTimeStamp;//!
+	TString *fSubEventTag;//!
+	Int_t fRunOrigin;//!
+	Int_t fSubRunOrigin;//!
 
-        std::vector <Double_t> fObservableValues;
+	std::vector <Double_t*> fObservableValues;//!
 #endif
 
-        Int_t fNObservables;
-        std::vector <TString> fObservableNames;
-        std::vector <TString> fObservableDescriptions;
+	Int_t fNObservables;
+	std::vector <TString> fObservableNames;
+	std::vector <TString> fObservableDescriptions;
 
-    protected:
-    public:
+protected:
+public:
+	void Initialize();
 
-        Int_t GetObservableID( TString obsName )
-        {
-            for( int i = 0; i < GetNumberOfObservables( ); i++ )
-                if( fObservableNames[i] == obsName ) return i;
-            return -1;
-        }
+	Int_t GetObservableID(TString obsName)
+	{
+		for (int i = 0; i < GetNumberOfObservables(); i++)
+			if (fObservableNames[i] == obsName) return i;
+		return -1;
+	}
 
-        Bool_t ObservableExists( TString obsName )
-        {
-            if( GetObservableID( obsName ) >= 0 ) return true;
-            return false;
-        }
+	Bool_t ObservableExists(TString obsName)
+	{
+		if (GetObservableID(obsName) >= 0) return true;
+		return false;
+	}
 
-        Bool_t isConnected( ) { return fConnected; }
+	Bool_t isConnected() { return fConnected; }
 
-        Int_t GetEventID( ) { return fEventID; }
-        Int_t GetSubEventID( ) { return fSubEventID; }
-        Double_t GetTimeStamp( ) { return fTimeStamp; }
-        TString GetSubEventTag( ) { return *fSubEventTag; }
-        Int_t GetRunOrigin( ) { return fRunOrigin; }
-        Int_t GetSubRunOrigin( ) { return fSubRunOrigin; }
+	Int_t GetEventID() { return fEventID; }
+	Int_t GetSubEventID() { return fSubEventID; }
+	Double_t GetTimeStamp() { return fTimeStamp; }
+	TString GetSubEventTag() { return *fSubEventTag; }
+	Int_t GetRunOrigin() { return fRunOrigin; }
+	Int_t GetSubRunOrigin() { return fSubRunOrigin; }
 
-        Int_t GetNumberOfObservables( ) { return fNObservables; }
-        TString GetObservableName( Int_t n ) { return fObservableNames[n]; } // TODO implement error message in case n >= fNObservables
-        TString GetObservableDescription( Int_t n ) { return fObservableDescriptions[n]; }
-        Double_t GetObservableValue( Int_t n ) { return fObservableValues[n]; } // TODO implement error message in case n >= fNObservables
+	Int_t GetNumberOfObservables() { return fNObservables; }
+	TString GetObservableName(Int_t n) { return fObservableNames[n]; } // TODO implement error message in case n >= fNObservables
+	TString GetObservableDescription(Int_t n) { return fObservableDescriptions[n]; }
+	Double_t GetObservableValue(Int_t n) { return *fObservableValues[n]; } // TODO implement error message in case n >= fNObservables
 
-	Double_t GetObservableValue ( TString obsName ) { return this->GetObservableValue( this->GetObservableID( obsName ) ); }
+	Double_t GetObservableValue(TString obsName) { return this->GetObservableValue(this->GetObservableID(obsName)); }
 
-        void SetObservableValue( Int_t n, Double_t value ) {  fObservableValues[n] = value; }
+	void SetObservableValue(Int_t n, Double_t value) { *fObservableValues[n] = value; }
 
-        void SetObservableValue( TString obsName, Double_t value )
-        {
-            Int_t id =  GetObservableID( obsName);
-            if( id >= 0 ) SetObservableValue( id, value );
-        }
+	void SetObservableValue(TString obsName, Double_t value)
+	{
+		Int_t id = GetObservableID(obsName);
+		if (id >= 0) SetObservableValue(id, value);
+	}
 
-        void CreateEventBranches( );
-        void CreateObservableBranches( );
+	void CreateEventBranches();
+	void CreateObservableBranches();
 
-        void ConnectEventBranches( );
-        void ConnectObservables( );
+	void ConnectObservables(TRestAnalysisTree * from);
 
-        void PrintObservables( )
-        {
-            std::cout << "Run origin : " << GetRunOrigin() << std::endl;
-            std::cout << "Sub run origin : " << GetSubRunOrigin() << std::endl;
-            std::cout << "Event ID : " << GetEventID() << std::endl;
-            std::cout << "Event Time : " << GetTimeStamp() << std::endl;
-            std::cout << "-----------------------------------------" << std::endl;
-            for( int n = 0; n < GetNumberOfObservables(); n++ )
-                std::cout << "Observable Name : " << fObservableNames[n] << "    Value : " << fObservableValues[n] << std::endl;
-            std::cout << std::endl;
+	void ConnectEventBranches()
+	{
+		TBranch *br1 = GetBranch("eventID");
+		br1->SetAddress(&fEventID);
 
-        }
-        
-        Int_t FillEvent( TRestEvent *evt );
-        
-        Int_t AddObservable( TString observableName, TString description = "" );
-        
-        //Construtor
-        TRestAnalysisTree();
-        TRestAnalysisTree( TString name, TString title );
-        //Destructor
-        virtual ~ TRestAnalysisTree();
+		TBranch *br2 = GetBranch("subEventID");
+		br2->SetAddress(&fSubEventID);
+
+		TBranch *br3 = GetBranch("timeStamp");
+		br3->SetAddress(&fTimeStamp);
+
+		TBranch *br4 = GetBranch("subEventTag");
+		br4->SetAddress(&fSubEventTag);
+
+		TBranch *br5 = GetBranch("runOrigin");
+		br5->SetAddress(&fRunOrigin);
+
+		TBranch *br6 = GetBranch("subRunOrigin");
+		br6->SetAddress(&fSubRunOrigin);
+	}
+
+	void ConnectObservables()
+	{
+		for (int i = 0; i < GetNumberOfObservables(); i++)
+		{
+			Double_t x = 0;
+			fObservableValues.push_back(&x);
+		}
+
+		for (int i = 0; i < GetNumberOfObservables(); i++)
+		{
+			TBranch*branch = GetBranch(fObservableNames[i]);
+			branch->SetAddress(fObservableValues[i]);
+		}
+		fConnected = true;
+
+	}
+
+	void PrintObservables()
+	{
+		std::cout << "Run origin : " << GetRunOrigin() << std::endl;
+		std::cout << "Sub run origin : " << GetSubRunOrigin() << std::endl;
+		std::cout << "Event ID : " << GetEventID() << std::endl;
+		std::cout << "Event Time : " << GetTimeStamp() << std::endl;
+		std::cout << "-----------------------------------------" << std::endl;
+		for (int n = 0; n < GetNumberOfObservables(); n++)
+			std::cout << "Observable Name : " << fObservableNames[n] << "    Value : " << fObservableValues[n] << std::endl;
+		std::cout << std::endl;
+
+	}
+
+	Int_t FillEvent(TRestEvent *evt);
+
+	Int_t AddObservable(TString observableName, Double_t* observableValue, TString description = "");
+
+	//Construtor
+	TRestAnalysisTree();
+	TRestAnalysisTree(TString name, TString title);
+	//Destructor
+	virtual ~TRestAnalysisTree();
 
 
-        ClassDef(TRestAnalysisTree, 1);     // REST run class
+	ClassDef(TRestAnalysisTree, 1);     // REST run class
 };
 #endif
--- source/restcore/src/TRestAnalysisTree.cxx
+++ source/restcore/src/TRestAnalysisTree.cxx
@@ -21,127 +21,120 @@ using namespace std;
 
 
 ClassImp(TRestAnalysisTree)
- 
+
 //______________________________________________________________________________
-TRestAnalysisTree::TRestAnalysisTree() : TTree( )
+TRestAnalysisTree::TRestAnalysisTree() : TTree()
 {
-    SetName( "TRestAnalysisTree" );
-    SetTitle( "Analysis tree" );
+	SetName("TRestAnalysisTree");
+	SetTitle("Analysis tree");
 
-    Initialize( );
+	Initialize();
 }
 
-TRestAnalysisTree::TRestAnalysisTree( TString name, TString title ) : TTree( name, title )
+TRestAnalysisTree::TRestAnalysisTree(TString name, TString title) : TTree(name, title)
 {
-    SetName( name );
-    SetTitle( title );
+	SetName(name);
+	SetTitle(title);
 
-    Initialize( );
+	Initialize();
 }
 
-void TRestAnalysisTree::Initialize( )
+void TRestAnalysisTree::Initialize()
 {
 
-    fSubEventTag = new TString();
+	fSubEventTag = new TString();
 
-    fNObservables = 0;
+	fNObservables = 0;
 
-    fConnected = false;
-    fBranchesCreated = false;
-
-}
-
-void TRestAnalysisTree::ConnectEventBranches( )
-{
-    TBranch *br1 = GetBranch( "eventID" );
-    br1->SetAddress( &fEventID );
-
-    TBranch *br2 = GetBranch( "subEventID" );
-    br2->SetAddress( &fSubEventID );
-
-    TBranch *br3 = GetBranch( "timeStamp" );
-    br3->SetAddress( &fTimeStamp );
-
-    TBranch *br4 = GetBranch( "subEventTag" );
-    br4->SetAddress( &fSubEventTag );
-
-    TBranch *br5 = GetBranch( "runOrigin" );
-    br5->SetAddress( &fRunOrigin );
-
-    TBranch *br6 = GetBranch( "subRunOrigin" );
-    br6->SetAddress( &fSubRunOrigin );
-}
-
-void TRestAnalysisTree::ConnectObservables( )
-{
-    TBranch *branch[fNObservables];
-    for( int i = 0; i < GetNumberOfObservables(); i++ )
-    {
-        Double_t x = 0;
-        fObservableValues.push_back( x );
-    }
-
-    for( int i = 0; i < GetNumberOfObservables(); i++ )
-    {
-        branch[i] = GetBranch( fObservableNames[i] );
-        branch[i]->SetAddress( &fObservableValues[i] );
-    }
-    fConnected = true;
+	fConnected = false;
+	fBranchesCreated = false;
 
 }
 
-Int_t TRestAnalysisTree::AddObservable( TString observableName, TString description )
-{
-    if( fBranchesCreated )
-    {
-        cout << "REST ERROR : Branches have been already created" << endl; 
-        cout << "No more observables can be added" << endl;
-        return -1;
-    }
-    Double_t x = 0;
-    fObservableNames.push_back( observableName );
-    fObservableDescriptions.push_back( description );
-    fObservableValues.push_back( x );
 
-    fNObservables++;
-    return fNObservables-1;
+void TRestAnalysisTree::ConnectObservables(TRestAnalysisTree * from)
+{
+	if (from != NULL) {
+		fNObservables = from->GetNumberOfObservables();
+		fObservableValues.clear();
+		fObservableNames.clear();
+		fObservableDescriptions.clear();
+
+		for (int i = 0; i < GetNumberOfObservables(); i++)
+		{
+			fObservableValues.push_back(new double(0));
+			fObservableNames.push_back("old_" + from->GetObservableName(i));
+			fObservableDescriptions.push_back(from->GetObservableDescription(i));
+		}
+
+
+		fConnected = true;
+	}
+
+
 }
 
-Int_t TRestAnalysisTree::FillEvent( TRestEvent *evt )
+Int_t TRestAnalysisTree::AddObservable(TString observableName, Double_t* observableValue, TString description)
 {
-    fEventID = evt->GetID( );
-    fSubEventID = evt->GetSubID( );
-    fTimeStamp = evt->GetTimeStamp( ).AsDouble();
-    *fSubEventTag = evt->GetSubEventTag( );
-    fRunOrigin = evt->GetRunOrigin();
-    fSubRunOrigin = evt->GetSubRunOrigin();
+	if (fBranchesCreated)
+	{
+		cout << "REST ERROR : Branches have been already created" << endl;
+		cout << "No more observables can be added" << endl;
+		return -1;
+	}
+	Double_t x = 0;
+	if (GetObservableID(observableName) == -1) {
+		fObservableNames.push_back(observableName);
+		fObservableDescriptions.push_back(description);
+		fObservableValues.push_back(observableValue);
 
-    return this->Fill( );
+		fNObservables++;
+	}
+	else
+	{
+		cout << "observable \"" << observableName << "\" has already been created! skipping" << endl;
+		return -1;
+	}
+
+	return fNObservables - 1;
 }
 
-void TRestAnalysisTree::CreateEventBranches( )
+Int_t TRestAnalysisTree::FillEvent(TRestEvent *evt)
 {
-    Branch( "runOrigin", &fRunOrigin );
-    Branch( "subRunOrigin", &fSubRunOrigin );
-    Branch( "eventID", &fEventID );
-    Branch( "subEventID", &fSubEventID );
-    Branch( "timeStamp", &fTimeStamp );
-    Branch( "subEventTag", fSubEventTag );
+	fEventID = evt->GetID();
+	fSubEventID = evt->GetSubID();
+	fTimeStamp = evt->GetTimeStamp().AsDouble();
+	*fSubEventTag = evt->GetSubEventTag();
+	fRunOrigin = evt->GetRunOrigin();
+	fSubRunOrigin = evt->GetSubRunOrigin();
+
+	return this->Fill();
 }
 
-void TRestAnalysisTree::CreateObservableBranches( )
+void TRestAnalysisTree::CreateEventBranches()
 {
-    if( fBranchesCreated ) 
-    {
-        cout << "REST ERROR : Branches have been already created" << endl; 
-        exit(1); 
-    }
-
-    for( int n = 0; n < GetNumberOfObservables(); n++ )
-        Branch( fObservableNames[n], (Double_t *) &fObservableValues[n] );
-
-    fBranchesCreated = true;
+	Branch("runOrigin", &fRunOrigin);
+	Branch("subRunOrigin", &fSubRunOrigin);
+	Branch("eventID", &fEventID);
+	Branch("subEventID", &fSubEventID);
+	Branch("timeStamp", &fTimeStamp);
+	Branch("subEventTag", fSubEventTag);
 }
+
+void TRestAnalysisTree::CreateObservableBranches()
+{
+	if (fBranchesCreated)
+	{
+		cout << "REST ERROR : Branches have been already created" << endl;
+		exit(1);
+	}
+
+	for (int n = 0; n < GetNumberOfObservables(); n++)
+		Branch(fObservableNames[n], fObservableValues[n]);
+
+	fBranchesCreated = true;
+}
+
 
 
 //______________________________________________________________________________
