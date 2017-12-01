e789c38d2fc9b89c1b18d28301b2a9399d6068d6
Javier Galan <j.a.galan.81@gmail.com>
2017-12-01 15:33:32 +0100

    REST_VIEWER_HitsEvent now uses generic viewer and not EveViewer.
    Former-commit-id: c5171f615654118f4b38c4bf342763a419aaed8c

--- scripts/viewers/REST_VIEWER_HitsEvent.C
+++ scripts/viewers/REST_VIEWER_HitsEvent.C
@@ -1,20 +1,16 @@
 
+REST_VIEWER_HitsEvent( TString fName=" ",  TString option = " " )
+{
+    TRestBrowser *browser = new TRestBrowser( );
 
+    TRestGenericEventViewer *viewer = new TRestGenericEventViewer( );
 
+    TRestHitsEvent *hEvent = new TRestHitsEvent();
 
+    viewer->SetOption( option );
+    viewer->SetEvent(hEvent);
 
-REST_VIEWER_HitsEvent(TString fName=" "){
+    browser->SetViewer( viewer );
 
-TRestBrowser *browser = new TRestBrowser( );
-
-TRestHitsEventViewer *viewer = new TRestHitsEventViewer( );
-browser->SetViewer( viewer );
-
-browser->OpenFile(fName);
-
+    browser->OpenFile(fName);
 }
-
-
-
-
-
