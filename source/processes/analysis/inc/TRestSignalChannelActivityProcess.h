13688d80ae128d439d1232ffd8990d7f69955ca8
Javier Galan <j.a.galan.81@gmail.com>
2018-03-28 21:02:39 +0200

    New process TRestSignalChannelActivityProcess added.
    Former-commit-id: 39990b2a06ea13a95aa6a3b351c3a02ce90bd9b2

---
+++ source/processes/analysis/inc/TRestSignalChannelActivityProcess.h
@@ -0,0 +1,84 @@
+///______________________________________________________________________________
+///______________________________________________________________________________
+///______________________________________________________________________________
+///             
+///
+///             RESTSoft : Software for Rare Event Searches with TPCs
+///
+///             TRestSignalChannelActivityProcess.h
+///
+///_______________________________________________________________________________
+
+
+#ifndef RestCore_TRestSignalChannelActivityProcess
+#define RestCore_TRestSignalChannelActivityProcess
+
+#include <TH1D.h>
+
+#include <TRestReadout.h>
+#include <TRestSignalEvent.h>
+
+#include "TRestEventProcess.h"
+
+class TRestSignalChannelActivityProcess:public TRestEventProcess {
+    private:
+#ifndef __CINT__
+        TRestSignalEvent *fSignalEvent;
+        TRestReadout *fReadout;
+
+#endif
+        void InitFromConfigFile();
+
+        void Initialize();
+
+        void LoadDefaultConfig();
+
+    protected:
+        
+        //add here the members of your event process
+        //
+        Double_t fLowThreshold;
+        Double_t fHighThreshold;
+
+        TH1D *fDaqChannelsHisto;
+
+        TH1D *fReadoutChannelsHisto_OneSignal;
+        TH1D *fReadoutChannelsHisto_OneSignal_High;
+        TH1D *fReadoutChannelsHisto_TwoSignals;
+        TH1D *fReadoutChannelsHisto_TwoSignals_High;
+        TH1D *fReadoutChannelsHisto_ThreeSignals;
+        TH1D *fReadoutChannelsHisto_ThreeSignals_High;
+        TH1D *fReadoutChannelsHisto_MultiSignals;
+        TH1D *fReadoutChannelsHisto_MultiSignals_High;
+
+    public:
+        void InitProcess();
+        void BeginOfEventProcess(); 
+        TRestEvent *ProcessEvent( TRestEvent *eventInput );
+        void EndOfEventProcess(); 
+        void EndProcess();
+
+        void LoadConfig( std::string cfgFilename, std::string name = "" );
+
+        void PrintMetadata() { 
+
+            BeginPrintProcess();
+
+            cout << "Low signal threshold activity : " << fLowThreshold << endl;
+            cout << "High signal threshold activity : " << fHighThreshold << endl;
+
+            EndPrintProcess();
+        }
+
+        TString GetProcessName() { return (TString) "signalChannelActivity"; }
+
+        //Constructor
+        TRestSignalChannelActivityProcess();
+        TRestSignalChannelActivityProcess( char *cfgFileName );
+        //Destructor
+        ~TRestSignalChannelActivityProcess();
+
+        ClassDef(TRestSignalChannelActivityProcess, 1);      // Template for a REST "event process" class inherited from TRestEventProcess
+};
+#endif
+
