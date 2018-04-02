c78046840f3b0e54e06117a2e3d702a52f065a93
nkx Nicholas <bur_ning@outlook.com>
2018-04-02 13:06:06 +0800

    TRestMetadata: GetFieldValue returns "Not defined" if not found
    Former-commit-id: 253fc7da7fe8895e228a543be71970dd08f4f301

--- source/restcore/src/TRestMetadata.cxx
+++ source/restcore/src/TRestMetadata.cxx
@@ -1026,7 +1026,7 @@ string TRestMetadata::GetParameter(std::string parName, TiXmlElement* e, TString
 ///
 std::string TRestMetadata::GetFieldValue(std::string parName, TiXmlElement* e)
 {
-	return GetParameter(parName, e, "");
+	return GetParameter(parName, e, "Not defined");
 }
 
 
