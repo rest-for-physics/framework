3e7edcc94faf150480d3ae5304b7e071479fc89a
echoliz <elisaruizcholiz@gmail.com>
2018-05-16 11:32:27 +0200

    Fixing a small bug in TRestRawSignal::GetTripleMaxIntegral
    Former-commit-id: 00bb23cf972d5d1476a87b65e510059134a88b33

--- source/events/general/src/TRestRawSignal.cxx
+++ source/events/general/src/TRestRawSignal.cxx
@@ -268,17 +268,23 @@ Int_t TRestRawSignal::GetRiseTime( )
 
 Double_t TRestRawSignal::GetTripleMaxIntegral( Int_t startBin, Int_t endBin )
 {
+	cout << "Signal id : " << GetID() << endl;
 	if( startBin < 0 ) startBin = 0;
 	if( endBin <= 0 || endBin > GetNumberOfPoints() ) endBin = GetNumberOfPoints();
+
+	cout << "Start bin : " << startBin << " end Bin : " << endBin << endl;
 
 	if( fThresholdIntegral == -1 )
 		cout << "REST Warning. TRestRawSignal::GetTripleMaxIntegral. GetIntegralWithThreshold should be called first." << endl;
 
 	if( fPointsOverThreshold.size() < 2 )
+	{
 		cout << "REST Warning. TRestRawSignal::GetTripleMaxIntegral. Points over threshold = " << fPointsOverThreshold.size() << endl;
-        return 0;
+	        return 0;
+	}
 
 	Int_t cBin = GetMaxPeakBin( startBin, endBin );
+	cout << "Center bin : " << cBin << endl;
 
 	if( cBin+1 >= GetNumberOfPoints() ) return 0;
 
@@ -286,6 +292,7 @@ Double_t TRestRawSignal::GetTripleMaxIntegral( Int_t startBin, Int_t endBin )
 	Double_t a2 = GetData( cBin-1 );
 	Double_t a3 = GetData( cBin+1 );
 
+	cout << "Signal triple addition : " << a1 + a2 + a3 << endl;
 	return a1 + a2 + a3;
 }
 
