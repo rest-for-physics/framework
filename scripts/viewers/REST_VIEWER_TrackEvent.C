dd148bf20332dbd10e2aeffb7728286ac0b69bc0
Javier Galan <j.a.galan.81@gmail.com>
2018-05-25 16:17:05 +0200

    REST_VIEWER_TrackEvent now accepts an option argument.
    Former-commit-id: 7a6d097a8cf4dceef049e74a4c76d3b4a276f261

--- scripts/viewers/REST_VIEWER_TrackEvent.C
+++ scripts/viewers/REST_VIEWER_TrackEvent.C
@@ -1,11 +1,13 @@
 
-REST_VIEWER_TrackEvent(TString fName=" "){
+REST_VIEWER_TrackEvent(TString fName=" ", TString option = "" ){
 
 TRestBrowser *browser = new TRestBrowser( );
 
 TRestGenericEventViewer *viewer = new TRestGenericEventViewer( );
 TRestTrackEvent *tEvent = new TRestTrackEvent();
 viewer->SetEvent(tEvent);
+
+viewer->SetOption( option );
 
 browser->SetViewer( viewer );
 
