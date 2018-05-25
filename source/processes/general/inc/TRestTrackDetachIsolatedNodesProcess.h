8e5cd7a0073ba143f2a670569762f5398bc35d1a
Javier Galan <j.a.galan.81@gmail.com>
2018-05-25 09:57:30 +0200

    TRestTrackDetachIsolatedNodesProcess added.
    Former-commit-id: 20cf7ff1c1008b419c80b96930520855de546e0b

---
+++ source/processes/general/inc/TRestTrackDetachIsolatedNodesProcess.h
@@ -0,0 +1,70 @@
+//////////////////////////////////////////////////////////////////////////
+///             
+///
+///             RESTSoft : Software for Rare Event Searches with TPCs
+///
+///             TRestTrackDetachIsolatedNodesProcess.h
+///
+///              Jan 2016 : Javier Galan
+///
+//////////////////////////////////////////////////////////////////////////
+
+
+#ifndef RestCore_TRestTrackDetachIsolatedNodesProcess
+#define RestCore_TRestTrackDetachIsolatedNodesProcess
+
+#include <TRestTrackEvent.h>
+#include "TRestEventProcess.h"
+
+class TRestTrackDetachIsolatedNodesProcess:public TRestEventProcess {
+
+    private:
+
+#ifndef __CINT__
+        TRestTrackEvent *fInputTrackEvent;
+        TRestTrackEvent *fOutputTrackEvent;
+#endif
+
+        void InitFromConfigFile();
+
+        void Initialize();
+
+        Double_t fThresholdDistance;
+        Double_t fConnectivityThreshold;
+
+        Double_t fTubeLengthReduction;
+        Double_t fTubeRadius;
+
+    protected:
+
+
+    public:
+
+        void InitProcess();
+        void BeginOfEventProcess(); 
+        TRestEvent *ProcessEvent( TRestEvent *eventInput );
+        void EndOfEventProcess(); 
+        void EndProcess();
+        void LoadDefaultConfig( );
+
+        void LoadConfig( std::string cfgFilename, std::string name = "" );
+
+        void PrintMetadata() 
+        { 
+            BeginPrintProcess();
+
+            EndPrintProcess();
+        }
+
+        TString GetProcessName() { return (TString) "trackDetachIsolatedNode"; }
+
+        //Constructor
+        TRestTrackDetachIsolatedNodesProcess();
+        TRestTrackDetachIsolatedNodesProcess( char *cfgFileName );
+        //Destructor
+        ~TRestTrackDetachIsolatedNodesProcess();
+
+        ClassDef( TRestTrackDetachIsolatedNodesProcess, 1);
+};
+#endif
+
