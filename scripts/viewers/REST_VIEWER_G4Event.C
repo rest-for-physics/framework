8ac3fb718bf7b0ceaa794dbd029cd923c6da9e52
Javier Galan <j.a.galan.81@gmail.com>
2018-06-20 15:43:39 +0200

    Viewer scripts updated to accept redefinition of canvas size.
    Former-commit-id: d392a7a2ab0fc64c4279faa1a6f02062cafaed81

--- scripts/viewers/REST_VIEWER_G4Event.C
+++ scripts/viewers/REST_VIEWER_G4Event.C
@@ -3,14 +3,28 @@
 
 
 
-REST_VIEWER_G4Event(TString fName=" "){
+REST_VIEWER_G4Event(TString fName=" ", TString option = "", Int_t canvasWidth = 800, Int_t canvasHeight = 600 ){
 
-TRestBrowser *browser = new TRestBrowser( );
+    TRestBrowser *browser = new TRestBrowser( );
 
-TRestG4EventViewer *viewer = new TRestG4EventViewer( );
-browser->SetViewer( viewer );
+    if( option == "eveViewer" )
+    {
+        TRestG4EventViewer *viewer = new TRestG4EventViewer( );
+        browser->SetViewer( viewer );
+    }
+    else
+    {
 
-browser->OpenFile(fName);
+        TRestGenericEventViewer *viewer = new TRestGenericEventViewer( canvasWidth, canvasHeight );
+        TRestG4Event *tEvent = new TRestG4Event();
+        viewer->SetEvent(tEvent);
+
+        viewer->SetOption( option );
+
+        browser->SetViewer( viewer );
+    }
+
+    browser->OpenFile(fName);
 
 }
 
