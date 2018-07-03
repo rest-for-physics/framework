cdf305a53d1cb201f9e90582c89f23ebee5bd367
nkx Nicholas <bur_ning@outlook.com>
2018-07-03 21:57:25 +0800

    TRestDataBase: add several methods
    Former-commit-id: 614f19ceba3e9a16b80916638a14b95d495a01c6

--- source/tools/resttools/inc/TRestDataBase.h
+++ source/tools/resttools/inc/TRestDataBase.h
@@ -5,50 +5,97 @@
 #include "TClass.h"
 #include <map>
 #include <vector>
+#include <time.h>  
 
 using namespace std;
+
+class DataBaseFileInfo {
+public:
+	DataBaseFileInfo() {
+		fileSize = 0;
+		evtRate = 0;
+		for (int i = 0; i < 41; i++) {
+			sha1sum[i] = 0;
+		}
+		quality = true;
+		start = 0;
+		stop = 0;
+	}
+
+	DataBaseFileInfo(string filename);
+
+	void Print();
+
+	Long64_t fileSize;
+	double evtRate;
+	char sha1sum[41];//last bit is \0
+	bool quality;
+	time_t start;
+	time_t stop;
+};
 
 class TRestDataBase {
 public:
 	TRestDataBase() {}
 	~TRestDataBase() {}
 
-	static TRestDataBase* instantiate() {
-		TClass*c = TClass::GetClass("TRestDataBaseImpl");
-		if (c != NULL)//this means we have the package installed
-		{
-			return (TRestDataBase*)c->New();
-		}
-		return NULL;
-	}
-	virtual int test() { return 0; }
+	static TRestDataBase* instantiate();
+	virtual void test() {}
+	virtual void print(int runnumber, int subrun = 0) {}
+	virtual void exec(string cmd) {}
 
-	virtual string query_file(int runnumber) { return ""; }
-	virtual double query_start(int runnumber) { return 0; }
-	virtual double query_end(int runnumber) { return 0; }
-	virtual string query_type(int runnumber) { return ""; }
-	virtual string query_user(int runnumber) { return ""; }
-	virtual string query_tag(int runnumber) { return ""; }
-	virtual string query_description(int runnumber) { return ""; }
-	virtual map<string, string> query_config(int runnumber) { return map<string,string>(); }
+	virtual int query_run(int runnumber, int subrun = 0) { return runnumber; }
+	virtual int query_subrun(int runnumber, int subrun = 0) { return subrun; }
+	virtual vector<string> query_files(int runnumber, int subrun = 0) { return vector<string>(0); }
+	virtual string query_file(int runnumber, int subrun = 0, int fileid = 0) { return ""; }
+	virtual string query_filepattern(int runnumber, int subrun = 0) { return ""; }
+	virtual DataBaseFileInfo query_fileinfo(int runnumber, int subrun = 0, string filename = "") { return DataBaseFileInfo(); }
+	virtual DataBaseFileInfo query_fileinfo(int runnumber, int subrun = 0, int fileid = 0) { return DataBaseFileInfo(); }
+	virtual double query_start(int runnumber, int subrun = 0) { return 0; }
+	virtual double query_end(int runnumber, int subrun = 0) { return 0; }
+	virtual string query_type(int runnumber, int subrun = 0) { return ""; }
+	virtual string query_user(int runnumber, int subrun = 0) { return ""; }
+	virtual string query_tag(int runnumber, int subrun = 0) { return ""; }
+	virtual string query_description(int runnumber, int subrun = 0) { return ""; }
+	virtual string query_version(int runnumber, int subrun = 0) { return ""; }
 
-	virtual vector<int> getrunswithintime(double t1, double t2) { return vector<int>(0); }
-	virtual vector<int> getrunswithtag(string tag) { return vector<int>(0); }
-	virtual int getrunnumber(string filename) { return 0; }
-	virtual int getnumberofruns() { return 0; }
+	virtual vector<pair<int, int>> search_filepattern(string filepattern) { return vector<pair<int, int>>(0); }
+	virtual vector<pair<int, int>> search_withintime(time_t t1, time_t t2) { return vector<pair<int, int>>(0); }
+	virtual vector<pair<int, int>> search_tag(string tag) { return vector<pair<int, int>>(0); }
+	virtual vector<pair<int, int>> search_user(string user) { return vector<pair<int, int>>(0); }
+	virtual vector<pair<int, int>> search_type(string type) { return vector<pair<int, int>>(0); }
+	virtual vector<pair<int, int>> search_description(string description) { return vector<pair<int, int>>(0); }
+	virtual vector<pair<int, int>> search_version(string version) { return vector<pair<int, int>>(0); }
+	virtual vector<pair<int, int>> searchexp(string expresstion) { return vector<pair<int, int>>(0); }
 
-	virtual int newrun() { return 0; }
+	virtual pair<int, int> getrunwithfilename(string filename) { return pair<int, int>(); }
+	virtual int getlastrun() { return 0; }
+	virtual int getlastsubrun(int runnumber) { return 0; }
+	virtual int getcurrentrun() { return fRunNumber; }
+	virtual int getcurrentsubrun() { return fSubRunNumber; }
 
-	virtual int set_runtype(int runnumber, string type) { return 0; }
-	virtual int set_runuser(int runnumber, string user) { return 0; }
-	virtual int set_runtag(int runnumber, string tag) { return 0; }
-	virtual int set_rundescription(int runnumber, string description) { return 0; }
-	virtual int set_runconfig(int runnumber, map<string, string> config) { return 0; }
+	virtual int new_run() { return 0; }
+	virtual int new_run(int runnumber) { return 0; }
+	virtual int new_runfile(string filename) { return new_runfile(filename, DataBaseFileInfo(filename)); }
+	virtual int new_runfile(string filename, DataBaseFileInfo info) { return 0; }
 
-	virtual int add_runconfig(int runnumber, string key, string value) { return 0; }
-	virtual int add_runfile(int runnumber, string filename) { return 0; }
+	virtual int set_runnumber(int runnumber) { return 0; }
+	virtual int set_subrun(int subrun) { return 0; }
+	virtual int set_type(string type) { return 0; }
+	virtual int set_user(string user) { return 0; }
+	virtual int set_tag(string tag) { return 0; }
+	virtual int set_description(string description) { return 0; }
+	virtual int set_version(string version) { return 0; }
+	virtual int set_runstart(double starttime) { return 0; }
+	virtual int set_runend(double endtime) { return 0; }
+	virtual int set_fileinfo(int fileid, DataBaseFileInfo info) { return 0; }
+	virtual int set_fileinfo(string filename, DataBaseFileInfo info) { return 0; }
 
+protected:
+	int fRunNumber = 0;
+	int fSubRunNumber = 0;
 };
+
 
 
 #endif
--- source/tools/resttools/src/TRestDataBase.cxx
+++ source/tools/resttools/src/TRestDataBase.cxx
@@ -1,3 +1,74 @@
 #include "TRestDataBase.h"
+#include "TRestTools.h"
+#include "TSystem.h"
+#include <sys/types.h>  
+#include <sys/stat.h>  
+#include <stdio.h>  
+#include <errno.h>  
+#include "TRestStringHelper.h"
 
-//dummy
+
+TRestDataBase* TRestDataBase::instantiate() {
+
+	vector <TString> list = TRestTools::GetListOfRESTLibraries();
+	for (unsigned int n = 0; n < list.size(); n++)
+	{
+		gSystem->Load(list[n]);
+	}
+
+	TClass*c = TClass::GetClass("TRestDataBaseImpl");
+	if (c != NULL)//this means we have the package installed
+	{
+		return (TRestDataBase*)c->New();
+	}
+	return NULL;
+}
+
+
+DataBaseFileInfo::DataBaseFileInfo(string filename) {
+
+	auto _fullname = ToAbsoluteName(filename);
+
+	struct stat buf;
+	int result = stat(_fullname.c_str(), &buf);
+
+	if (result != 0)
+	{
+		cout << "Failed to load file \"" << _fullname << "\"!" << endl;
+		fileSize = 0;
+		evtRate = 0;
+		quality = true;
+		start = 0;
+		stop = 0;
+		for (int i = 0; i < 41; i++) {
+			sha1sum[i] = 0;
+		}
+
+	}
+	else
+	{
+		fileSize = buf.st_size;
+		evtRate = 0;
+		quality = true;
+		start = buf.st_ctime;
+		stop = buf.st_atime;
+		string sha1result = ExecuteShellCommand("sha1sum " + _fullname);
+		string sha1 = Spilt(sha1result, " ")[0];
+		if (sha1.size() == 40) {
+			for (int i = 0; i < 40; i++) {
+				sha1sum[i] = sha1[i];
+			}
+			sha1sum[40] = 0;
+		}
+	}
+}
+
+void DataBaseFileInfo::Print() {
+	cout << "----DataBaseFileInfo struct----" << endl;
+	cout << "size: " << fileSize << endl;
+	cout << "event rate: " << evtRate << endl;
+	cout << "sha1sum: " << sha1sum << endl;
+	cout << "quality: " << quality << endl;
+	cout << "start time: " << ToDateTimeString(start) << endl;
+	cout << "stop time: " << ToDateTimeString(stop) << endl;
+}
