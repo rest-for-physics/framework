b52ebc6126290390a80b755b7f2061eb9e24fcba
Javier Galan <j.a.galan.81@gmail.com>
2018-09-18 17:18:00 +0200

    CMake. Linkdef automatic ROOT schema evolution solved
    Former-commit-id: 6be56bdcd623db2f6ef83cc8f8b76ec0f56789d3

--- cmake/MacroRootDict.cmake
+++ cmake/MacroRootDict.cmake
@@ -73,7 +73,14 @@ MACRO( GEN_ROOT_DICT_LINKDEF_HEADER _namespace )
     SET( _linkdef_header "${ROOT_DICT_OUTPUT_DIR}/${_namespace}_Linkdef.h" )
 
     FOREACH( _header ${_input_headers} )
-        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#pragma link C++ defined_in \\\"${_header}\\\"\\;\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#ifdef __CINT__\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#pragma link off all globals\\;\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#pragma link off all classes\\;\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#pragma link off all functions\\;\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#pragma link C++ nestedclasses\\;\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#pragma link C++ nestedclasses\\;\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#pragma link C++ class ${_namespace}\\+\\;\\\\n" )
+        SET( ${_namespace}_file_contents "${${_namespace}_file_contents}\\#endif\\\\n" )
     ENDFOREACH()
 
     ADD_CUSTOM_COMMAND(
