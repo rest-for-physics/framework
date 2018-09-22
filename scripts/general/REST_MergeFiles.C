0bbce5e3a9225d37ca030caaa6f7a610be56fe68
Javier Galan <j.a.galan.81@gmail.com>
2018-09-22 10:47:30 +0200

    REST_MergeFiles.C updated for compatibility.
    Former-commit-id: 092c64a2f1fc4b23642ff44f01b938167af37ce1

--- scripts/general/REST_MergeFiles.C
+++ scripts/general/REST_MergeFiles.C
@@ -2,14 +2,11 @@
 
 int REST_MergeFiles( TString pathAndPattern, TString outputFilename  )
 {
-    gSystem->Load("librestcore.so");
-    gSystem->Load("librestsim.so");
-
     TRestRunMerger *runMerger = new TRestRunMerger( pathAndPattern );
 
     runMerger->MergeFiles( outputFilename );
 
+    runMerger->PrintFilelist();
+
     delete runMerger;
-
-
 }
