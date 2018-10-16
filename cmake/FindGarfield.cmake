8145c099bda8126d65e65144a4c8cb0a1cb13f06
Javier Galan <j.a.galan.81@gmail.com>
2018-10-16 10:08:21 +0200

    cmake/FindGarfield.cmake. Fixed retrieval of GARFIELD_HOME env var.
    Former-commit-id: 491dfa19a898d00284f8b36a0aa21964ca65daa5

--- cmake/FindGarfield.cmake
+++ cmake/FindGarfield.cmake
@@ -12,7 +12,7 @@
 #set(Garfield_DIR "" CACHE PATH "Directory where Garfield is installed")
 #set(Garfield_INC_DIR "" CACHE PATH "Alternative directory for Garfield includes")
 #set(Garfield_LIB_DIR "" CACHE PATH "Alternative directory for Garfield libraries")
-set(Garfield_DIR ${GARFIELD_HOME})
+set(Garfield_DIR $ENV{GARFIELD_HOME})
 
 find_path(Garfield_INCLUDE_DIRS Sensor.hh
     HINTS ${Garfield_DIR}/include ${Garfield_INC_DIR}
