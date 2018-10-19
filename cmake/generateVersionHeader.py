5fa84d91bef0f787d23c271d5c65a658c6779694
Javier Galan <j.a.galan.81@gmail.com>
2018-10-19 17:34:14 +0200

    generateVersionHeader.py now updates TRestVersion only in case it is necessary to avoid unwanted recompilation
    Former-commit-id: 3b34311b5ad4306e1ce912d0c08c98205b7c6c12

--- cmake/generateVersionHeader.py
+++ cmake/generateVersionHeader.py
@@ -5,7 +5,7 @@
 # J. Galan - Javier.Galan.Lacarra@cern.ch
 # 8 - Oct - 2018
 
-import os,sys,re
+import os,sys,re,filecmp
 
 outputHeader = sys.argv[1]
 print " -- Generating TRestVersion.h"
@@ -47,7 +47,7 @@ codeC = c
 code = codeA + codeB + codeC
 
 
-f = open( outputHeader , "w")
+f = open( "TRestVersion.tmp" , "w")
 
 f.write("#ifndef REST_Version\n")
 f.write("#define REST_Version\n")
@@ -76,3 +76,7 @@ f.write("#define REST_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))\n")
 f.write("#endif\n")
 f.close()
 
+if filecmp.cmp( "TRestVersion.tmp", outputHeader ):
+    os.remove( "TRestVersion.tmp" )
+else:
+    os.rename( "TRestVersion.tmp", outputHeader )
