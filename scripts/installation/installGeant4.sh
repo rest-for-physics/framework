4b7178a49b361e0388d88c78b209d072fbd9d09c
jgalan <j.a.galan.81@gmail.com>
2018-10-31 11:52:56 +0800

    scripts/installGeant4.sh updated to Geant4 10.4.2
    Former-commit-id: d04f8c091d51f7375dc463b14815260e9c68c634

--- scripts/installation/installGeant4.sh
+++ scripts/installation/installGeant4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $(geant4-config --version | grep "10.4.1") ];then
+if [ $(geant4-config --version | grep "10.4.2") ];then
 
-echo Geant4 of version 10.4.1 has already been installed
+echo Geant4 of version 10.4.2 has already been installed
 echo prefix:
 echo $(geant4-config --prefix)
 echo cflags:
@@ -13,8 +13,8 @@ else
 set -e
 WP=$PWD
 
-G4_ROOT=geant4.10.04.p01
-G4_FILE=$G4_ROOT.tar
+G4_ROOT=geant4.10.04.p02
+G4_FILE=$G4_ROOT.tar.gz
 
 echo installing: $G4_ROOT
 
@@ -24,14 +24,14 @@ cd $HOME/apps/
 if [ -f $G4_FILE ]; then
     echo "use build/$G4_FILE"
 else
-    wget http://geant4.cern.ch/support/source/$G4_FILE
+    wget http://cern.ch/geant4-data/releases/$G4_FILE
 fi
 
 tar xvf $G4_FILE
 
-mkdir $G4_ROOT-build -p
+mkdir -p $G4_ROOT-build
 cd $G4_ROOT-build
-cmake ../$G4_ROOT -DGEANT4_USE_GDML=ON  -DGEANT4_USE_QT=ON -DCMAKE_INSTALL_PREFIX=$HOME/apps/$G4_ROOT-install -DGEANT4_INSTALL_DATA=ON -DGEANT4_INSTALL_DATA_TIMEOUT=7200
+cmake ../$G4_ROOT -DGEANT4_USE_GDML=ON -DGEANT4_USE_QT=OFF -DCMAKE_INSTALL_PREFIX=$HOME/apps/$G4_ROOT-install -DGEANT4_INSTALL_DATA=ON -DGEANT4_INSTALL_DATA_TIMEOUT=7200
 make -j4
 make install
 cd $WP
