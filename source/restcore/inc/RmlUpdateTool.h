4247078f4b79ec6839ab507ebd880aaf08457471
nkx Nicholas <bur_ning@outlook.com>
2018-11-08 14:56:24 +0800

    add a class: RmlUpdateTool
    Former-commit-id: fa38f461966379ee05b84771c54ec628684e38f2

---
+++ source/restcore/inc/RmlUpdateTool.h
@@ -0,0 +1,28 @@
+#include "TRestStringHelper.h"
+#include "TRestMetadata.h"
+#include "tinyxml.h"
+
+class RmlUpdateTool:public TRestMetadata {
+private:
+	string fFileName = "";//the input file name
+	string fOutputFileName = "";//the output file name
+	string fFileContent = "";//the whole content of input file
+	bool fStatus = false;
+
+protected:
+	void InitFromConfigFile() {}
+	bool UpdateSyntax();
+	bool UpdateStructure();
+	string WriteFile();
+
+public:
+	void Import(string Filename);
+	bool Is2_1();
+	bool UpdateSucceed() { return fStatus; }
+	bool Process();
+	string GetOutputFile() { return fOutputFileName; }
+
+	RmlUpdateTool();
+	RmlUpdateTool(string Filename, bool process = false);
+	ClassDef(RmlUpdateTool, 1);
+};
