be55aff7f5ed6d1219b75e9906e8fea47c4d936c
nkx Nicholas <bur_ning@outlook.com>
2019-04-22 23:39:15 +0800

    REST_ViewGeometry: It can now directly view the geometry of a gdml file

--- macros/viewers/REST_ViewGeometry.C
+++ macros/viewers/REST_ViewGeometry.C
@@ -2,60 +2,78 @@
 #include "TRestTask.h"
 #include "TRestG4EventViewer.h"
 #include "TRestBrowser.h"
+#include "GdmlPreprocessor.h"
 using namespace std;
 
 #ifndef RestTask_ViewGeometry
 #define RestTask_ViewGeometry
 
-Int_t REST_ViewGeometry( TString fName)
+Int_t REST_ViewGeometry(TString fName, TString option = "")
 {
 
-    TEveManager::Create();
-   
-	TRestStringOutput cout;
-    cout << "Filename : " << fName << endl;
-        
-    TFile *fFile = new TFile( fName );
-	TGeoManager *geo = NULL;
 
-	TIter nextkey(fFile->GetListOfKeys());
-	TKey *key;
-	while ((key = (TKey*)nextkey()) != NULL) {
-		if (key->GetClassName() == (TString)"TGeoManager") {
-			if (geo == NULL)
-				geo = (TGeoManager*)fFile->Get(key->GetName());
-			else if (key->GetName() == (TString)"Geometry")
-				geo = (TGeoManager*)fFile->Get(key->GetName());
+
+	TRestStringOutput cout;
+	cout << "Filename : " << fName << endl;
+
+	TGeoManager *geo = NULL;
+	if (isRootFile((string)fName)) {
+		TFile *fFile = new TFile(fName);
+
+		TIter nextkey(fFile->GetListOfKeys());
+		TKey *key;
+		while ((key = (TKey*)nextkey()) != NULL) {
+			if (key->GetClassName() == (TString)"TGeoManager") {
+				if (geo == NULL)
+					geo = (TGeoManager*)fFile->Get(key->GetName());
+				else if (key->GetName() == (TString)"Geometry")
+					geo = (TGeoManager*)fFile->Get(key->GetName());
+			}
 		}
 	}
+	else if (((string)fName).find(".gdml") != string::npos) {
+		GdmlPreprocessor* p = new GdmlPreprocessor();
+		p->Load((string)fName);
+		geo = p->CreateGeoM();
+	}
+	else {
+		cout << "File is not supported!" << endl;
+	}
 
-    TGeoNode* node = geo->GetTopNode();
+	if (option == "") {
+		geo->GetMasterVolume()->Draw();
+	}
+	else if(ToUpper((string)option) == "EVE")
+	{
+		TEveManager::Create();
 
-    TObjArray *arr = geo->GetListOfVolumes();
-    Int_t nVolumes = arr->GetEntries();
-    for( int i=0; i< nVolumes; i++ )
-        geo->GetVolume(i)->SetTransparency(50);
-    
-    geo->CheckOverlaps(0.0000001);
-    geo->PrintOverlaps();
-    
-    TEveGeoTopNode* vol =new TEveGeoTopNode(geo, node);
-    
-    vol->SetVisLevel(3);
-    
-    gEve->AddGlobalElement(vol);
-    
-    gEve->FullRedraw3D(kTRUE);
-    
-    TGLViewer *v = gEve->GetDefaultGLViewer();
-    v->GetClipSet()->SetClipType( (TGLClip::EType) 1);
-    v->SetGuideState(TGLUtil::kAxesEdge, kTRUE, kFALSE, 0);
-    v->SetStyle(TGLRnrCtx::kOutline);
-    v->RefreshPadEditor(v);
+		TGeoNode* node = geo->GetTopNode();
 
-    //v->CurrentCamera().RotateRad(-.7, 0.5);
-    v->DoDraw();
-    
+		TObjArray *arr = geo->GetListOfVolumes();
+		Int_t nVolumes = arr->GetEntries();
+		for (int i = 0; i < nVolumes; i++)
+			geo->GetVolume(i)->SetTransparency(50);
+
+		geo->CheckOverlaps(0.0000001);
+		geo->PrintOverlaps();
+
+		TEveGeoTopNode* vol = new TEveGeoTopNode(geo, node);
+
+		vol->SetVisLevel(3);
+
+		gEve->AddGlobalElement(vol);
+
+		gEve->FullRedraw3D(kTRUE);
+
+		TGLViewer *v = gEve->GetDefaultGLViewer();
+		v->GetClipSet()->SetClipType((TGLClip::EType) 1);
+		v->SetGuideState(TGLUtil::kAxesEdge, kTRUE, kFALSE, 0);
+		v->SetStyle(TGLRnrCtx::kOutline);
+		v->RefreshPadEditor(v);
+
+		//v->CurrentCamera().RotateRad(-.7, 0.5);
+		v->DoDraw();
+	}
 	//when we run this macro from restManager from bash,
 	//we need to call TRestMetadata::GetChar() to prevent returning,
 	//while keeping GUI alive.
@@ -65,7 +83,7 @@ Int_t REST_ViewGeometry( TString fName)
 #endif
 
 
-    return 0;
+	return 0;
 }
 
 #endif
