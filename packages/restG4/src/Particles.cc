df433c4f6c024536130de90dfb989c2e00325e75
Javier Galan <j.a.galan.81@gmail.com>
2019-06-17 22:33:57 +0200

    package/restG4. Remastered using reformat-clang.sh

--- packages/restG4/src/Particles.cc
+++ packages/restG4/src/Particles.cc
@@ -16,62 +16,62 @@ Particles::Particles(const G4String& name) : G4VPhysicsConstructor(name) {}
 Particles::~Particles() {}
 
 void Particles::ConstructParticle() {
-  G4Gamma::GammaDefinition();
+    G4Gamma::GammaDefinition();
 
-  // *******//
-  // leptons//
-  // *******//
+    // *******//
+    // leptons//
+    // *******//
 
-  G4Electron::ElectronDefinition();
-  G4Positron::PositronDefinition();
-  G4MuonPlus::MuonPlusDefinition();
-  G4MuonMinus::MuonMinusDefinition();
-  G4NeutrinoE::NeutrinoEDefinition();
-  G4AntiNeutrinoE::AntiNeutrinoEDefinition();
-  G4NeutrinoMu::NeutrinoMuDefinition();
-  G4AntiNeutrinoMu::AntiNeutrinoMuDefinition();
+    G4Electron::ElectronDefinition();
+    G4Positron::PositronDefinition();
+    G4MuonPlus::MuonPlusDefinition();
+    G4MuonMinus::MuonMinusDefinition();
+    G4NeutrinoE::NeutrinoEDefinition();
+    G4AntiNeutrinoE::AntiNeutrinoEDefinition();
+    G4NeutrinoMu::NeutrinoMuDefinition();
+    G4AntiNeutrinoMu::AntiNeutrinoMuDefinition();
 
-  // ********//
-  //  mesons //
-  // ********//
-  G4PionPlus::PionPlusDefinition();
-  G4PionMinus::PionMinusDefinition();
-  G4PionZero::PionZeroDefinition();
-  G4KaonPlus::KaonPlusDefinition();
-  G4KaonMinus::KaonMinusDefinition();
+    // ********//
+    //  mesons //
+    // ********//
+    G4PionPlus::PionPlusDefinition();
+    G4PionMinus::PionMinusDefinition();
+    G4PionZero::PionZeroDefinition();
+    G4KaonPlus::KaonPlusDefinition();
+    G4KaonMinus::KaonMinusDefinition();
 
-  // **********//
-  //  barions  //
-  // **********//
-  G4Proton::ProtonDefinition();
-  G4AntiProton::AntiProtonDefinition();
-  G4Neutron::NeutronDefinition();
-  G4AntiNeutron::AntiNeutronDefinition();
+    // **********//
+    //  barions  //
+    // **********//
+    G4Proton::ProtonDefinition();
+    G4AntiProton::AntiProtonDefinition();
+    G4Neutron::NeutronDefinition();
+    G4AntiNeutron::AntiNeutronDefinition();
 
-  // ******//
-  //  ions //
-  // ******//
-  G4Deuteron::DeuteronDefinition();
-  G4Triton::TritonDefinition();
-  G4He3::He3Definition();
-  G4Alpha::AlphaDefinition();
-  G4GenericIon::GenericIonDefinition();
+    // ******//
+    //  ions //
+    // ******//
+    G4Deuteron::DeuteronDefinition();
+    G4Triton::TritonDefinition();
+    G4He3::He3Definition();
+    G4Alpha::AlphaDefinition();
+    G4GenericIon::GenericIonDefinition();
 
-  G4LeptonConstructor lepton;
-  lepton.ConstructParticle();
+    G4LeptonConstructor lepton;
+    lepton.ConstructParticle();
 
-  G4BosonConstructor boson;
-  boson.ConstructParticle();
+    G4BosonConstructor boson;
+    boson.ConstructParticle();
 
-  G4MesonConstructor meson;
-  meson.ConstructParticle();
+    G4MesonConstructor meson;
+    meson.ConstructParticle();
 
-  G4BaryonConstructor baryon;
-  baryon.ConstructParticle();
+    G4BaryonConstructor baryon;
+    baryon.ConstructParticle();
 
-  G4ShortLivedConstructor shortLived;
-  shortLived.ConstructParticle();
+    G4ShortLivedConstructor shortLived;
+    shortLived.ConstructParticle();
 
-  G4IonConstructor ion;
-  ion.ConstructParticle();
+    G4IonConstructor ion;
+    ion.ConstructParticle();
 }
