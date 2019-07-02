994926dc5ba3cc5064eaf212e64f1de4bcf91dd3
nkx Nicholas <bur_ning@outlook.com>
2019-07-02 16:43:44 +0800

    update git info collection strategy. related to issue #64. Now we use CollectGitInfo.cmake instead of string commands

--- cmake/CollectGitInfo.cmake
+++ cmake/CollectGitInfo.cmake
@@ -1,7 +1,7 @@
 set(GIT_VALID FALSE)
 set(GIT_TAG)
 set(GIT_DATE)
-set(GIT_COMMIT)
+set(GIT_COMMIT " N/A ")
 set(GIT_BRANCH)
 
 if(CMAKE_SYSTEM_NAME MATCHES "Windows")
@@ -9,7 +9,7 @@ if(CMAKE_SYSTEM_NAME MATCHES "Windows")
 endif()
 
 execute_process(COMMAND git describe --tags 
-WORKING_DIRECTORY ${PROJECT_SOURCE_DIR} 
+WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} 
 OUTPUT_VARIABLE gitdiscribe ERROR_VARIABLE err)
 
 if(err MATCHES ".*fatal.*")
@@ -28,27 +28,33 @@ set(GIT_TAG ${version})
 
 #get git commit date
 execute_process(COMMAND git log -1 --format=%ai
-WORKING_DIRECTORY ${PROJECT_SOURCE_DIR} 
+WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} 
 OUTPUT_VARIABLE git_date)
 string(REPLACE "\n" "" git_date ${git_date})
 set(GIT_DATE ${git_date})
 
 #get git number	
 execute_process(COMMAND git rev-parse --verify HEAD 
-WORKING_DIRECTORY ${PROJECT_SOURCE_DIR} 
+WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR} 
 OUTPUT_VARIABLE git_ver)
 string(SUBSTRING ${git_ver} 0 8 git_commit )
 set(GIT_COMMIT ${git_commit})
 
 #get git branch
 execute_process(COMMAND git branch
-WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
 OUTPUT_FILE /tmp/temp.txt)
 execute_process(COMMAND grep -e "^*"
-WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
 INPUT_FILE /tmp/temp.txt 
 OUTPUT_VARIABLE git_branch)
 execute_process(COMMAND rm /tmp/temp.txt)
 string(REPLACE "\n" "" git_branch ${git_branch})
 string(SUBSTRING ${git_branch} 2 -1 git_branch)
 set(GIT_BRANCH ${git_branch})
+
+message(STATUS "REST release : ${GIT_TAG}")
+message(STATUS "REST date : ${GIT_DATE}")
+message(STATUS "REST commit : ${GIT_COMMIT}")
+message(STATUS "REST branch : ${GIT_BRANCH}")
+message("")
