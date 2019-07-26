fd74b1d1316cfb48a0e1e1719476845357351c9a
Javier Galan <j.a.galan.81@gmail.com>
2019-07-26 12:57:44 +0200

    geant4/REST_ReadNEvents_inROI.C. Added date and author.

--- macros/geant4/REST_ReadNEvents_inROI.C
+++ macros/geant4/REST_ReadNEvents_inROI.C
@@ -1,47 +1,42 @@
-
-Int_t REST_ReadNEvents_inROI(TString fName, int n1, int n2, double en1, double en2 )
-{
-   
-    TRestRun *run = new TRestRun();
+// Author : Gloria Luzon
+// Date : 26/July/2019
+Int_t REST_ReadNEvents_inROI(TString fName, int n1, int n2, double en1, double en2) {
+    TRestRun* run = new TRestRun();
 
     string fname = fName.Data();
-     //if( !fileExists( fname ) ) { cout << "WARNING. Input file does not exist" << endl; exit(1); }
+    // if( !fileExists( fname ) ) { cout << "WARNING. Input file does not exist" << endl; exit(1); }
 
-    //else run->OpenInputFile( fName );
+    // else run->OpenInputFile( fName );
 
-    run->OpenInputFile( fName );
+    run->OpenInputFile(fName);
 
     run->PrintInfo();
 
     /////////////////////////////
-    
+
     /////////////////////////////
     // Reading events
-     int n=0;
-      double en=0;
-    TRestG4Event *ev = new TRestG4Event();
-   
-   
-    run->SetInputEvent( ev );
-     for(int i=n1; i<n2+1;i++)
-    {
-    run->GetEntry ( i );
-     en=ev->GetSensitiveVolumeEnergy ();
-     if((en>=en1)&&(en<=en2)) 
-       {ev->PrintEvent();
-        n++;
-         cout<<n<<" event with "<<en<<" energy in sensitive volume "<<endl;
-        }
-     }
+    int n = 0;
+    double en = 0;
+    TRestG4Event* ev = new TRestG4Event();
 
-    
-     delete ev;
-    
-    // Do what ever you want with g4Event 
+    run->SetInputEvent(ev);
+    for (int i = n1; i < n2 + 1; i++) {
+        run->GetEntry(i);
+        en = ev->GetSensitiveVolumeEnergy();
+        if ((en >= en1) && (en <= en2)) {
+            ev->PrintEvent();
+            n++;
+            cout << n << " event with " << en << " energy in sensitive volume " << endl;
+        }
+    }
+
+    delete ev;
+
+    // Do what ever you want with g4Event
 
     /////////////////////////////
 
     delete run;
- return 0;
-    
+    return 0;
 }
