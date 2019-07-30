234ef9cb8f8dde6a4328e0e00cf09f0164320e64
David Diez <david101010@hotmail.com>
2019-07-30 12:07:56 +0200

    TRestRawSignalAnalysisProcess::Observables documentation

--- source/processes/analysis/inc/TRestRawSignalAnalysisProcess.h
+++ source/processes/analysis/inc/TRestRawSignalAnalysisProcess.h
@@ -1,22 +1,29 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             TRestRawSignalAnalysisProcess.h
-///
-///
-///_______________________________________________________________________________
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
 #ifndef RestCore_TRestRawSignalAnalysisProcess
 #define RestCore_TRestRawSignalAnalysisProcess
 
 #include <TH1D.h>
-
-//#include <TCanvas.h>
-
 #include <TRestGas.h>
 #include <TRestHitsEvent.h>
 #include <TRestRawSignalEvent.h>
@@ -24,11 +31,13 @@
 
 #include "TRestEventProcess.h"
 
+//! An analysis REST process to extract valuable information from RawSignal type of data.
 class TRestRawSignalAnalysisProcess : public TRestEventProcess {
    private:
 #ifndef __CINT__
+    /// A pointer to the specific TRestRawSignalEvent input
     TRestRawSignalEvent* fSignalEvent;  //!
-
+    
     TRestReadout* fReadout;  //!
 
     Double_t fFirstEventTime;             //!
@@ -69,6 +78,9 @@ class TRestRawSignalAnalysisProcess : public TRestEventProcess {
 
     void LoadDefaultConfig();
 
+   protected:
+    // add here the members of your event process
+    
    public:
     void InitProcess();
     void BeginOfEventProcess();
@@ -95,14 +107,11 @@ class TRestRawSignalAnalysisProcess : public TRestEventProcess {
 
     TString GetProcessName() { return (TString) "rawSignalAnalysis"; }
 
-    // Constructor
-    TRestRawSignalAnalysisProcess();
+    TRestRawSignalAnalysisProcess(); // Constructor
     TRestRawSignalAnalysisProcess(char* cfgFileName);
-    // Destructor
-    ~TRestRawSignalAnalysisProcess();
+    ~TRestRawSignalAnalysisProcess(); // Destructor
 
-    ClassDef(TRestRawSignalAnalysisProcess,
-             1);  // Template for a REST "event process" class inherited from
-                  // TRestEventProcess
+    ClassDef(TRestRawSignalAnalysisProcess, 1);  
+    // Template for a REST "event process" class inherited from TRestEventProcess
 };
 #endif
