a175b2a9e75f617271d10705686662d90bb1666b
Javier Galan <j.a.galan.81@gmail.com>
2019-08-07 10:28:20 +0200

    TRestHitsAnalysisProcess::PrintMetadata. Fixed metadata output.

--- source/processes/analysis/inc/TRestHitsAnalysisProcess.h
+++ source/processes/analysis/inc/TRestHitsAnalysisProcess.h
@@ -73,16 +73,14 @@ class TRestHitsAnalysisProcess : public TRestEventProcess {
     void PrintMetadata() {
         BeginPrintProcess();
 
-        std::cout << " Fiducial parameters" << std::endl;
-        std::cout << " -------------------" << std::endl;
-        std::cout << " x0 : (" << fFid_x0.X() << " , " << fFid_x0.Y() << " , " << fFid_x0.Z() << ")"
-                  << std::endl;
-        std::cout << " x1 : (" << fFid_x1.X() << " , " << fFid_x1.Y() << " , " << fFid_x1.Z() << ")"
-                  << std::endl;
-        std::cout << " R : " << fFid_R << std::endl;
-        std::cout << " sX : " << fFid_sX << std::endl;
-        std::cout << " sY : " << fFid_sY << std::endl;
-        std::cout << " -------------------" << std::endl;
+        metadata << " Fiducial parameters" << endl;
+        metadata << " -------------------" << endl;
+        metadata << " x0 : (" << fFid_x0.X() << " , " << fFid_x0.Y() << " , " << fFid_x0.Z() << ")" << endl;
+        metadata << " x1 : (" << fFid_x1.X() << " , " << fFid_x1.Y() << " , " << fFid_x1.Z() << ")" << endl;
+        metadata << " R : " << fFid_R << endl;
+        metadata << " sX : " << fFid_sX << endl;
+        metadata << " sY : " << fFid_sY << endl;
+        metadata << " -------------------" << endl;
 
         EndPrintProcess();
     }
