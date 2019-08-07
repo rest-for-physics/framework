3b7be8c1fca5e539d44f5dca05f9dd009198bb4e
lobis <lobis@unizar.es>
2019-08-07 16:24:35 +0200

    Added TRestEnums.h to store configurations enums

---
+++ source/tools/resttools/inc/TRestEnums.h
@@ -0,0 +1,35 @@
+//
+// Created by lobis on 07/08/2019.
+//
+
+#ifndef REST_TRESTENUMS_H
+#define REST_TRESTENUMS_H
+
+#endif  // REST_TRESTENUMS_H
+
+namespace TRestParticleGeneratorConfig {
+
+enum class energyGeneratorTypes {
+    ROOT_HIST,
+    MONO,
+    FLAT,
+};
+enum class angularGeneratorTypes {
+    ROOT_HIST,
+    ISOTROPIC,
+    FLUX,
+    BACK_TO_BACK,
+};
+enum class spatialGeneratorTypes {
+    FILE,
+    VOLUME,
+    SURFACE,
+    POINT,
+    VIRTUAL_WALL,
+    VIRTUAL_BOX,
+    VIRTUAL_SPHERE,
+    VIRTUAL_CIRCLE_WALL,
+    VIRTUAL_CYLINDER,
+};
+
+}
