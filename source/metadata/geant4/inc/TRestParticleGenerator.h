3a42473a914b0add8560d01c60c59429573fc187
lobis <lobis@unizar.es>
2019-08-07 12:42:26 +0200

    TRestParticleGenerator: added class files and description

---
+++ source/metadata/geant4/inc/TRestParticleGenerator.h
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////
+///
+///             RESTSoft: Software for Rare Event Searches with TPCs
+///
+///             TRestParticleGenerator.h
+///
+///             This class handles particle generation: spatial, angular and energy sampling according to the
+///             information stored in this class, which is supposed to be loaded from a `TRestG4Metadata`
+///             object. This class is invoked in the GEANT4 code to define primary sources.
+///
+///             Aug 2019: Introduction. Luis Obis (@lobis)
+///
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef REST_TRESTPARTICLEGENERATOR_H
+#define REST_TRESTPARTICLEGENERATOR_H
+
+#endif  // REST_TRESTPARTICLEGENERATOR_H
---
+++ source/metadata/geant4/src/TRestParticleGenerator.cxx
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////////////////
+///
+///             RESTSoft: Software for Rare Event Searches with TPCs
+///
+///             TRestParticleGenerator.cxx
+///
+///             This class handles particle generation: spatial, angular and energy sampling according to the
+///             information stored in this class, which is supposed to be loaded from a `TRestG4Metadata`
+///             object. This class is invoked in the GEANT4 code to define primary sources.
+///
+///             Aug 2019: Introduction. Luis Obis (@lobis)
+///
+//////////////////////////////////////////////////////////////////////////
