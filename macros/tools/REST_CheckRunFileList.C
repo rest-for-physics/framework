0ff952928eff6331a6b05a502892a8cafdf5998f
Javier Galan <j.a.galan.81@gmail.com>
2019-11-04 15:53:00 +0100

    REST_CheckRunFileList.C now prints total time of run list given.

--- macros/tools/REST_CheckRunFileList.C
+++ macros/tools/REST_CheckRunFileList.C
@@ -10,22 +10,12 @@ Int_t REST_CheckRunFileList(TString namePattern, Int_t N = 100000) {
 
     vector<TString> filesNotWellClosed;
 
-    // TString command = "ls -d -1 " + namePattern + " > /tmp/CheckRunListCommand_72nd72jdl";
-    // char cmd[512];
-    // sprintf( cmd, "%s", command.Data() );
-    // system( cmd );
-
-    // gSystem->Load("librestcore.so");
-
-    // int cont = 0;
-
-    // char filename[256];
-    // FILE *flist = fopen( "/tmp/CheckRunListCommand_72nd72jdl", "rt" );
     TRestStringOutput cout;
 
     string a = TRestTools::Execute((string)("ls -d -1 " + namePattern));
     vector<string> b = Split(a, "\n");
 
+    Double_t totalTime = 0;
     int cont = 0;
     for (int i = 0; i < b.size(); i++) {
         string filename = b[i];
@@ -54,8 +44,9 @@ Int_t REST_CheckRunFileList(TString namePattern, Int_t N = 100000) {
         }
 
         cout << "Run time (hours) : " << run->GetRunLength() / 3600. << endl;
+        totalTime += run->GetRunLength() / 3600.;
 
-        if (run->GetRunLength() == -1) {
+        if (run->GetEndTimestamp() == 0) {
             filesNotWellClosed.push_back(filename);
         }
 
@@ -64,47 +55,15 @@ Int_t REST_CheckRunFileList(TString namePattern, Int_t N = 100000) {
         f->Close();
     }
 
-    // while ( fscanf( flist, "%s\n", filename ) != EOF && cont < N )
-    //{
-    //    cout << filename << endl;
-    //    cont++;
-    //    TRestRun *run = new TRestRun();
+    if (filesNotWellClosed.size() > 0) {
+        cout << "---------------------" << endl;
+        cout << "Files not well closed" << endl;
+        cout << "---------------------" << endl;
+        for (int i = 0; i < filesNotWellClosed.size(); i++) cout << filesNotWellClosed[i] << endl;
+        cout << "---------------------" << endl;
+    }
 
-    //    TFile *f = new TFile( filename );
-
-    //    if( !fileExists( filename ) ) { cout << "WARNING. Input file does not exist" << endl; exit(1); }
-
-    //    /////////////////////////////
-    //    // Reading metadata classes
-
-    //    TIter nextkey(f->GetListOfKeys());
-    //    TKey *key;
-    //    while ( ( key =  (TKey*)nextkey() ) ) {
-    //        string className = key->GetClassName();
-    //        if ( className == "TRestRun" )
-    //        {
-    //            cout << key->GetName() << endl;
-    //            run = (TRestRun *) f->Get( key->GetName() );
-    //        }
-    //    }
-
-    //    cout << "Run time (hours) : " << run->GetRunLength()/3600. << endl;
-
-    //    if( run->GetRunLength() == -1 )
-    //    {
-    //        filesNotWellClosed.push_back( filename );
-    //    }
-
-    //    delete run;
-
-    //    f->Close();
-    //}
-
-    cout << "---------------------" << endl;
-    cout << "Files not well closed" << endl;
-    cout << "---------------------" << endl;
-    for (int i = 0; i < filesNotWellClosed.size(); i++) cout << filesNotWellClosed[i] << endl;
-    cout << "---------------------" << endl;
+    cout << "Total runs time : " << totalTime << " hours" << endl;
 
     return 0;
 }
