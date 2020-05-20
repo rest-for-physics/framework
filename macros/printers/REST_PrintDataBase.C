ba84283a9311a12d5547c8bb498838f71f74b21a
nkx Nicholas <bur_ning@outlook.com>
2020-05-20 22:09:10 +0800

    REST_PrintDataBase: fix a bug

--- macros/printers/REST_PrintDataBase.C
+++ macros/printers/REST_PrintDataBase.C
@@ -8,14 +8,14 @@ using namespace std;
 
 Int_t REST_DataBase(Int_t run = -1) {
     TRestStringOutput fout;
-    TRestDataBase* db = TRestDataBase::instantiate();
+    TRestDataBase* db = TRestDataBase::instantiate("PSQL");
     if (db == NULL) {
         fout.setcolor(COLOR_BOLDRED);
         fout << "REST ERROR!! package: restDataBaseImpl not installed!" << endl;
         fout << "cannot print database record!" << endl;
     } else {
         if (run < 0) run = db->get_lastrun();
-		db->print(run);
+        db->print(Form("select * from rest_runs where run_id=%i", run));
     }
 
     return 0;
