92705e8f0068928566e72578a74e67224e387d2b
Ni Kaixiang <bur_ning@outlook.com>
2020-05-28 19:34:40 +0200

    Update ValidatePlot.C

--- scripts/pipeline/pandaxiii/ValidatePlot.C
+++ scripts/pipeline/pandaxiii/ValidatePlot.C
@@ -1,9 +1,8 @@
-
 Int_t ValidatePlot(string fname) {
 
     string output = TRestTools::Execute("sha1sum " + fname);
     string sha1sum = Split(output, " ")[0];
-    if (sha1sum != "0869177ec497f62967fd5aca3cba1856717e16ec") {
+    if (sha1sum != "d0ad272769250daa47c37221dae2a759c278cb53") {
         cout << "incoherent sha1sum: " << sha1sum << endl;
         return -1;
     }
