425c03c1ec74527d71ffa895a70390b340471975
Ni Kaixiang <bur_ning@outlook.com>
2020-05-28 19:24:13 +0200

    Update ValidateTrack.C

--- scripts/pipeline/pandaxiii/ValidateTrack.C
+++ scripts/pipeline/pandaxiii/ValidateTrack.C
@@ -4,22 +4,22 @@ Int_t ValidateTrack(string fname) {
     TRestAnalysisTree* AnalysisTree = run->GetAnalysisTree();
     
     bool flag=true;
-    if (AnalysisTree->GetObservableValue<double>("tckBlob_2D_Qhigh_X_R20") != 404.79117548465728759766) {
+    if (round(1000 * AnalysisTree->GetObservableValue<double>("tckBlob_2D_Qhigh_X_R20")) != 328099) {
         cout<<"wrong tckBlob_2D_Qhigh_X_R20: "<< AnalysisTree->GetObservableValue<double>("tckBlob_2D_Qhigh_X_R20")<<endl;
         flag = false;
     }
 
-    if (AnalysisTree->GetObservableValue<double>("tckBlob_2D_Qhigh_Y_R20") != 301.09379857778549194336) {
+    if (round(1000 * AnalysisTree->GetObservableValue<double>("tckBlob_2D_Qhigh_Y_R20")) != 361361) {
         cout<<"wrong tckBlob_2D_Qhigh_Y_R20: "<< AnalysisTree->GetObservableValue<double>("tckBlob_2D_Qhigh_Y_R20")<<endl;
         flag = false;
     }
 
-    if (AnalysisTree->GetObservableValue<double>("hitsAnaSmear_xMean") != 112.28328575433145886109) {
+    if (round(1000 * AnalysisTree->GetObservableValue<double>("hitsAnaSmear_xMean")) != 102933) {
         cout<<"wrong hitsAnaSmear_xMean: "<< AnalysisTree->GetObservableValue<double>("hitsAnaSmear_xMean")<<endl;
         flag = false;
     }
 
-    if (AnalysisTree->GetObservableValue<double>("hitsAnaSmear_yMean") != 567.69430957018153094396) {
+    if (round(1000 * AnalysisTree->GetObservableValue<double>("hitsAnaSmear_yMean")) != -636089) {
         cout<<"wrong hitsAnaSmear_yMean: "<< AnalysisTree->GetObservableValue<double>("hitsAnaSmear_yMean")<<endl;
         flag = false;
     }  
