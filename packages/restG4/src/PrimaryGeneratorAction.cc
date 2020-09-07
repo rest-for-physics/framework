464196128349400da2526d938c407f1ab55d67e5
gluzon <luzon@unizar.es>
2020-09-07 14:42:28 +0200

    PrimaryGeneratorAction. Commented duplicated info about primary ion

--- packages/restG4/src/PrimaryGeneratorAction.cc
+++ packages/restG4/src/PrimaryGeneratorAction.cc
@@ -136,7 +136,7 @@ G4ParticleDefinition* PrimaryGeneratorAction::SetParticleDefinition(int n) {
     for (int Z = 1; Z <= 110; Z++)
         for (int A = 2 * Z; A <= 3 * Z; A++) {
             if (particle_name == G4IonTable::GetIonTable()->GetIonName(Z, A)) {
-                cout << "particle : " << G4IonTable::GetIonTable()->GetIonName(Z, A, excited_energy) << endl;
+                //cout << "particle : " << G4IonTable::GetIonTable()->GetIonName(Z, A, excited_energy) << endl;
                 particle = G4IonTable::GetIonTable()->GetIon(Z, A, excited_energy);
                 particle_name = G4IonTable::GetIonTable()->GetIonName(Z, A, excited_energy);
                 fParticleGun->SetParticleCharge(charge);
