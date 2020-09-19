02c6723c7e4b0c1be87a2d01d63a925dce87dc5b
lobis <lobis@unizar.es>
2020-09-19 15:02:09 +0200

    TRestGeant4VetoAnalysisProcess: Added documentation header

--- source/processes/analysis/inc/TRestGeant4VetoAnalysisProcess.h
+++ source/processes/analysis/inc/TRestGeant4VetoAnalysisProcess.h
@@ -1,6 +1,25 @@
-//
-// Created by Luis Obis on 15/09/2020.
-//
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2020 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
+
 
 #ifndef REST_TRESTGEANT4VETOANALYSISPROCESS_H
 #define REST_TRESTGEANT4VETOANALYSISPROCESS_H
--- source/processes/analysis/src/TRestGeant4VetoAnalysisProcess.cxx
+++ source/processes/analysis/src/TRestGeant4VetoAnalysisProcess.cxx
@@ -1,6 +1,41 @@
-//
-// Created by Luis Obis on 15/09/2020.
-//
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2020 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
+
+//////////////////////////////////////////////////////////////////////////
+/// TODO: fill documentation
+///
+///--------------------------------------------------------------------------
+///
+/// RESTsoft - Software for Rare Event Searches with TPCs
+///
+/// History of developments:
+///
+/// 2020-September: Implementation.
+///
+/// \class      TRestGeant4VetoAnalysisProcess
+/// \author     Luis Obis
+///
+/// <hr>
+///
 
 #include "TRestGeant4VetoAnalysisProcess.h"
 using namespace std;
