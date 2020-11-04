2fae2881ec694d4d8a79ad27ebea5c337018fee6
@cmargalejo <cristinamargalejoblasco@gmail.com>
2020-11-04 13:12:43 +0100

    REST_HitCentering.C: update macro to generate both X and Y histograms at the same time

--- macros/general/REST_HitCentering.C
+++ macros/general/REST_HitCentering.C
@@ -11,27 +11,33 @@
 
 //*******************************************************************************************************
 //*** Description: This macro reads each hits event inside the file.root given by argument and centers it
-//*** around 0 by subtracting the mean event position to each individual hit position. It creates a
-//*** histogram including events n1 to n2 and stores it in file.root. It can be executed for both XZ
-//*** events (isX = true) or YZ events (isX = false).
+//*** around 0 by subtracting the mean event position to each individual hit position. It creates two
+//*** histograms including events n1 to n2 and stores it in file.root. One for the XZ events (with an 'X'
+//*** suffix) and one for YZ events (with a 'Y' suffix).
+//*** The given number of bins and bin range will be applied to both histograms.
+//*** 
+//*** Hits will only be added to the histograms, iff they have a valid coordinate. That is if their 
+//*** coordinate is unequal to `invalidVal`. The default value of -0.125 corresponds to an invalid 
+//*** coordinate for the IAXO/CAST readout.
+//*** Each hit only has one coordinate that is valid (either X or Y) while the other is `invalidVal`, 
+//*** even if two strips are produced from one physical event.
 //*** --------------
-//*** The output histogram shows the standard shape of the hits events, so their symmetry, gaussianity
-//*** and others can be estimated. 
+//*** The output histograms show the standard shape of the hits events, so their symmetry, gaussianity
+//*** and others can be estimated.
 //*** --------------
-//*** Usage: restManager HitCentering bool /full/path/file.root [histoName] [firstBin] [lastBin]
-//*** [#bins] [firstEvent] [lastEvent]
+//*** Usage: restManager HitCentering /full/path/file.root [histoName] [firstBin] [lastBin]
+//*** [#bins] [firstEvent] [lastEvent] [invalidVal]
 //*******************************************************************************************************
 
-Int_t REST_HitCentering(bool isX, TString rootFileName, TString histoName, int startVal = -30,
-                        int endVal = 30, int bins = 120, int n1 = 0, int n2 = 60000) {
-	/*
-	Set first argument to true to run the macro on the X direction, and to false to run it on the Y direction.
-	*/
+Int_t REST_HitCentering(TString rootFileName, TString histoName, int startVal = -30,
+                        int endVal = 30, int bins = 120, int n1 = 0, int n2 = 60000,
+                        double invalidVal = -0.125) {
     TRestStringOutput cout;
-
+    
     std::vector<string> inputFilesNew = TRestTools::GetFilesMatchingPattern((string)rootFileName);
 
-    TH1D* h = new TH1D(histoName, histoName, bins, startVal, endVal);
+    TH1D* hX = new TH1D(histoName + "X", histoName + "X", bins, startVal, endVal);
+    TH1D* hY = new TH1D(histoName + "Y", histoName + "Y", bins, startVal, endVal);
 
     if (inputFilesNew.size() == 0) {
         cout << "Files not found!" << endl;
@@ -43,53 +49,38 @@ Int_t REST_HitCentering(bool isX, TString rootFileName, TString histoName, int s
 
         run->OpenInputFile(inputFilesNew[n]);
 
-    // Reading events
+        // Reading events
+        TRestHitsEvent* ev = new TRestHitsEvent();
 
-    TRestHitsEvent* ev = new TRestHitsEvent();
+        run->SetInputEvent(ev);
+        for (int i = n1; i < n2 + 1; i++) {
+            run->GetEntry(i);
 
-    run->SetInputEvent(ev);
-    for (int i = n1; i < n2 + 1; i++) {
-        run->GetEntry(i);
+            // Fill in the histogram with position and energy if the coordinate is valid
+            // i.e. unequal to `invalidVal`
+            for( int n = 0; n < ev->GetNumberOfHits(); n++ ) {
+                Double_t en = ev->GetEnergy(n);
+                Double_t valX = ev->GetX(n);
+                Double_t meanX = ev->GetMeanPosition().X();
+                Double_t valY = ev->GetY(n);
+                Double_t meanY = ev->GetMeanPosition().Y();
 
-		//Fill in the histogram with position and energy
+                if(valX != invalidVal) hX->Fill(valX - meanX, en);
+                if(valY != invalidVal) hY->Fill(valY - meanY, en);
+            }
+        }
 
-		for( int n = 0; n < ev->GetNumberOfHits(); n++ ) {
-			Double_t val, mean;
-			if (isX) {
-				val = ev->GetX(n);   
-				mean = ev->GetMeanPosition().X();  	
-			}
-			else {
-				val = ev->GetY(n);
-				mean = ev->GetMeanPosition().Y();
-			}
-            
-			if (val != -0.125){
-				// I subtract the mean position of the event to center the histogram.
-				Double_t cent_val = val - mean;
-
-				Double_t en = ev->GetEnergy(n);
-
-				// Fill in the histogram
-
-				h->Fill( cent_val, en);
-			}
-		}
-    }
-
-    delete ev;
-
-    /////////////////////////////
-
+        // cleanup
+        delete ev;
         delete run;
     }
 
-
     TFile* f = new TFile(rootFileName, "update");
-    h->Write(histoName);
+    hX->Write(histoName + "X");
+    hY->Write(histoName + "Y");
     f->Close();
 
-    cout << "Written histogram " << histoName << " into " << rootFileName << endl;
+    cout << "Written histograms " << histoName << "X/Y into " << rootFileName << endl;
 
     return 0;
 };
