4d90c02792dcae8d664f60744a9529a18734e51a
Javier Galan <j.a.galan.81@gmail.com>
2020-11-24 17:59:15 +0100

    Updated TRestVersion.h to v2.2.21

--- source/restcore/inc/TRestVersion.h
+++ source/restcore/inc/TRestVersion.h
@@ -10,11 +10,11 @@
  * #endif
  *
  */
-#define REST_RELEASE "2.2.20"
-#define REST_RELEASE_DATE "Sun Sep 27"
-#define REST_RELEASE_TIME "21:07:38 CST 2020"
-#define REST_GIT_COMMIT "76c1771f"
-#define REST_VERSION_CODE 131604
+#define REST_RELEASE "2.2.21"
+#define REST_RELEASE_DATE "Tue Nov 24"
+#define REST_RELEASE_TIME "17:58:09 CET 2020"
+#define REST_GIT_COMMIT "099b1960"
+#define REST_VERSION_CODE 131605
 #define REST_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 #define REST_SCHEMA_EVOLUTION "ON"
 #endif
