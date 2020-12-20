aee5ad5008e8d8d45a98df3a33f25e96067e364a
j.a.galan.81@gmail.com <j.a.galan.81@gmail.com>
2020-12-20 16:04:49 +0100

    TRestRawFindResponseSignalProcess. Adapted to official documentation format

--- source/libraries/raw/inc/TRestRawFindResponseSignalProcess.h
+++ source/libraries/raw/inc/TRestRawFindResponseSignalProcess.h
@@ -1,13 +1,24 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             TRestRawFindResponseSignalProcess.h
-///
-///_______________________________________________________________________________
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
 #ifndef RestCore_TRestRawFindResponseSignalProcess
 #define RestCore_TRestRawFindResponseSignalProcess
@@ -16,6 +27,7 @@
 
 #include "TRestEventProcess.h"
 
+//! A process to generate an average response signal
 class TRestRawFindResponseSignalProcess : public TRestEventProcess {
    private:
     TRestRawSignalEvent* fInputSignalEvent;   //!
@@ -50,14 +62,10 @@ class TRestRawFindResponseSignalProcess : public TRestEventProcess {
 
     TString GetProcessName() { return (TString) "findResponseSignal"; }
 
-    // Constructor
     TRestRawFindResponseSignalProcess();
     TRestRawFindResponseSignalProcess(char* cfgFileName);
-    // Destructor
     ~TRestRawFindResponseSignalProcess();
 
-    ClassDef(TRestRawFindResponseSignalProcess,
-             1);  // Template for a REST "event process" class inherited from
-                  // TRestEventProcess
+    ClassDef(TRestRawFindResponseSignalProcess, 1);
 };
 #endif
--- source/libraries/raw/src/TRestRawFindResponseSignalProcess.cxx
+++ source/libraries/raw/src/TRestRawFindResponseSignalProcess.cxx
@@ -1,27 +1,81 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             TRestRawFindResponseSignalProcess.cxx
-///
-///             February 2016: Javier Galan
-///_______________________________________________________________________________
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
+//////////////////////////////////////////////////////////////////////////
+/// The TRestRawAFTERToSignalProcess is a process used to read a binary file
+/// produced by the AFTER electronics, the resulting signal will be registered
+/// inside a TRestRawSignalEvent, and then it processing can continue inside
+/// with the REST framework libraries.
+///
+/// \warning TODO. This process might be obsolete today. It may need additional
+/// revision, validation, and documentation. Use it under your own risk. If you
+/// find this process useful for your work feel free to use it, improve it,
+/// validate and/or document this process. If all those points are addressed
+/// these TODO lines can be removed.
+///
+/// \warning **⚠ WARNING: REST is under continous development.** This documentation
+/// is offered to you by the REST community. Your HELP is needed to keep this file
+/// up to date. You are very welcome to contribute fixing typos, updating
+/// information or adding new contributions. See also our [Contribution
+/// Guide](https://lfna.unizar.es/rest-development/REST_v2/-/blob/master/CONTRIBUTING.md).
+///
+///--------------------------------------------------------------------------
+///
+/// RESTsoft - Software for Rare Event Searches with TPCs
+///
+/// History of developments:
+///
+/// 2016-Feb: First implementation
+///           Javier Galan
+///
+/// \class      TRestRawFindResponseSignalProcess
+/// \author     Javier Galan
+///
+/// <hr>
+///
 #include "TRestRawFindResponseSignalProcess.h"
 using namespace std;
 
 #include <TFile.h>
 
-ClassImp(TRestRawFindResponseSignalProcess)
-    //______________________________________________________________________________
-    TRestRawFindResponseSignalProcess::TRestRawFindResponseSignalProcess() {
-    Initialize();
-}
+ClassImp(TRestRawFindResponseSignalProcess);
 
-//______________________________________________________________________________
+///////////////////////////////////////////////
+/// \brief Default constructor
+///
+TRestRawFindResponseSignalProcess::TRestRawFindResponseSignalProcess() { Initialize(); }
+
+///////////////////////////////////////////////
+/// \brief Constructor loading data from a config file
+///
+/// If no configuration path is defined using TRestMetadata::SetConfigFilePath
+/// the path to the config file must be specified using full path, absolute or
+/// relative.
+///
+/// The default behaviour is that the config file must be specified with
+/// full path, absolute or relative.
+///
+/// \param cfgFileName A const char* giving the path to an RML file.
+///
 TRestRawFindResponseSignalProcess::TRestRawFindResponseSignalProcess(char* cfgFileName) {
     Initialize();
 
@@ -31,18 +85,23 @@ TRestRawFindResponseSignalProcess::TRestRawFindResponseSignalProcess(char* cfgFi
     // TRestRawFindResponseSignalProcess default constructor
 }
 
-//______________________________________________________________________________
-TRestRawFindResponseSignalProcess::~TRestRawFindResponseSignalProcess() {
-    delete fOutputSignalEvent;
-    // TRestRawFindResponseSignalProcess destructor
-}
+///////////////////////////////////////////////
+/// \brief Default destructor
+///
+TRestRawFindResponseSignalProcess::~TRestRawFindResponseSignalProcess() { delete fOutputSignalEvent; }
 
+///////////////////////////////////////////////
+/// \brief Function to load the default config in absence of RML input
+///
 void TRestRawFindResponseSignalProcess::LoadDefaultConfig() {
     SetName("findResponseSignal-Default");
     SetTitle("Default config");
 }
 
-//______________________________________________________________________________
+///////////////////////////////////////////////
+/// \brief Function to initialize input/output event members and define the
+/// section name
+///
 void TRestRawFindResponseSignalProcess::Initialize() {
     SetSectionName(this->ClassName());
 
@@ -50,24 +109,36 @@ void TRestRawFindResponseSignalProcess::Initialize() {
     fOutputSignalEvent = new TRestRawSignalEvent();
 }
 
+///////////////////////////////////////////////
+/// \brief Function to load the configuration from an external configuration
+/// file.
+///
+/// If no configuration path is defined in TRestMetadata::SetConfigFilePath
+/// the path to the config file must be specified using full path, absolute or
+/// relative.
+///
+/// \param cfgFileName A const char* giving the path to an RML file.
+/// \param name The name of the specific metadata. It will be used to find the
+/// correspondig TRestGeant4AnalysisProcess section inside the RML.
+///
 void TRestRawFindResponseSignalProcess::LoadConfig(string cfgFilename, string name) {
     if (LoadConfigFromFile(cfgFilename, name) == -1) LoadDefaultConfig();
 }
 
-//______________________________________________________________________________
-void TRestRawFindResponseSignalProcess::InitProcess() {
-    // Function to be executed once at the beginning of process
-    // (before starting the process of the events)
+///////////////////////////////////////////////
+/// \brief Process initialization. Function to be executed once at the
+/// beginning of process (before starting the event processing)
+///
+void TRestRawFindResponseSignalProcess::InitProcess() {}
 
-    // Start by calling the InitProcess function of the abstract class.
-    // Comment this if you don't want it.
-    // TRestEventProcess::InitProcess();
-}
-
-//______________________________________________________________________________
+///////////////////////////////////////////////
+/// \brief The main processing event function
+///
 TRestEvent* TRestRawFindResponseSignalProcess::ProcessEvent(TRestEvent* evInput) {
     fInputSignalEvent = (TRestRawSignalEvent*)evInput;
 
+    // We accept signals that are inside a given condition.
+    // TODO: Now it is also possible to use ApplyCut and <cut definitions?
     if (fInputSignalEvent->GetNumberOfSignals() <= 0) return NULL;
     if (fInputSignalEvent->GetNumberOfSignals() > 8) return NULL;
 
@@ -89,36 +160,12 @@ TRestEvent* TRestRawFindResponseSignalProcess::ProcessEvent(TRestEvent* evInput)
 
     fOutputSignalEvent->AddSignal(*sgnl);
 
-    /// Copying the signal event to the output event
-    /*
-
-    fOutputSignalEvent->SetID( fInputSignalEvent->GetID() );
-    fOutputSignalEvent->SetSubID( fInputSignalEvent->GetSubID() );
-    fOutputSignalEvent->SetTimeStamp( fInputSignalEvent->GetTimeStamp() );
-    fOutputSignalEvent->SetSubEventTag( fInputSignalEvent->GetSubEventTag() );
-
-    fOutputSignalEvent->AddSignal( *fInputSignalEvent->GetSignal( dominantSignal )
-    );
-    /////////////////////////////////////////////////
-    */
-
     return fOutputSignalEvent;
 }
 
-//______________________________________________________________________________
-void TRestRawFindResponseSignalProcess::EndProcess() {
-    // Function to be executed once at the end of the process
-    // (after all events have been processed)
-
-    // Start by calling the EndProcess function of the abstract class.
-    // Comment this if you don't want it.
-    // TRestEventProcess::EndProcess();
-}
-
-//______________________________________________________________________________
-void TRestRawFindResponseSignalProcess::InitFromConfigFile() {
-    /*
-        fTriggerTime = StringToInteger( GetParameter( "triggerTime" ) );
-        fNBins = StringToInteger( GetParameter( "nBins" ) );
-    */
-}
+///////////////////////////////////////////////
+/// \brief End of procesing. Function to be executed once at the
+/// end of the process. It gets access to all the analysis tree
+/// entries. See for example: TRestDataSummary.
+///
+void TRestRawFindResponseSignalProcess::EndProcess() {}
