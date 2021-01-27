eeb5769ec3dbfb30c4198213919ed0032d2ba5b5
git <j.a.galan.81@gmail.com>
2021-01-27 14:38:20 +0100

    clean-state.sh comment update

--- clean-state.sh
+++ clean-state.sh
@@ -1,2 +1,3 @@
 # I believe we need python3.5 or higher. Not sure how to make this generic for all users.
+# Each system administrator should point python3 to a python3.5 or higher
 python3 pull-submodules.py --clean
