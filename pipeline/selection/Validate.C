681e162f16ed6f8462cace3023b129327bf01951
git <javier.galan@unizar.es>
2021-03-22 13:29:56 +0100

    .gitlab-ci.yml and pipeline/selection. Replacing absolute paths for relative ones

--- pipeline/selection/Validate.C
+++ pipeline/selection/Validate.C
@@ -3,12 +3,12 @@
 #include <iostream>
 #include <vector>
 Int_t Validate(TString validationFile) {
-    TFile* f = new TFile("${CI_PROJECT_DIR}/pipeline/selection/Run00001_NLDBD_Test_EvSelection.root");
+    TFile* f = new TFile("Run00001_NLDBD_Test_EvSelection.root");
     TTree* tr = (TTree*)f->Get("AnalysisTree");
-      
+
     TFile* fV = new TFile(validationFile);
     TTree* tV = (TTree*)fV->Get("AnalysisTree");
-    
+
     // Check number of entries.
     if (tr->GetEntries() != tV->GetEntries()) {
         cout << "Number of entries is not the same!" << endl;
@@ -16,14 +16,14 @@ Int_t Validate(TString validationFile) {
         cout << "Obtained: " << tr->GetEntries() << endl;
         return 1;
     }
-    
+
     // Check IDs.
-    for (Int_t i = 0; i < tr->GetEntries(); i++){     
+    for (Int_t i = 0; i < tr->GetEntries(); i++) {
         tr->GetEntry(i);
         tV->GetEntry(i);
-        if (tr->GetLeaf("eventID")->GetValue(0) != tV->GetLeaf("eventID")->GetValue(0)){
+        if (tr->GetLeaf("eventID")->GetValue(0) != tV->GetLeaf("eventID")->GetValue(0)) {
             cout << "Same number of entries but different IDs" << endl;
-            cout << "Found entry " << i << " with ID: " <<  tr->GetLeaf("eventID")->GetValue(0) << endl;
+            cout << "Found entry " << i << " with ID: " << tr->GetLeaf("eventID")->GetValue(0) << endl;
             cout << "Expected entry " << i << " with ID: " << tV->GetLeaf("eventID")->GetValue(0) << endl;
             return 2;
         }
