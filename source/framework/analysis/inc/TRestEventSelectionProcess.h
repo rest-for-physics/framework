98345408ebdf59a79df276af31b0831e7ff729de
git <javier.galan@unizar.es>
2021-04-21 10:44:25 +0200

    TRestEventSelectionProcess. Initializing metadata members as empty strings

--- source/framework/analysis/inc/TRestEventSelectionProcess.h
+++ source/framework/analysis/inc/TRestEventSelectionProcess.h
@@ -31,15 +31,15 @@
 class TRestEventSelectionProcess : public TRestEventProcess {
    private:
     TRestEvent* fEvent;  //!
-    string fFileWithIDs;
-    string fConditions;
+    string fFileWithIDs = "";
+    string fConditions = "";
     std::vector<Int_t> fList;
 
     /// A list with the event ids that have been selected.
-    //std::vector<Int_t> fEventIds;  //<
+    // std::vector<Int_t> fEventIds;  //<
 
     // void InitFromConfigFile();
-    
+
     void Initialize();
 
    protected:
