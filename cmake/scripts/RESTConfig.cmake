3d4f9047b07fe8b0deb7e2f64450a414682ecd45
lobis <luis.antonio.obis@gmail.com>
2021-05-05 14:21:15 +0200

    RESTConfig.cmake: added REST_PATH, REST_LIBRARIES and REST_VERSION variables

--- cmake/scripts/RESTConfig.cmake
+++ cmake/scripts/RESTConfig.cmake
@@ -1,3 +1,12 @@
+
 # This cmake file will enable to find REST from another CMake project via "find_package(REST)"
+# It will define useful variables such as "REST_PATH" or "REST_LIBRARIES" which makes linking easier
 
 set(REST_PATH $ENV{REST_PATH})
+
+# TODO: I think there is a better way to do this, we shouldn't call 'rest-config' for this
+execute_process(COMMAND rest-config --libs OUTPUT_VARIABLE REST_LIBRARIES)
+string(STRIP ${REST_LIBRARIES} REST_LIBRARIES) # It is necessary to strip the whitespaces, or it will give error
+
+execute_process(COMMAND rest-config --version OUTPUT_VARIABLE REST_VERSION)
+string(STRIP ${REST_VERSION} REST_VERSION)
