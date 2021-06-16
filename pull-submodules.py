18722e15e5f8c07e63e3dc42c6068d1a48e27fd9
git <javier.galan@unizar.es>
2021-06-16 09:12:15 +0200

    pull-submodules.py will print a message on screen in case a parallel submodule branch is being pulled

--- pull-submodules.py
+++ pull-submodules.py
@@ -155,6 +155,7 @@ def main():
                              branchToPull = "master"
                              if( branchExistsPcs.stdout.decode("utf-8").rstrip("\n") != "0"):
                                  branchToPull = frameworkBranchName
+                                 print( "Pulling branch : " + branchToPull )
  
                              p = subprocess.run('cd {}/{} && git pull --tags origin {}'.format(root, submodule, branchToPull), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                              if(debug):
