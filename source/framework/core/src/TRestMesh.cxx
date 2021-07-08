c72ddee6d248165d6ab30a4e9c316a732add98d8
git <javier.galan@unizar.es>
2021-07-08 16:57:20 +0200

    TRestMesh::Added GetGroupPosition. GetGroupIdEnergy renamed to GetGroupEnergy.

--- source/framework/core/src/TRestMesh.cxx
+++ source/framework/core/src/TRestMesh.cxx
@@ -428,7 +428,7 @@ Int_t TRestMesh::GetGroupId(Int_t index) {
 ///////////////////////////////////////////////
 /// \brief It returns the total energy of all nodes corresponding to the group id given by argument
 ///
-Double_t TRestMesh::GetGroupIdEnergy(Int_t index) {
+Double_t TRestMesh::GetGroupEnergy(Int_t index) {
     if (index > (int)fNodeGroupID.size()) return 0.0;
 
     Double_t sum = 0;
@@ -436,6 +436,23 @@ Double_t TRestMesh::GetGroupIdEnergy(Int_t index) {
         if (fNodeGroupID[n] == index) sum += fEnergy[n];
 
     return sum;
+}
+
+///////////////////////////////////////////////
+/// \brief It returns the average position for all nodes weighted with their corresponding energy
+///
+TVector3 TRestMesh::GetGroupPosition(Int_t index) {
+    double nan = numeric_limits<double>::quiet_NaN();
+
+    if (index > (int)fNodeGroupID.size()) return TVector3(nan, nan, nan);
+
+    TVector3 v(0, 0, 0);
+    for (int n = 0; n < GetNumberOfNodes(); n++)
+        if (fNodeGroupID[n] == index) v += fEnergy[n] * GetPosition(fNodeX[n], fNodeY[n], fNodeZ[n]);
+
+    v *= 1. / GetGroupEnergy(index);
+
+    return v;
 }
 
 ///////////////////////////////////////////////
