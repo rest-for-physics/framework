71fafdca1d554dc1f53040a555f5c0b1c25c2d84
git <javier.galan@unizar.es>
2021-07-30 18:32:15 +0200

    ValidateClassify. Added additional protection

--- pipeline/analysisPlot/ValidateClassify.C
+++ pipeline/analysisPlot/ValidateClassify.C
@@ -2,17 +2,29 @@ Int_t ValidateClassify() {
     TFile* f = new TFile("/tmp/canvas.root");
 
     TH1D* h1 = (TH1D*)f->Get("NumberSignals10852");
-    cout << h1->GetEntries() << endl;
-    if (h1->GetEntries() != 7584) {
-        cout << "The number of entries at Run 10852 is not 7584!" << endl;
+    if (!h1) {
+        cout << "Histogram NumberSignals10852 not found!" << endl;
         return 1;
     }
 
+    cout << h1->GetEntries() << endl;
+
+    if (h1->GetEntries() != 7584) {
+        cout << "The number of entries at Run 10852 is not 7584!" << endl;
+        return 2;
+    }
+
     TH1D* h2 = (TH1D*)f->Get("NumberSignals10964");
+    if (!h2) {
+        cout << "Histogram NumberSignals10964 not found!" << endl;
+        return 3;
+    }
+
     cout << h2->GetEntries() << endl;
+
     if (h2->GetEntries() != 3812) {
         cout << "The number of entries at Run 10964 is not 3812!" << endl;
-        return 2;
+        return 4;
     }
 
     return 0;
