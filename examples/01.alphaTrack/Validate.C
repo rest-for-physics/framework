dc1ba5bec4e4cbb74131a938ea31f9f7426d7743
git <javier.galan@unizar.es>
2022-01-31 21:58:31 +0100

    examples/01.alphaTrack increasing validation range

--- examples/01.alphaTrack/Validate.C
+++ examples/01.alphaTrack/Validate.C
@@ -1,8 +1,8 @@
 Int_t Validate(string fname) {
     TRestRun* run = new TRestRun(fname);
     cout << "Run entries : " << run->GetEntries() << endl;
-    if (run->GetEntries() != 4424) {
-        cout << "The number of entries is not 4424!" << endl;
+    if (run->GetEntries() < 4350 || run->GetEntries() > 4500) {
+        cout << "The number of entries is not in the range (4350,4500)!" << endl;
         return 1;
     }
     delete run;
