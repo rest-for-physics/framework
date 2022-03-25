2f3b4b7640435aa66ce4c07b98d65a83ea164a16
lobis <luis.antonio.obis@gmail.com>
2022-03-25 18:45:18 +0100

    Updated testing cmake to reduce code replication

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -15,3 +15,44 @@ macro(ADD_TEST)
         add_subdirectory(test)
     endif ()
 endmacro()
+
+macro(ADD_LIBRARY_TEST)
+    if (TEST)
+        message(STATUS "Adding tests at ${CMAKE_CURRENT_SOURCE_DIR}")
+        set(TESTING_EXECUTABLE testRestGeant4)
+
+        get_filename_component(DIR_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+
+        string(SUBSTRING ${DIR_NAME} 0 1 FIRST_LETTER)
+        string(TOUPPER ${FIRST_LETTER} FIRST_LETTER)
+        string(REGEX REPLACE "^.(.*)" "${FIRST_LETTER}\\1" DIR_NAME_CAPITALIZED "${DIR_NAME}")
+
+        set(LIBRARY_NAME "Rest${DIR_NAME_CAPITALIZED}")
+        message(STATUS "TESTING: ${LIBRARY_NAME}")
+
+        set(TESTING_EXECUTABLE "test${LIBRARY_NAME}")
+
+        enable_testing()
+
+        add_executable(${TESTING_EXECUTABLE})
+
+        FILE(GLOB SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/test/src/*.cxx)
+        target_sources(
+                ${TESTING_EXECUTABLE} PUBLIC
+                ${SOURCES}
+        )
+
+        target_link_libraries(
+                ${TESTING_EXECUTABLE} PUBLIC
+                ${LIBRARY_NAME}
+                gtest_main
+                RestFramework
+                stdc++fs # <filesystem>
+        )
+
+        include(GoogleTest)
+
+        gtest_discover_tests(${TESTING_EXECUTABLE})
+
+    endif ()
+endmacro()
