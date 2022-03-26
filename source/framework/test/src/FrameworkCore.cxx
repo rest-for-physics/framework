b7ca972a9a3daa4e0f13f8f49d30ad42efdc3fc8
DavidDiezIb <david101010@hotmail.com>
2022-03-26 20:34:35 +0100

    New Test for TRestMetadata

--- source/framework/test/src/FrameworkCore.cxx
+++ source/framework/test/src/FrameworkCore.cxx
@@ -1,5 +1,6 @@
 
 #include <TRestRun.h>
+#include <TRestMetadata.h>
 #include <gtest/gtest.h>
 
 #include <filesystem>
@@ -10,6 +11,7 @@ using namespace std;
 
 #define FILES_PATH fs::path(__FILE__).parent_path().parent_path() / "files"
 #define BASIC_TRESTRUN_RML FILES_PATH / "TRestRunBasic.rml"
+#define BASIC_TRESTMETADATA_RML FILES_PATH / "metadata.rml"
 
 TEST(FrameworkCore, TestFiles) {
     cout << "FrameworkCore test files path: " << FILES_PATH << endl;
@@ -36,4 +38,27 @@ TEST(FrameworkCore, TRestRun) {
     // EXPECT_TRUE(run.GetOutputFileName() == "Run[fRunNumber]_[fRunTag]_[fExperimentName].root");
     EXPECT_TRUE(run.GetRunDescription() == "This is a test for TRestRun");
     EXPECT_TRUE(run.GetVerboseLevelString() == "debug");
+}
+
+TEST(FrameworkCore, TRestMetadata) {
+    // Check required files exist
+    EXPECT_TRUE(fs::exists(BASIC_TRESTMETADATA_RML));
+    
+    // Create new TRestMetadata class
+    class metadataTestClass: public TRestMetadata{
+        int fP1;
+        double fP2;
+        string fP3;
+    };
+    
+    metadataTestClass meta;
+    meta.LoadConfigFromFile(BASIC_TRESTMETADATA_RML, "metadata");
+
+    meta.PrintMetadata();
+    //cout << meta.GetParameter("p1") << endl;
+
+    EXPECT_TRUE(meta.GetParameter("p1") == "75");
+    EXPECT_TRUE(meta.GetParameter("p2") == "12.32");
+    EXPECT_TRUE(meta.GetParameter("p3") == "Aloha");
+    
 }
