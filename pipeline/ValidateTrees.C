7e1732cc8ee681800cefb55c2a93041e2e87052e
Luis Obis <luis.antonio.obis@gmail.com>
2022-04-03 03:53:05 +0200

    pipeline - updated `ValidateTrees.C` macro, more readable variable names

--- pipeline/ValidateTrees.C
+++ pipeline/ValidateTrees.C
@@ -1,52 +1,60 @@
 #include <TFile.h>
 #include <TTree.h>
+
 #include <iostream>
 #include <vector>
-Int_t ValidateTrees(TString validationFile) {
-    TFile* f = new TFile("results.root");
-    TTree* tr = (TTree*)f->Get("bTree");
-    std::vector<double>* vr = 0;
-    tr->SetBranchAddress("doubleObservables", &vr);
 
-    TFile* fV = new TFile(validationFile);
-    TTree* tV = (TTree*)fV->Get("bTree");
-    std::vector<double>* vV = 0;
-    tV->SetBranchAddress("doubleObservables", &vV);
+Int_t ValidateTrees(const char* validationFilename, const char* inputFilename = "results.root") {
+    TFile inputFile(inputFilename);
+    TTree* inputTree = inputFile.Get<TTree>("bTree");
+    std::vector<double>* inputObservableVector = 0;
+    inputTree->SetBranchAddress("doubleObservables", &inputObservableVector);
 
-    if (tr->GetEntries() != tV->GetEntries()) {
+    TFile validationFile(inputFilename);
+    TTree* validationTree = validationFile.Get<TTree>("bTree");
+    std::vector<double>* validationObservableVector = 0;
+    validationTree->SetBranchAddress("doubleObservables", &validationObservableVector);
+
+    if (inputTree->GetEntries() != validationTree->GetEntries()) {
         cout << "Number of entries is not the same!" << endl;
-        cout << "result: " << tr->GetEntries() << endl;
-        cout << "anticipated: " << tV->GetEntries() << endl;
+        cout << "result: " << inputTree->GetEntries() << endl;
+        cout << "anticipated: " << validationTree->GetEntries() << endl;
         return 1;
     }
 
     bool obsValErr = false;
-    for (int n = 0; n < tr->GetEntries(); n++) {
-        tr->GetEntry(n);
-        tV->GetEntry(n);
+    for (int n = 0; n < inputTree->GetEntries(); n++) {
+        inputTree->GetEntry(n);
+        validationTree->GetEntry(n);
 
-        if ((*vr).size() != (*vV).size()) {
+        if ((*inputObservableVector).size() != (*validationObservableVector).size()) {
             cout << "Vectors are different size at entry " << n << "!!" << endl;
-            cout << "results.root vector size : " << (*vr).size() << endl;
-            cout << "validation.root vector size : " << (*vV).size() << endl;
+            cout << "results.root vector size : " << (*inputObservableVector).size() << endl;
+            cout << "validation.root vector size : " << (*validationObservableVector).size() << endl;
 
             cout << "Validation vector contents: " << endl;
-            for (int m = 0; m < (*vV).size(); m++) cout << (*vV)[m] << "\t";
+            for (int m = 0; m < (*validationObservableVector).size(); m++)
+                cout << (*validationObservableVector)[m] << "\t";
             cout << endl;
 
             cout << "Results vector contents: " << endl;
-            for (int m = 0; m < (*vr).size(); m++) cout << (*vr)[m] << "\t";
+            for (int m = 0; m < (*inputObservableVector).size(); m++)
+                cout << (*inputObservableVector)[m] << "\t";
             cout << endl;
             return 2;
         }
 
-        for (int m = 0; m < (*vr).size(); m++) {
-            if ((Int_t)(1000. * (*vr)[m]) != (Int_t)(1000. * (*vV)[m])) {
+        for (int m = 0; m < (*inputObservableVector).size(); m++) {
+            if ((Int_t)(1000. * (*inputObservableVector)[m]) !=
+                (Int_t)(1000. * (*validationObservableVector)[m])) {
                 cout << "Double Observable with index " << m << " in entry " << n
                      << " is not the same value!!" << endl;
-                printf("  value: %.15f, should be: %.15f\n", (*vr)[m], (*vV)[m]);
+                printf("  value: %.15f, should be: %.15f\n", (*inputObservableVector)[m],
+                       (*validationObservableVector)[m]);
 
-                if (!TMath::IsNaN((*vr)[m] - (*vV)[m]) && abs(((*vr)[m] - (*vV)[m]) / (*vr)[m]) < 1e-15) {
+                if (!TMath::IsNaN((*inputObservableVector)[m] - (*validationObservableVector)[m]) &&
+                    abs(((*inputObservableVector)[m] - (*validationObservableVector)[m]) /
+                        (*inputObservableVector)[m]) < 1e-15) {
                     cout << "  (ignored)" << endl;
                 } else {
                     obsValErr = true;
@@ -56,6 +64,6 @@ Int_t ValidateTrees(TString validationFile) {
     }
     if (obsValErr) return 3;
 
-    cout << "Tree validation sucess!" << endl;
+    cout << "Tree validation success!" << endl;
     return 0;
 }
