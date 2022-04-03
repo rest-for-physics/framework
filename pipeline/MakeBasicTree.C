2b138edee8332e24579f583cfff1d4886c5960b1
Luis Obis <luis.antonio.obis@gmail.com>
2022-04-03 03:10:08 +0200

    pipeline - updated `MakeBasicTree.C` macro with more checks

--- pipeline/MakeBasicTree.C
+++ pipeline/MakeBasicTree.C
@@ -1,21 +1,22 @@
 // This macro will extract the double observables and will fill a tree with std::vector<double>
-Int_t MakeBasicTree(string fname) {
-    TRestRun* run = new TRestRun(fname);
+Int_t MakeBasicTree(const char* inputFilename, const char* outputFilename = "results.root",
+                    Bool_t check = true) {
+    TRestRun run(inputFilename);
 
-    TRestAnalysisTree* aTree = run->GetAnalysisTree();
+    TRestAnalysisTree* aTree = run.GetAnalysisTree();
 
     std::vector<double> obsValues;
     for (int n = 0; n < aTree->GetNumberOfObservables(); n++) {
         if (aTree->GetObservableType(n) == "double") obsValues.push_back(0);
     }
 
-    TFile* outFile = new TFile("results.root", "RECREATE");
+    TFile* outFile = new TFile(outputFilename, "RECREATE");
     TTree* myTree = new TTree("bTree", "basicTree");
 
     myTree->Branch("doubleObservables", &obsValues);
 
-    for (int n = 0; n < run->GetEntries(); n++) {
-        run->GetEntry(n);
+    for (int n = 0; n < run.GetEntries(); n++) {
+        run.GetEntry(n);
         // cout << "Entry : " << n << endl;
 
         obsValues.clear();
@@ -31,7 +32,28 @@ Int_t MakeBasicTree(string fname) {
         myTree->Fill();
     }
 
+    // check all trees have the same number of elements
+    if (run.GetEntries() != aTree->GetEntries() || run.GetEntries() != myTree->GetEntries()) {
+        cout << "ERROR: mismatch in number of tree entries" << endl;
+        return 1;
+    }
     myTree->Write();
     outFile->Close();
+
+    // check output file tree has the correct number of entries
+    if (check) {
+        TFile outFileCheck(outputFilename, "READ");
+        TTree* outTreeCheck = outFileCheck.Get<TTree>("bTree");
+        if (outTreeCheck == nullptr) {
+            cout << "ERROR: tree was not correctly written into file" << endl;
+            return 1;
+        }
+
+        if (outTreeCheck->GetEntries() != run.GetEntries()) {
+            cout << "ERROR: output file check - mismatch in number of tree entries" << endl;
+            return 1;
+        }
+    }
+
     return 0;
 }
