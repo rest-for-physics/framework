89d2df3585e64d5851103db2daff5548637c0e60
jgalan <javier.galan@unizar.es>
2022-05-05 19:35:21 +0200

    REST_MakeMetadata moving to private area

--- macros/REST_MakeMetadata.C
+++ macros/REST_MakeMetadata.C
@@ -70,6 +70,8 @@ Int_t REST_MakeMetadata(TString name) {
     headerFile << "    /// REMOVE MEMBER. A dummy member that will be NOT written to the ROOT file." << endl;
     headerFile << "    Double_t fDummyVar = 3.14; //!" << endl;
     headerFile << endl;
+    headerFile << "    void Initialize();" << endl;
+    headerFile << endl;
     headerFile << "public:" << endl;
     headerFile << "    /// UPDATE Documentation of dummy getter" << endl;
     headerFile << "    Double_t GetDummy() { return fDummy;}" << endl;
@@ -83,9 +85,6 @@ Int_t REST_MakeMetadata(TString name) {
     headerFile << "    /// UPDATE Documentation of dummy setter" << endl;
     headerFile << "    void SetDummyVar( const Double_t &dummy) { fDummyVar = dummy;}" << endl;
     headerFile << endl;
-    headerFile << "    void Initialize();" << endl;
-    headerFile << endl;
-    headerFile << "public:" << endl;
     headerFile << "    void PrintMetadata();" << endl;
     headerFile << endl;
     headerFile << "    " << name << "();" << endl;
