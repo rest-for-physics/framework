b83c9f08ecd45bdae9c3e54a6f6508bc139e8bb0
juanan <juanan318@gmail.com>
2022-05-10 16:52:53 +0200

    Added GitHook to cmake, now the clang-format is checked at the commit stage

---
+++ cmake/GitHooks.cmake
@@ -0,0 +1,18 @@
+
+if(NOT GIT_FOUND)
+  find_package(Git QUIET)
+endif()
+
+if(NOT CLANG_FORMAT)
+  find_program(CLANG_FORMAT clang-format)
+endif()
+
+# Installing clang-format precommit hook if prerequisites are met and it doesn't
+# exist yet.
+if(GIT_FOUND AND CLANG_FORMAT AND EXISTS ${PROJECT_SOURCE_DIR}/.git AND
+   NOT EXISTS ${PROJECT_SOURCE_DIR}/.git/hooks/pre-commit)
+
+   # We cannot write the file from here because we need exec permissions
+   configure_file(${CMAKE_SOURCE_DIR}/cmake/utils/git_pre-commit.in
+                  ${PROJECT_SOURCE_DIR}/.git/hooks/pre-commit)
+endif()
