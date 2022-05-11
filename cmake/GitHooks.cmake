c49a26ca2cf9c432764bdcdb1585681a8e2ff987
Luis Antonio Obis Aparicio <lobis@unizar.es>
2022-05-11 11:02:05 +0200

    Updated clang-format git hook logic

--- cmake/GitHooks.cmake
+++ cmake/GitHooks.cmake
@@ -1,18 +1,23 @@
 
-if(NOT GIT_FOUND)
-  find_package(Git QUIET)
-endif()
+find_program(clang-format QUIET)
 
-if(NOT CLANG_FORMAT)
-  find_program(CLANG_FORMAT clang-format)
-endif()
+if (clang-format_NOTFOUND)
+    message(WARNING "clang-format not found, skipping formatting in pre-commit hook")
+    return()
+endif ()
 
-# Installing clang-format precommit hook if prerequisites are met and it doesn't
-# exist yet.
-if(GIT_FOUND AND CLANG_FORMAT AND EXISTS ${PROJECT_SOURCE_DIR}/.git AND
-   NOT EXISTS ${PROJECT_SOURCE_DIR}/.git/hooks/pre-commit)
+# Installing clang-format pre-commit hook if prerequisites are met and it doesn't exist yet.
 
-   # We cannot write the file from here because we need exec permissions
-   configure_file(${CMAKE_SOURCE_DIR}/cmake/utils/git_pre-commit.in
-                  ${PROJECT_SOURCE_DIR}/.git/hooks/pre-commit)
-endif()
+if (NOT EXISTS ${PROJECT_SOURCE_DIR}/.git)
+    message(WARNING ".git directory not found at the root of the repository (${PROJECT_SOURCE_DIR}), skipping formatting in pre-commit hook")
+    return()
+endif ()
+
+if (EXISTS ${PROJECT_SOURCE_DIR}/.git/hooks/pre-commit)
+    message(DEBUG "git hook found at ${PROJECT_SOURCE_DIR}/.git/hooks/pre-commit")
+    return()
+endif ()
+
+# We cannot write the file from here because we need exec permissions
+configure_file(${CMAKE_SOURCE_DIR}/cmake/utils/git_pre-commit.in ${PROJECT_SOURCE_DIR}/.git/hooks/pre-commit)
+
