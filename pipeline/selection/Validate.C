6f79d725de309f61f0a62dd9ac0928c55591e518
lobis <luis.antonio.obis@gmail.com>
2022-06-07 16:26:52 +0200

    pipeline/selection - updated validation

--- pipeline/selection/Validate.C
+++ pipeline/selection/Validate.C
@@ -3,30 +3,38 @@
 
 #include <iostream>
 #include <vector>
-Int_t Validate(TString fname) {
-    TFile* f = new TFile(fname);
-    TTree* tr = (TTree*)f->Get("AnalysisTree");
 
-    tr->Scan("eventID:g4Ana_totalEdep:g4Ana_energyPrimary");
+Int_t Validate(const char* filename) {
+    TFile* file = TFile::Open(filename);
+    TTree* analysisTree = file->Get<TTree>("AnalysisTree");
 
-    if (tr->GetEntries() != 1) {
+    analysisTree->Scan("eventID:g4Ana_totalEdep:g4Ana_energyPrimary");
+
+    constexpr size_t expectedNumberOfEntries = 16;
+    if (analysisTree->GetEntries() != expectedNumberOfEntries) {
         cout << "Number of entries is not the same!" << endl;
-        cout << "Expected: 1. Obtained: " << tr->GetEntries() << endl;
+        cout << "Expected: " << expectedNumberOfEntries << ". Obtained: " << analysisTree->GetEntries()
+             << endl;
         return 1;
     }
 
     // Check IDs.
+    bool success = true;
     std::vector<Int_t> ids = {1};
-    for (Int_t i = 0; i < tr->GetEntries(); i++) {
-        tr->GetEntry(i);
-        if (tr->GetLeaf("eventID")->GetValue(0) != ids[i]) {
+    for (Int_t i = 0; i < analysisTree->GetEntries(); i++) {
+        analysisTree->GetEntry(i);
+        if (analysisTree->GetLeaf("eventID")->GetValue(0) != ids[i]) {
             cout << "Same number of entries but different IDs" << endl;
-            cout << "Found entry " << i << " with ID: " << tr->GetLeaf("eventID")->GetValue(0) << endl;
+            cout << "Found entry " << i << " with ID: " << analysisTree->GetLeaf("eventID")->GetValue(0)
+                 << endl;
             cout << "Expected entry " << i << " with ID: " << ids[i] << endl;
-            return 2;
+            success = false;
         }
     }
+    if (!success) {
+        return 2;
+    }
 
-    cout << "Tree validation sucess!" << endl;
+    cout << "Tree validation success!" << endl;
     return 0;
 }
