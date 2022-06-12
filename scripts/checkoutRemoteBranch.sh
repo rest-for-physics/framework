33b292fd71e821e57bcf399cbb5476a663f246dc
juanan <juanan@sultan2>
2022-06-12 18:28:45 +0200

    Adding origin to git pull in checkoutRemoteBranch.sh

--- scripts/checkoutRemoteBranch.sh
+++ scripts/checkoutRemoteBranch.sh
@@ -3,4 +3,4 @@ var=$(git ls-remote --heads origin ${1})
 
 var2="${var:1:3}"
 
-if [ -z $var2 ]; then echo "Checking out master"; git checkout master; else echo "Checking out ${1}"; git checkout ${1}; fi
+if [ -z $var2 ]; then echo "Checking out master"; git checkout origin master; else echo "Checking out ${1}"; git checkout origin ${1}; fi
