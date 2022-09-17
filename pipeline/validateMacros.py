99c8c985ed3c2a2bd99ea60bcee4226924f66459
Ni Kaixiang <bur_ning@outlook.com>
2022-09-17 22:25:26 +0800

    Update validateMacros.py

--- pipeline/validateMacros.py
+++ pipeline/validateMacros.py
@@ -6,7 +6,9 @@ with open('error.log') as f:
     lines = f.readlines()
 
 for x in lines:
-    if( x.find("warning") >= 0 ):
+    if( x.find("warning:") >= 0 ):
+        sys.exit(1)
+    if( x.find("error:") >= 0 ):
         sys.exit(1)
 print("No warning found when loading macros")
 print("Succeed")
