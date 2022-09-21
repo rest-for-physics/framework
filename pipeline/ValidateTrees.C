1d126bdf24b02d0752608f3d6502f6dfb7be1f95
Javier Galan <javier.galan@unizar.es>
2022-09-21 15:30:21 +0200

    ValidateTrees.C rounding to 2-decimal digits

--- pipeline/ValidateTrees.C
+++ pipeline/ValidateTrees.C
@@ -53,8 +53,8 @@ Int_t ValidateTrees(const char* validationFilename, const char* inputFilename = 
         }
 
         for (int m = 0; m < (*inputObservableVector).size(); m++) {
-            if ((Int_t)(1000. * (*inputObservableVector)[m]) !=
-                (Int_t)(1000. * (*validationObservableVector)[m])) {
+            if (std::round(100. * (*inputObservableVector)[m]) !=
+                std::round(100. * (*validationObservableVector)[m])) {
                 cout << "Double Observable with index " << m << " in entry " << n
                      << " is not the same value!!" << endl;
                 printf("  value: %.15f, should be: %.15f\n", (*inputObservableVector)[m],
