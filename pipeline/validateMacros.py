5aa197c9830bfc3ac5561a5db0958e8d3b80d353
Luis Obis <luis.antonio.obis@gmail.com>
2022-09-23 10:30:29 +0200

    formatting python script

--- pipeline/validateMacros.py
+++ pipeline/validateMacros.py
@@ -1,19 +1,21 @@
-import os,sys
+import os, sys
 
 os.system("restRoot -b -q -m 1 > error.log 2>&1")
 
 with open('error.log') as f:
     lines = f.readlines()
 
-for x in lines:
-    if( x.find("warning") >= 0 or x.find("Warning") >= 0 ):
+for line in lines:
+    if line.find("warning") >= 0 or line.find("Warning") >= 0:
         print("Find warnings when launching restRoot!")
         print(lines)
         sys.exit(1)
-    if( x.find("error") >= 0 or x.find("Error") >= 0):
+    if line.find("error") >= 0 or line.find("Error") >= 0:
         print("Find error messages when launching restRoot!")
         print(lines)
         sys.exit(2)
+
 print("No warning found when loading macros")
 print("Succeed")
+
 sys.exit(0)
