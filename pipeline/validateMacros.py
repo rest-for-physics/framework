4d694c1df0053a9d74188b9734a7320300bdd17b
Javier Galan <javier.galan@unizar.es>
2022-10-19 15:48:03 +0200

    validateMacros.py added undefined message identification

--- pipeline/validateMacros.py
+++ pipeline/validateMacros.py
@@ -7,13 +7,17 @@ with open('error.log') as f:
 
 for line in lines:
     if line.find("warning") >= 0 or line.find("Warning") >= 0:
-        print("Find warnings when launching restRoot!")
+        print("Found warnings when launching restRoot!")
         print(lines)
         sys.exit(1)
     if line.find("error") >= 0 or line.find("Error") >= 0:
-        print("Find error messages when launching restRoot!")
+        print("Found error messages when launching restRoot!")
         print(lines)
         sys.exit(2)
+    if line.find("undefined symbol") >= 0:
+        print("Found undefined symbol messages when launching restRoot!")
+        print(lines)
+        sys.exit(3)
 
 print("No warning found when loading macros")
 print("Succeed")
