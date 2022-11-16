c9bb581f874a5f8797e0cdfa5710bd5312981011
Javier Galan <javier.galan@unizar.es>
2022-11-16 12:55:06 +0100

    TRestPatternMask fixing an issue affecting mask rotation and internal offset

--- source/framework/masks/inc/TRestPatternMask.h
+++ source/framework/masks/inc/TRestPatternMask.h
@@ -62,7 +62,7 @@ class TRestPatternMask : public TRestMetadata {
     Bool_t HitsPattern(Double_t x, Double_t y);
 
     /// To be implemented at the inherited class with the pattern and region identification logic
-    virtual Int_t GetRegion(Double_t x, Double_t y) {
+    virtual Int_t GetRegion(Double_t& x, Double_t& y) {
         if (ApplyCommonMaskTransformation(x, y) == 0) return 1;
         return 0;
     }
