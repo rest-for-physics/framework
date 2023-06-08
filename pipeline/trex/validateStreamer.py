74cd7ca07a4ae0096046971ed9c78cf1260df00f
Javier Galan <javier.galan@unizar.es>
2023-06-08 12:53:37 +0200

    validateStreamer.py adding extra information

--- pipeline/trex/validateStreamer.py
+++ pipeline/trex/validateStreamer.py
@@ -1,6 +1,10 @@
 import os, sys
 
 os.system(
+    "restRoot -b -q StreamerOutput.C'(\"Hits_01928.root\")' | grep Process | grep TRest | grep version"
+)
+
+os.system(
     "restRoot -b -q StreamerOutput.C'(\"Hits_01928.root\")' | grep Process | grep TRest | grep version | wc -l > output.log 2>&1"
 )
 
@@ -12,7 +16,8 @@ for line in lines:
         print("The number of processes inside the event data chain is 9. Succeed!")
         sys.exit(0)
     else:
-        print("The number of processes inside the event data chain is NOT 6! Fail!")
+        print("The number of processes inside the event data chain is NOT 9! Fail!")
+        print("The number of processes is: " + line + "\n" )
         sys.exit(1)
 
 sys.exit(0)
