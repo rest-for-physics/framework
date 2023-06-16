a40254360d667bba57ebc7134819d4d517f9f40c
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-06-16 12:25:53 +0200

    add status message to library version macro, early exit if not found (no error)

--- cmake/LibraryVersion.cmake
+++ cmake/LibraryVersion.cmake
@@ -8,8 +8,9 @@ function (set_library_version TAG_VARIABLE)
     if (NOT GIT_TAG)
         message(
             WARNING
-                "Failed to retrieve Git tag. Probably the '.git' directory is missing. This may happen in remove development environments. To fix this configure the syncing of the '.git' directory."
+                "Failed to retrieve Git tag for ${CMAKE_CURRENT_SOURCE_DIR}. Probably the '.git' directory is missing. This may happen in remove development environments. To fix this configure the syncing of the '.git' directory."
         )
+        return()
     endif ()
 
     string(REGEX REPLACE "^v(.*)" "\\1" LIB_VERSION ${GIT_TAG})
@@ -17,4 +18,7 @@ function (set_library_version TAG_VARIABLE)
     set(${TAG_VARIABLE}
         ${LIB_VERSION}
         PARENT_SCOPE)
+
+    message(
+        STATUS "${CMAKE_CURRENT_SOURCE_DIR} library version: ${LIB_VERSION}")
 endfunction ()
