4a006d241c8376b6a5df731e6f9a1939d133b41c
juanan <juanan318@gmail.com>
2023-06-16 18:03:55 +0200

    Now REST_OpenInputFile attach also metadata structures from TRestRun as metadata0[metaName]

--- macros/REST_OpenInputFile.C
+++ macros/REST_OpenInputFile.C
@@ -3,6 +3,7 @@ TRestRun *run0 = nullptr;
 TRestAnalysisTree *ana_tree0 = nullptr;
 TTree *ev_tree0 = nullptr;
 TRestDataSet *dSet0 = nullptr;
+std::map<std::string, TRestMetadata *> metadata0;
 
 void REST_OpenInputFile(const std::string &fileName){
 
@@ -14,7 +15,16 @@ void REST_OpenInputFile(const std::string &fileName){
     printf("\nAttaching TRestAnalysisTree as ana_tree0...\n");
     ev_tree0 = run0->GetEventTree();
     printf("\nAttaching event tree as ev_tree0...\n");
-    
+    std::map<std::string, int> metanames;
+    for(auto &[name, meta] : metadata0)delete meta;
+    metadata0.clear();
+      for (int n = 0; n < run0->GetNumberOfMetadata(); n++) {
+        std::string metaName = run0->GetMetadataNames()[n];
+          if (metaName.find("Historic") != string::npos)continue;
+          TRestMetadata* md = run0->GetMetadata(metaName);
+          metadata0[metaName] = md;
+          printf("\nAttaching Metadata class %s as metadata0[\"%s\"]...\n", md->ClassName() , metaName.c_str());
+      }
     
   } else if (TRestTools::isDataSet(fileName )){
     printf("\n%s\n", "REST dataset file identified. It contains a valid TRestDataSet.");
@@ -23,6 +33,7 @@ void REST_OpenInputFile(const std::string &fileName){
     printf("\n%s\n", " - dSet0->PrintMetadata()");
     printf("%s\n", " - dSet0->GetDataFrame().GetColumnNames()");
     printf("%s\n\n", " - dSet0->GetTree()->GetEntries()");
+    if(dSet0)delete dSet0;
     dSet0 = new TRestDataSet();
     dSet0->Import(fileName);
   } else {
