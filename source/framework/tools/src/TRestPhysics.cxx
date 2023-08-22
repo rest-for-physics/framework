67b60cd3f1d283fe706a7be2d4a2b2d76dbb05ff
jovoy <50204158+jovoy@users.noreply.github.com>
2023-08-22 13:58:52 +0200

    @jovoy Correction of z projection of mirror lengths

--- source/framework/tools/src/TRestPhysics.cxx
+++ source/framework/tools/src/TRestPhysics.cxx
@@ -102,10 +102,10 @@ TVector3 GetParabolicVectorIntersection(const TVector3& pos, const TVector3& dir
     if (a != 0) {
         Double_t root1 = (-half_b - TMath::Sqrt(half_b * half_b - a * c)) / a;
         Double_t root2 = (-half_b + TMath::Sqrt(half_b * half_b - a * c)) / a;
-        if (pos.Z() + root1 * dir.Z() > -lMirr and pos.Z() + root1 * dir.Z() < 0) {
+        if (pos.Z() + root1 * dir.Z() > -(lMirr * TMath::Cos(alpha)) and pos.Z() + root1 * dir.Z() < 0) {
             pos.Z() -= 0.5 * x_sep;
             return pos + root1 * dir;
-        } else if (pos.Z() + root2 * dir.Z() > -lMirr and pos.Z() + root2 * dir.Z() < 0) {
+        } else if (pos.Z() + root2 * dir.Z() > -(lMirr * TMath::Cos(alpha)) and pos.Z() + root2 * dir.Z() < 0) {
             pos.Z() -= 0.5 * x_sep;
             return pos + root2 * dir;
         }
@@ -135,10 +135,10 @@ TVector3 GetHyperbolicVectorIntersection(const TVector3& pos, const TVector3& di
     Double_t c = pos.X() * pos.X() + pos.Y() * pos.Y() - R3 * R3 + e * pos.Z() - g * pos.Z() * pos.Z();
     Double_t root1 = (-half_b - TMath::Sqrt(half_b * half_b - a * c)) / a;
     Double_t root2 = (-half_b + TMath::Sqrt(half_b * half_b - a * c)) / a;
-    if (pos.Z() + root1 * dir.Z() > 0 and pos.Z() + root1 * dir.Z() < lMirr) {
+    if (pos.Z() + root1 * dir.Z() > 0 and pos.Z() + root1 * dir.Z() < (lMirr * TMath::Cos(alpha))) {
         pos.Z() += 0.5 * x_sep;
         return pos + root1 * dir;
-    } else if (pos.Z() + root2 * dir.Z() > 0 and pos.Z() + root2 * dir.Z() < lMirr) {
+    } else if (pos.Z() + root2 * dir.Z() > 0 and pos.Z() + root2 * dir.Z() < (lMirr * TMath::Cos(alpha))) {
         pos.Z() += 0.5 * x_sep;
         return pos + root2 * dir;
     }
