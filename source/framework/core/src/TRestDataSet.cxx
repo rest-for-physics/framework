0aad6921703a8ee4a4471bbefe01ca0c3fca3d8d
Javier Galan <j.a.galan.81@gmail.com>
2023-11-08 12:11:22 +0100

    TRestDataSet::EnableMultiThreading added. Now restRoot imports with MT disabled

--- source/framework/core/src/TRestDataSet.cxx
+++ source/framework/core/src/TRestDataSet.cxx
@@ -341,7 +341,10 @@ void TRestDataSet::GenerateDataSet() {
     std::sort(finalList.begin(), finalList.end());
     finalList.erase(std::unique(finalList.begin(), finalList.end()), finalList.end());
 
-    if (fMT) ROOT::EnableImplicitMT();
+    if (fMT)
+        ROOT::EnableImplicitMT();
+    else
+        ROOT::DisableImplicitMT();
 
     RESTInfo << "Initializing dataset" << RESTendl;
     fDataSet = ROOT::RDataFrame("AnalysisTree", fFileSelection);
@@ -350,7 +353,7 @@ void TRestDataSet::GenerateDataSet() {
     fDataSet = MakeCut(fCut);
 
     // Adding new user columns added to the dataset
-    for (const auto& [cName, cExpression] : fColumnNameExpressions) {
+    for (const auto & [ cName, cExpression ] : fColumnNameExpressions) {
         RESTInfo << "Adding column to dataset: " << cName << RESTendl;
         finalList.emplace_back(cName);
         fDataSet = DefineColumn(cName, cExpression);
@@ -381,8 +384,7 @@ std::vector<std::string> TRestDataSet::FileSelection() {
 
     if (!time_stamp_end || !time_stamp_start) {
         RESTError << "TRestDataSet::FileSelect. Start or end dates not properly formed. Please, check "
-                     "REST_StringHelper::StringToTimeStamp documentation for valid formats"
-                  << RESTendl;
+                     "REST_StringHelper::StringToTimeStamp documentation for valid formats" << RESTendl;
         return fFileSelection;
     }
 
@@ -437,7 +439,7 @@ std::vector<std::string> TRestDataSet::FileSelection() {
         if (!accept) continue;
 
         Double_t acc = 0;
-        for (auto& [name, properties] : fQuantity) {
+        for (auto & [ name, properties ] : fQuantity) {
             std::string value = run.ReplaceMetadataMembers(properties.metadata);
             const Double_t val = REST_StringHelper::StringToDouble(value);
 
@@ -494,7 +496,7 @@ ROOT::RDF::RNode TRestDataSet::MakeCut(const TRestCut* cut) {
 
     auto paramCut = cut->GetParamCut();
     auto obsList = df.GetColumnNames();
-    for (const auto& [param, condition] : paramCut) {
+    for (const auto & [ param, condition ] : paramCut) {
         if (std::find(obsList.begin(), obsList.end(), param) != obsList.end()) {
             std::string pCut = param + condition;
             RESTDebug << "Applying cut " << pCut << RESTendl;
@@ -541,7 +543,7 @@ ROOT::RDF::RNode TRestDataSet::DefineColumn(const std::string& columnName, const
     auto df = fDataSet;
 
     std::string evalFormula = formula;
-    for (auto const& [name, properties] : fQuantity)
+    for (auto const & [ name, properties ] : fQuantity)
         evalFormula = REST_StringHelper::Replace(evalFormula, name, properties.value);
 
     df = df.Define(columnName, evalFormula);
@@ -607,7 +609,7 @@ void TRestDataSet::PrintMetadata() {
         RESTMetadata << " Relevant quantities: " << RESTendl;
         RESTMetadata << " -------------------- " << RESTendl;
 
-        for (auto const& [name, properties] : fQuantity) {
+        for (auto const & [ name, properties ] : fQuantity) {
             RESTMetadata << " - Name : " << name << ". Value : " << properties.value
                          << ". Strategy: " << properties.strategy << RESTendl;
             RESTMetadata << " - Metadata: " << properties.metadata << RESTendl;
@@ -619,7 +621,7 @@ void TRestDataSet::PrintMetadata() {
     if (!fColumnNameExpressions.empty()) {
         RESTMetadata << " New columns added to generated dataframe: " << RESTendl;
         RESTMetadata << " ---------------------------------------- " << RESTendl;
-        for (const auto& [cName, cExpression] : fColumnNameExpressions) {
+        for (const auto & [ cName, cExpression ] : fColumnNameExpressions) {
             RESTMetadata << " - Name : " << cName << RESTendl;
             RESTMetadata << " - Expression: " << cExpression << RESTendl;
             RESTMetadata << " " << RESTendl;
@@ -792,8 +794,7 @@ void TRestDataSet::Export(const std::string& filename) {
             if (type != "Double_t" && type != "Int_t") {
                 RESTError << "Branch name : " << bName << " is type : " << type << RESTendl;
                 RESTError << "Only Int_t and Double_t types are allowed for "
-                             "exporting to ASCII table"
-                          << RESTendl;
+                             "exporting to ASCII table" << RESTendl;
                 RESTError << "File will not be generated" << RESTendl;
                 return;
             }
@@ -828,7 +829,7 @@ void TRestDataSet::Export(const std::string& filename) {
         }
         fprintf(f, "###\n");
         fprintf(f, "### Relevant quantities: \n");
-        for (auto& [name, properties] : fQuantity) {
+        for (auto & [ name, properties ] : fQuantity) {
             fprintf(f, "### - %s : %s - %s\n", name.c_str(), properties.value.c_str(),
                     properties.description.c_str());
         }
@@ -939,7 +940,10 @@ void TRestDataSet::Import(const std::string& fileName) {
         return;
     }
 
-    ROOT::EnableImplicitMT();
+    if (fMT)
+        ROOT::EnableImplicitMT();
+    else
+        ROOT::DisableImplicitMT();
 
     fDataSet = ROOT::RDataFrame("AnalysisTree", fileName);
 
