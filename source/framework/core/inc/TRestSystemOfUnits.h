72205b83a17fd0c2b2eb563b74e882d24a8593f5
Javier Galan <j.a.galan.81@gmail.com>
2024-01-13 20:03:44 +0100

    TRestSystemOfUnits. Adding official headers

--- source/framework/core/inc/TRestSystemOfUnits.h
+++ source/framework/core/inc/TRestSystemOfUnits.h
@@ -1,15 +1,24 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             TRestSystemOfUnits.h
-///
-///             Feb 2016:   First concept
-///                 author : Javier Galan
-///_______________________________________________________________________________
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
 #ifndef RestCore_TRestSystemOfUnits
 #define RestCore_TRestSystemOfUnits
@@ -33,7 +42,17 @@
 namespace REST_Units {
 
 // We use more common physics units instead of SI unit
-enum Physical_Unit { Energy, Time, Length, Mass, Voltage, MagneticField, Pressure, Angle, NOT_A_UNIT = -1 };
+enum Physical_Unit {
+    Energy,
+    Time,
+    Length,
+    Mass,
+    Voltage,
+    MagneticField,
+    Pressure,
+    Angle,
+    NOT_A_UNIT = -1
+};
 
 class TRestSystemOfUnits {
    private:
