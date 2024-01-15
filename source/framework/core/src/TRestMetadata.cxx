c9a2455adb7b1bb9060e51ac76561df2ce298904
Javier Galan <j.a.galan.81@gmail.com>
2024-01-15 10:35:17 +0100

    TRestMetadata. We ignore units inside string parameters

--- source/framework/core/src/TRestMetadata.cxx
+++ source/framework/core/src/TRestMetadata.cxx
@@ -1146,10 +1146,9 @@ void TRestMetadata::ReplaceForLoopVars(TiXmlElement* e, map<string, string> forL
                 }
             }
 
-            e->SetAttribute(name, ReplaceMathematicalExpressions(
-                                      outputBuffer, 0,
-                                      "Please, check parameter name: " + parName + " (ReplaceForLoopVars)")
-                                      .c_str());
+            e->SetAttribute(name, ReplaceMathematicalExpressions(outputBuffer, 0,
+                                                                 "Please, check parameter name: " + parName +
+                                                                     " (ReplaceForLoopVars)").c_str());
         }
 
         attr = attr->Next();
@@ -1306,8 +1305,7 @@ void TRestMetadata::ExpandIncludeFile(TiXmlElement* e) {
             TiXmlElement* ele = GetElementFromFile(filename);
             if (ele == nullptr) {
                 RESTError << "TRestMetadata::ExpandIncludeFile. No xml elements contained in the include "
-                             "file \""
-                          << filename << "\"" << RESTendl;
+                             "file \"" << filename << "\"" << RESTendl;
                 exit(1);
             }
             while (ele != nullptr) {
@@ -1389,8 +1387,7 @@ void TRestMetadata::ExpandIncludeFile(TiXmlElement* e) {
 
                 if (remoteele == nullptr) {
                     RESTWarning << "Cannot find the needed xml section in "
-                                   "include file!"
-                                << RESTendl;
+                                   "include file!" << RESTendl;
                     RESTWarning << "type: \"" << type << "\" , name: \"" << name << "\" . Skipping"
                                 << RESTendl;
                     RESTWarning << RESTendl;
@@ -2273,8 +2270,7 @@ TString TRestMetadata::GetLibraryVersion() { return fLibraryVersion; }
 void TRestMetadata::ReSetVersion() {
     if (!this->InheritsFrom("TRestRun"))
         RESTError << "version is a static value, you cannot set version "
-                     "for a class!"
-                  << RESTendl;
+                     "for a class!" << RESTendl;
     else {
         fVersion = REST_RELEASE;
     }
@@ -2286,8 +2282,7 @@ void TRestMetadata::ReSetVersion() {
 void TRestMetadata::UnSetVersion() {
     if (!this->InheritsFrom("TRestRun"))
         RESTError << "version is a static value, you cannot set version "
-                     "for a class!"
-                  << RESTendl;
+                     "for a class!" << RESTendl;
     else {
         fVersion = -1;
         fCommit = -1;
@@ -2549,11 +2544,12 @@ void TRestMetadata::ReadOneParameter(string name, string value) {
                         Double_t valueY = REST_Units::ConvertValueToRESTUnits(value.Y(), unit);
                         Double_t valueZ = REST_Units::ConvertValueToRESTUnits(value.Z(), unit);
                         *(TVector3*)datamember = TVector3(valueX, valueY, valueZ);
+                    } else if (datamember.type == "string") {
+                        // We just ignore this case
                     } else {
                         RESTWarning << this->ClassName() << " find unit definition in parameter: " << name
                                     << ", but the corresponding data member doesn't support it. Data "
-                                       "member type: "
-                                    << datamember.type << RESTendl;
+                                       "member type: " << datamember.type << RESTendl;
                         datamember.ParseString(value);
                     }
                 } else {
