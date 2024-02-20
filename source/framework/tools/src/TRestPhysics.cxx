5b7b44d9550773120ec3a90b18764773f7761925
jovoy <50204158+jovoy@users.noreply.github.com>
2024-02-20 21:24:59 +0100

    Fix problem with mirror intersections

--- source/framework/tools/src/TRestPhysics.cxx
+++ source/framework/tools/src/TRestPhysics.cxx
@@ -101,9 +101,11 @@ TVector3 GetParabolicVectorIntersection(const TVector3& pos, const TVector3& dir
     if (a != 0) {
         Double_t root1 = (-half_b - TMath::Sqrt(half_b * half_b - a * c)) / a;
         Double_t root2 = (-half_b + TMath::Sqrt(half_b * half_b - a * c)) / a;
-        if (pos.Z() + root1 * dir.Z() < 0) {
+        Double_t int1 = pos.Z() + root1 * dir.Z();
+        Double_t int2 = pos.Z() + root2 * dir.Z();
+        if (int1 < 0 and int2 < 0 and int1 > int2) {
             return pos + root1 * dir;
-        } else if (pos.Z() + root2 * dir.Z() < 0) {
+        } else if (int1 < 0 and int2 < 0 and int1 < int2) {
             return pos + root2 * dir;
         }
         return pos;
@@ -131,12 +133,13 @@ TVector3 GetHyperbolicVectorIntersection(const TVector3& pos, const TVector3& di
     Double_t c = pos.X() * pos.X() + pos.Y() * pos.Y() - R3 * R3 + e * pos.Z() - g * pos.Z() * pos.Z();
     Double_t root1 = (-half_b - TMath::Sqrt(half_b * half_b - a * c)) / a;
     Double_t root2 = (-half_b + TMath::Sqrt(half_b * half_b - a * c)) / a;
-    if (pos.Z() + root1 * dir.Z() > 0) {
+    Double_t int1 = pos.Z() + root1 * dir.Z();
+    Double_t int2 = pos.Z() + root2 * dir.Z();
+    if (int1 > 0 and int2 > 0 and int1 < int2) {
         return pos + root1 * dir;
-    } else if (pos.Z() + root2 * dir.Z() > 0) {
+    } else if (int1 > 0 and int2 > 0 and int1 > int2) {
         return pos + root2 * dir;
     }
-
     return pos;
 }
 
