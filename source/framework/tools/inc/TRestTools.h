6751aa04f0d707e03b5dca293bf7f8a4dbc36158
AlvaroEzq <alvaro@biarlighting.com>
2025-07-22 13:17:32 +0200

    add tool function GetObservablesInString

--- source/framework/tools/inc/TRestTools.h
+++ source/framework/tools/inc/TRestTools.h
@@ -83,6 +83,7 @@ class TRestTools {
 
     static std::string GetFileNameExtension(const std::string& fullname);
     static std::string GetFileNameRoot(const std::string& fullname);
+    static std::vector<std::string> GetObservablesInString(const std::string& observablesStr, bool removeDuplicates = true);
 
     static int GetBinaryFileColumns(std::string fname);
 
