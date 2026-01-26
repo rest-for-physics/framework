1ac9da676b27c54d5e1c6e76d7d0cf854b7b5b2a
Álvaro <110617994+AlvaroEzq@users.noreply.github.com>
2026-01-26 18:54:46 +0100

    Improve TRestEventTimeSelectionProcess (#559)
    * use Interval instead of pair of strings
    
    * sort fStartEndTimes
    
    * use positive time margins for both active and dead times
    
    * add functionality to use the run start and end times
    
    * fix applying of start and end run time
    
    * set fUseRunStartAndEndTimes to true by default and add documentation
    
    * improve documentation
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- source/framework/analysis/inc/TRestEventTimeSelectionProcess.h
+++ source/framework/analysis/inc/TRestEventTimeSelectionProcess.h
@@ -27,6 +27,8 @@
 
 #include <iostream>
 
+using Interval = std::pair<TTimeStamp, TTimeStamp>;
+
 class TRestEventTimeSelectionProcess : public TRestEventProcess {
    private:
     TRestEvent* fEvent;  //!
@@ -36,7 +38,8 @@ class TRestEventTimeSelectionProcess : public TRestEventProcess {
     Long_t fTimeOffsetInSeconds;
     Long_t fTimeStartMarginInSeconds;
     Long_t fTimeEndMarginInSeconds;
-    std::vector<std::pair<std::string, std::string>> fStartEndTimes;
+    std::vector<Interval> fStartEndTimes;
+    Bool_t fUseRunStartAndEndTimes;
 
     /// Information about the events processed
 
@@ -69,7 +72,7 @@ class TRestEventTimeSelectionProcess : public TRestEventProcess {
     Bool_t GetIsActiveTime() const { return fIsActiveTime; }
     Char_t GetDelimiter() const { return fDelimiter; }
 
-    std::vector<std::pair<std::string, std::string>> GetStartEndTimes() const { return fStartEndTimes; }
+    std::vector<Interval> GetStartEndTimes() const { return fStartEndTimes; }
     std::string GetTimeStampCut(std::string timeStampObsName = "timeStamp", Bool_t useOffset = true,
                                 Bool_t useMargins = true, Int_t nTimes = -1);
     Int_t GetNEventsRejected() const { return fNEventsRejected; }
@@ -80,17 +83,16 @@ class TRestEventTimeSelectionProcess : public TRestEventProcess {
     Long_t GetTimeEndMarginInSeconds() const { return fTimeEndMarginInSeconds; }
 
     Double_t CalculateTotalTimeInSeconds();
-    static std::vector<std::pair<std::string, std::string>> ReadFileWithTimes(std::string fileWithTimes,
-                                                                              Char_t delimiter = ',');
+    static std::vector<Interval> ReadFileWithTimes(std::string fileWithTimes, Char_t delimiter = ',');
+    void ApplyStartRunTime(const TTimeStamp& runStartTime);
+    void ApplyEndRunTime(const TTimeStamp& runEndTime);
 
     void SetAsActiveTime() { fIsActiveTime = true; }
     void SetAsDeadTime() { fIsActiveTime = false; }
     void SetFileWithTimes(const std::string& fileWithTimes) { fFileWithTimes = fileWithTimes; }
     void SetIsActiveTime(Bool_t isActiveTime) { fIsActiveTime = isActiveTime; }
     void SetDelimiter(Char_t delimiter) { fDelimiter = delimiter; }
-    void SetStartEndTimes(const std::vector<std::pair<std::string, std::string>>& startEndTimes) {
-        fStartEndTimes = startEndTimes;
-    }
+    void SetStartEndTimes(const std::vector<Interval>& startEndTimes) { fStartEndTimes = startEndTimes; }
     void SetTimeOffsetInSeconds(Long_t timeOffsetInSeconds) { fTimeOffsetInSeconds = timeOffsetInSeconds; }
     void SetTimeStartMarginInSeconds(Long_t timeStartMarginInSeconds) {
         fTimeStartMarginInSeconds = timeStartMarginInSeconds;
@@ -99,6 +101,6 @@ class TRestEventTimeSelectionProcess : public TRestEventProcess {
         fTimeEndMarginInSeconds = timeEndMarginInSeconds;
     }
 
-    ClassDefOverride(TRestEventTimeSelectionProcess, 1);
+    ClassDefOverride(TRestEventTimeSelectionProcess, 2);
 };
 #endif
